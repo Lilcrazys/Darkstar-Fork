8b8453bbc30e096832742ee03a168bb8f71f9b14
TeoTwawki <arawn.annwn@yandex.com>
2014-12-06 17:58:49 -0500

    Framework for lua scripting LS acquisition

--- src/map/lua/lua_baseentity.cpp
+++ src/map/lua/lua_baseentity.cpp
@@ -142,6 +142,7 @@ inline int32 CLuaBaseEntity::leavegame(lua_State *L)
 
 //==========================================================//
 
+
 inline int32 CLuaBaseEntity::ChangeMusic(lua_State *L)
 {
     DSP_DEBUG_BREAK_IF(m_PBaseEntity == NULL);
@@ -157,6 +158,58 @@ inline int32 CLuaBaseEntity::ChangeMusic(lua_State *L)
 
     PChar->pushPacket(new CChangeMusicPacket(BlockID, MusicTrackID));
     return 0;
+}
+
+//==========================================================//
+
+inline int32 CLuaBaseEntity::addLSpearl(lua_State* L)
+{
+    DSP_DEBUG_BREAK_IF(m_PBaseEntity->objtype == TYPE_NPC);
+
+    const int8* linkshellName = lua_tostring(L, 1);
+    const int8* Query = "SELECT name FROM linkshells WHERE name='%s'";
+    int32 ret = Sql_Query(SqlHandle, Query, linkshellName);
+
+    if (ret != SQL_ERROR && Sql_NumRows(SqlHandle) != 0 && Sql_NextRow(SqlHandle) == SQL_SUCCESS)
+    {
+        CCharEntity* PChar = (CCharEntity*)m_PBaseEntity;
+        uint8 invSlotID = charutils::AddItem(PChar, LOC_INVENTORY, 515, 1);
+        CItem* PItem = PChar->getStorage(LOC_INVENTORY)->GetItem(invSlotID);
+
+        if (PItem != NULL)
+        {
+            std::string qStr = ("UPDATE char_inventory SET signature='");
+            qStr += linkshellName;
+            qStr += "' WHERE charid = " + std::to_string(PChar->id);
+            qStr += " AND itemId = 515 AND signature = ''";
+            Sql_Query(SqlHandle, qStr.c_str());
+
+            Query = "SELECT linkshellid,color FROM linkshells WHERE name='%s'";
+            ret = Sql_Query(SqlHandle, Query, linkshellName);
+            if (ret != SQL_ERROR && Sql_NumRows(SqlHandle) != 0 && Sql_NextRow(SqlHandle) == SQL_SUCCESS)
+            {
+                CItemLinkshell* PLinkshell = NULL;
+
+                // Update item with name & color //
+                int8 EncodedString[16];
+                EncodeStringLinkshell((int8*)linkshellName, EncodedString);
+                PItem->setSignature(EncodedString);
+                ((CItemLinkshell*)PItem)->SetLSID(Sql_GetUIntData(SqlHandle, 0));
+                ((CItemLinkshell*)PItem)->SetLSColor(Sql_GetIntData(SqlHandle, 1));
+            /*
+                // auto-equip it //
+                PItem->setSubType(ITEM_LOCKED);
+                PChar->equip[SLOT_LINK] = invSlotID;
+                PChar->equipLoc[SLOT_LINK] = LOC_INVENTORY;
+                PLinkshell = (CItemLinkshell*)PItem;
+                if (PLinkshell)
+                    linkshell::AddOnlineMember(PChar, PLinkshell);
+            */
+            }
+        }
+    }
+
+    return 1;
 }
 
 //======================================================//
@@ -9670,6 +9723,7 @@ const int8 CLuaBaseEntity::className[] = "CBaseEntity";
 Lunar<CLuaBaseEntity>::Register_t CLuaBaseEntity::methods[] =
 {
     LUNAR_DECLARE_METHOD(CLuaBaseEntity,ChangeMusic),
+    LUNAR_DECLARE_METHOD(CLuaBaseEntity,addLSpearl),
     LUNAR_DECLARE_METHOD(CLuaBaseEntity,warp),
     LUNAR_DECLARE_METHOD(CLuaBaseEntity,leavegame),
     LUNAR_DECLARE_METHOD(CLuaBaseEntity,getID),
