5d05d421c045cdebfc09b14bdc6e979f0634c2c7
TeoTwawki <arawn.annwn@yandex.com>
2014-12-10 03:09:58 -0500

    Restored some lost progress from the other nights losses. Added added @kill for GM God to do his smiting with and confirmation messages to @setassistant. Also added basic guts of @heroshop, it doesn't work just yet though.

--- scripts/commands/setassistant.lua
+++ scripts/commands/setassistant.lua
@@ -19,8 +19,18 @@ function onTrigger(player, target)
 	end
 
 	local targ = GetPlayerByName(target);
+
 	if (targ ~= nil) then
-		targ:setFlag( 0x02002000 );
+		if (targ:getVar("AssistantGM") == 1) then
+			targ:setVar("AssistantGM",0);
+			player:PrintToPlayer( string.format( " '%s' has been stripped of Community Assistant status", target ) );
+			player:PrintToPlayer("You may need to use @setflag 0x02002000 <target> if the icon was active");
+		else
+			targ:setVar("AssistantGM",1);
+			targ:setFlag( 0x02002000 );
+			player:PrintToPlayer( string.format( " '%s' is now a Community Assistant", target ) );
+			targ:PrintToPlayer("You have been granted Community Assistant status.");
+		end
 	else
 		player:PrintToPlayer( string.format( "Player named '%s' not found!", target ) );
 		player:PrintToPlayer( "@setassistant <player>" );
