fd5481a96c9e401e6e83ecfd4dbc998ae8d762bb
LegionXI <mrsentientone@gmail.com>
2014-12-15 23:31:01 +0000

    Blue Mage Fixes
    Added missing animation ID's for blue spells, and added a few other
    animation ID's for other spells while i was adding blue stuff, for when
    they get implemented, removed some unwanted clutter and added remaining
    scripts for blue magic.

---
+++ scripts/globals/spells/bluemagic/restoral.lua
@@ -0,0 +1,41 @@
+-----------------------------------------
+-- Spell: Restoral
+-- Restores target party member's HP and enhances attack and magic attack..
+-----------------------------------------
+
+require("scripts/globals/settings");
+require("scripts/globals/status");
+require("scripts/globals/magic");
+
+-----------------------------------------
+-- OnSpellCast
+-----------------------------------------
+
+function OnMagicCastingCheck(caster,target,spell)
+	return 0;
+end;
+
+function onSpellCast(caster,target,spell)
+	local minCure = 725;
+	local divisor = 0.6666;
+	local constant = 500;
+	local power = getCurePowerOld(caster);
+	if(power > 559) then
+		divisor = 2.8333;
+		constant = 491.2
+	elseif(power > 319) then
+		divisor =  1;
+		constant = 310;
+	end
+
+	local final = getCureFinal(caster,spell,getBaseCureOld(power,divisor,constant),minCure,true);
+
+	final = final + (final * (target:getMod(MOD_CURE_POTENCY_RCVD)/100));
+	local diff = (target:getMaxHP() - target:getHP());
+	if(final > diff) then
+		final = diff;
+	end
+	target:addHP(final);
+	caster:updateEnmityFromCure(target,final);
+	return final;
+end;
--- sql/blue_spell_list.sql
+++ sql/blue_spell_list.sql
@@ -179,7 +179,7 @@ INSERT INTO `blue_spell_list` VALUES ('697','1568','4','28','1','2','0');    -- 
 INSERT INTO `blue_spell_list` VALUES ('698','388','2','22','1','0','0');     -- Wind Breath
 INSERT INTO `blue_spell_list` VALUES ('699','253','2','25','1','2','0');     -- Barbed Crescent
 INSERT INTO `blue_spell_list` VALUES ('700','2689','6','16','8','0','0');    -- Nature's Meditation
-INSERT INTO `blue_spell_list` VALUES ('701','2694','6','18','8','0','0')   ; -- Tempestuous Upheaval
+INSERT INTO `blue_spell_list` VALUES ('701','2694','6','18','8','0','0');	 -- Tempestuous Upheaval
 INSERT INTO `blue_spell_list` VALUES ('702','2702','6','13','8','0','0');    -- Rending Deluge
 INSERT INTO `blue_spell_list` VALUES ('703','2711','6','8','8','0','0');     -- Embalming Earth
 INSERT INTO `blue_spell_list` VALUES ('704','2714','6','23','8','3','0');    -- Paralyzing Triad
@@ -189,10 +189,10 @@ INSERT INTO `blue_spell_list` VALUES ('707','2774','5','17','8','0','0');    -- 
 INSERT INTO `blue_spell_list` VALUES ('708','2674','6','24','8','0','0');    -- Subduction
 INSERT INTO `blue_spell_list` VALUES ('709','893','7','24','3','0','0');     -- Thrashing Assault
 INSERT INTO `blue_spell_list` VALUES ('710','1696','4','17','2','0','0');    -- Erratic Flutter
--- INSERT INTO `blue_spell_list` VALUES ('711','0','0','0','0','0','0');     -- Restoral
--- INSERT INTO `blue_spell_list` VALUES ('712','0','0','0','0','0','0');     -- Rail Cannon
-INSERT INTO `blue_spell_list` VALUES ('713','1798','0','0','0','0','0');     -- Diffusion Ray
-INSERT INTO `blue_spell_list` VALUES ('714','1817','0','0','0','0','0');     -- Sinker Drill
+INSERT INTO `blue_spell_list` VALUES ('711','1785','7','15','8','0','0');    -- Restoral
+INSERT INTO `blue_spell_list` VALUES ('712','1798','6','27','8','0','0');    -- Rail Cannon
+INSERT INTO `blue_spell_list` VALUES ('713','1798','6','20','8','0','0');    -- Diffusion Ray
+INSERT INTO `blue_spell_list` VALUES ('714','1817','6','8','8','2','0');     -- Sinker Drill
 INSERT INTO `blue_spell_list` VALUES ('736','373','0','0','0','0','0');      -- Thunderbolt
 INSERT INTO `blue_spell_list` VALUES ('737','551','0','0','0','0','0');      -- Harden Shell
 INSERT INTO `blue_spell_list` VALUES ('738','1049','0','0','0','0','0');     -- Absolute Terror
