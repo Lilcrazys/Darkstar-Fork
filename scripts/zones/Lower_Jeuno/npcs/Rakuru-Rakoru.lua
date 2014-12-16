2ddc980589c6acb5d24f695f1d9e894d3472a9c7
Loki79 <keegancarr79@gmail.com>
2014-12-16 13:15:04 -0600

    Lower Jeuno npcs/Rakuru-Rakoru.lua fix

--- scripts/zones/Lower_Jeuno/npcs/Rakuru-Rakoru.lua
+++ scripts/zones/Lower_Jeuno/npcs/Rakuru-Rakoru.lua
@@ -12,35 +12,6 @@
 -----------------------------------
 
 function onTrade(player,npc,trade)
-	if (trade:getItemCount() == 1 and trade:hasItemQty(2331,1) == true) then
-		player:tradeComplete();
-	-- Adds all BLU spells
-		player:addSpell(513);
-		player:addSpell(515);
-		player:addSpell(517);
-		player:addSpell(518);
-		-- Spells 519 to 743
-		z = 519;
-		while z <= 743 do
-			player:addSpell(z);
-			z = z + 1;
-		end
-	end
-
-	if (trade:getItemCount() == 1 and trade:hasItemQty(1440,1) == true) then
-		player:tradeComplete();
-	-- Adds all BLU spells
-		player:addSpell(289);
-		player:addSpell(288);
-		player:addSpell(290);
-		player:addSpell(291);
-		-- Spells 292 to 306
-		z = 291;
-		while z <= 306 do
-			player:addSpell(z);
-			z = z + 1;
-		end
-	end
 end;
 
 -----------------------------------
--- scripts/zones/Lower_Jeuno/npcs/Rakuru_Rakoru.lua
+++ scripts/zones/Lower_Jeuno/npcs/Rakuru_Rakoru.lua
@@ -12,19 +12,6 @@
 -----------------------------------
 
 function onTrade(player,npc,trade)
-	if (trade:getItemCount() == 1 and trade:hasItemQty(2331,1) == true) then
-		player:tradeComplete();
-	-- Adds all BLU spells
-		player:addSpell(513);
-		player:addSpell(515);
-		player:addSpell(517);
-		player:addSpell(518);
-		-- Spells 519 to 743
-		z = 519;
-		while z <= 743 do
-			player:addSpell(z);
-			z = z + 1;
-		end
 end;
 
 -----------------------------------
