d1519f394febac529bec1bc72451629989e8fbce
TeoTwawki <arawn.annwn@yandex.com>
2014-12-19 03:47:40 -0500

    Prevent a possible crash that can be caused by players using using the costume command by restricting its use to towns for lower permission tiers.

--- scripts/commands/costume.lua
+++ scripts/commands/costume.lua
@@ -6,10 +6,19 @@
 
 cmdprops =
 {
-    permission = 0,
-    parameters = "i"
+	permission = 0,
+	parameters = "i"
 };
 
 function onTrigger(player, costume)
-    player:costume( costume );
-end
+	local ZoneID = player:getZone():getID();
+	local RegionID = player:getCurrentRegion();
+	if ( (player:getGMLevel() >= 4)
+		or (RegionID == 19 or RegionID == 20 or RegionID == 21 or RegionID == 22)
+		or (ZoneID == 48 ZoneID == 50 or (ZoneID >= 247 and ZoneID <= 250)
+		or ZoneID == 252 or ZoneID == 256 or ZoneID == 257 or ZoneID == 280) ) then
+		player:costume( costume );
+	else
+		player:PrintToPlayer("@costume cannot be used in this area for server stability reasons.");
+	end
+end;
