2873c6b2e56d02b78d3f5f2f614401600c7da8bf
Loki79 <keegancarr79@gmail.com>
2014-12-19 09:08:16 -0600

    hydra and wyrm variable fixes

--- scripts/zones/Abyssea-Grauberg/mobs/Alfard.lua
+++ scripts/zones/Abyssea-Grauberg/mobs/Alfard.lua
@@ -36,29 +36,34 @@ end;
 -- onMobFight
 -----------------------------------
 
+function OnMobSpawn(mob)
+end;
+
 function onMobFight(mob, target)
 
 	local battletime = mob:getBattleTime();
-	local headbreak, headgrow = mob:getExtraVar(2);
+    local headgrow = mob:getLocalVar("headgrow");
 	local broken = mob:AnimationSub();
 	
 	if (headgrow < battletime and broken > 0) then
 		mob:AnimationSub(broken - 1);
-		mob:setExtraVar(headbreak, battletime + 300);
+		mob:setLocalVar("headgrow", battletime + 300);
 	end
 
 end;
 
-function OnCriticalHit(mob)
+function onCriticalHit(mob)
 
 	local rand = math.random();
 	local battletime = mob:getBattleTime();
-	local headbreak, headgrow = mob:getExtraVar(2);
+    local headgrow = mob:getLocalVar("headgrow");
+	local headbreak = mob:getLocalVar("headbreak");
 	local broken = mob:AnimationSub();
 	
 	if (rand <= 0.15 and battletime >= headbreak and broken < 2) then
 		mob:AnimationSub(broken + 1);
-		mob:setExtraVar(battletime + math.random(120, 240), battletime + 300);
+		mob:setLocalVar("headgrow", battletime + math.random(120, 240))
+        mob:setLocalVar("headbreak", battletime + 300);
 	end
 
 end;
