5f797f40d170c77087416274682fa4898fb86a71
TeoTwawki <arawn.annwn@yandex.com>
2014-12-19 03:42:11 -0500

    Removed potential for abuse of a GM command that could have allowed players to randomly homepoint other players.

--- scripts/commands/homepoint.lua
+++ scripts/commands/homepoint.lua
@@ -11,7 +11,7 @@ cmdprops =
 };
 
 function onTrigger(player, target)
-    if (target == nil) then
+    if (target == nil or player:getGMLevel() == 0) then
         target = player:getName();
     end
 
@@ -21,4 +21,4 @@ function onTrigger(player, target)
     else
         player:PrintToPlayer( string.format( "Player named '%s' not found!", target ) );
     end
-end
+end;
