ab449a24ca985abec3a3338f5e38807805dbebe5
TeoTwawki <arawn.annwn@yandex.com>
2014-12-24 05:25:30 -0500

    Preventing possible nameflag conflicts.

--- scripts/commands/toggleassistant.lua
+++ scripts/commands/toggleassistant.lua
@@ -11,22 +11,34 @@ cmdprops =
 };
 
 function onTrigger(player)
-	-- First make sure player is a designated assistant.
-	if (player:getVar("AssistantGM") == 1) then
-		-- Check for and set normal GM flags to off, just in case.
-		-- This is copypasta from @togglegm so I certainly hope it works this way.
-		if (player:checkNameFlags(0x04000000)) then
-			if (player:checkNameFlags(0x04000000)) then
-				player:setFlag(0x04000000);
-			end
-			if (player:checkNameFlags(0x05000000)) then
-				player:setFlag(0x05000000);
-			end
-			if (player:checkNameFlags(0x02000000)) then
-				player:setFlag(0x02000000);
-			end
-		end
-		-- And now setting our intended icon and name color.
-		player:setFlag( 0x02002000 );
-	end
+    -- Removing any existing flags that might interfere.
+    if (player:checkNameFlags(0x04000000)) then
+        if (player:checkNameFlags(0x04000000)) then
+            player:setFlag(0x04000000);
+        end
+        if (player:checkNameFlags(0x05000000)) then
+            player:setFlag(0x05000000);
+        end
+        if (player:checkNameFlags(0x02000000)) then
+            player:setFlag(0x02000000);
+        end
+        if (player:checkNameFlags(0x06000000)) then
+            player:setFlag(0x06000000);
+        end
+    end
+    if (player:checkNameFlags(0x00002000)) then
+        player:setFlag(0x00002000);
+        if (player:checkNameFlags(0x02000000)) then
+            player:setFlag(0x02000000);
+        end
+    elseif (player:checkNameFlags(0x02000000)) then
+        player:setFlag(0x02000000);
+        if (player:checkNameFlags(0x00002000)) then
+            player:setFlag(0x00002000);
+        end
+    -- Now making sure player is a designated assistant.
+    elseif (player:getVar("AssistantGM") == 1) then
+        -- And now setting our intended icon and name color.
+        player:setFlag( 0x02002000 );
+    end
 end;
