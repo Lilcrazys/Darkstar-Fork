f289cd3c508e92ca24a02a9f4e49fa061558f2f4
Zikk420 <isaacdejong@gmail.com>
2015-01-12 17:09:05 -0500

    99 Nyzul gear missing from DS list

--- sql/item_mods_custom.sql
+++ sql/item_mods_custom.sql
@@ -8,10 +8,94 @@
 -- Using the wrong 1 of these 2 commands will result in errors.
 -- ---------------------------------------------------------------------------
 
-REPLACE INTO `item_mods` VALUES(11580, 2, 12);
-REPLACE INTO `item_mods` VALUES(11580, 27, -2);
+INSERT INTO `item_mods` VALUES(10523, 1, 31);
+INSERT INTO `item_mods` VALUES(10523, 8, 9);
+INSERT INTO `item_mods` VALUES(10523, 10, 9);
+INSERT INTO `item_mods` VALUES(10523, 12, 9);
+INSERT INTO `item_mods` VALUES(10523, 13, 9);
+INSERT INTO `item_mods` VALUES(10523, 23, 15);
+INSERT INTO `item_mods` VALUES(10523, 288, 2);
 
-REPLACE INTO `item_mods` VALUES(16002, 374, 5);
-REPLACE INTO `item_mods` VALUES(16002, 491, 5);
+INSERT INTO `item_mods` VALUES(10528, 1, 28);
+INSERT INTO `item_mods` VALUES(10528, 9, 8);
+INSERT INTO `item_mods` VALUES(10528, 10, 8);
+INSERT INTO `item_mods` VALUES(10528, 11, 8);
+INSERT INTO `item_mods` VALUES(10528, 25, 9);
+INSERT INTO `item_mods` VALUES(10528, 170, 40);
+INSERT INTO `item_mods` VALUES(10528, 167, 3);
+
+INSERT INTO `item_mods` VALUES(10533, 1, 24);
+INSERT INTO `item_mods` VALUES(10533, 6, 3);
+INSERT INTO `item_mods` VALUES(10533, 28, 13);
+INSERT INTO `item_mods` VALUES(10533, 71, 4);
+INSERT INTO `item_mods` VALUES(10533, 312, 8);
+
+INSERT INTO `item_mods` VALUES(10554, 1, 51);
+INSERT INTO `item_mods` VALUES(10554, 9, 10);
+INSERT INTO `item_mods` VALUES(10554, 10, 10);
+INSERT INTO `item_mods` VALUES(10554, 23, 24);
+INSERT INTO `item_mods` VALUES(10554, 73, 6);
+INSERT INTO `item_mods` VALUES(10554, 167, 6);
+
+INSERT INTO `item_mods` VALUES(10559, 1, 47);
+INSERT INTO `item_mods` VALUES(10559, 2, 40);
+INSERT INTO `item_mods` VALUES(10559, 8, 9);
+INSERT INTO `item_mods` VALUES(10559, 11, 9);
+INSERT INTO `item_mods` VALUES(10559, 25, 14);
+INSERT INTO `item_mods` VALUES(10559, 23, 14);
+INSERT INTO `item_mods` VALUES(10559, 167, 6);
+
+INSERT INTO `item_mods` VALUES(10564, 1, 42);
+INSERT INTO `item_mods` VALUES(10564, 3, 4);
+INSERT INTO `item_mods` VALUES(10564, 6, 4);
+INSERT INTO `item_mods` VALUES(10564, 13, 14);
+
+INSERT INTO `item_mods` VALUES(10620, 1, 28);
+INSERT INTO `item_mods` VALUES(10620, 25, 12);
+INSERT INTO `item_mods` VALUES(10620, 288, 3);
+INSERT INTO `item_mods` VALUES(10620, 161, -5);
+
+INSERT INTO `item_mods` VALUES(10625, 1, 26);
+INSERT INTO `item_mods` VALUES(10625, 9, 9);
+INSERT INTO `item_mods` VALUES(10625, 11, 9);
+INSERT INTO `item_mods` VALUES(10625, 13, 9);
+INSERT INTO `item_mods` VALUES(10625, 23, 14);
+INSERT INTO `item_mods` VALUES(10625, 24, 14);
+INSERT INTO `item_mods` VALUES(10625, 288, 3);
+
+INSERT INTO `item_mods` VALUES(10630, 1, 21);
+INSERT INTO `item_mods` VALUES(10630, 3, 3);
+INSERT INTO `item_mods` VALUES(10630, 6, 3);
+INSERT INTO `item_mods` VALUES(10630, 13, 8);
+INSERT INTO `item_mods` VALUES(10630, 14, 8);
+INSERT INTO `item_mods` VALUES(10630, 30, 7);
+INSERT INTO `item_mods` VALUES(10630, 28, 7);
+
+INSERT INTO `item_mods` VALUES(10901, 1, 36);
+INSERT INTO `item_mods` VALUES(10901, 25, 10);
+INSERT INTO `item_mods` VALUES(10901, 23, 10);
+INSERT INTO `item_mods` VALUES(10901, 73, 5);
+INSERT INTO `item_mods` VALUES(10901, 167, 7);
+INSERT INTO `item_mods` VALUES(10901, 289, 5);
+
+INSERT INTO `item_mods` VALUES(10906, 1, 34);
+INSERT INTO `item_mods` VALUES(10906, 8, 6);
+INSERT INTO `item_mods` VALUES(10906, 11, 14);
+INSERT INTO `item_mods` VALUES(10906, 24, 6);
+INSERT INTO `item_mods` VALUES(10906, 30, 6);
+INSERT INTO `item_mods` VALUES(10906, 28, 6);
+INSERT INTO `item_mods` VALUES(10906, 165, 4);
+
+INSERT INTO `item_mods` VALUES(10911, 1, 29);
+INSERT INTO `item_mods` VALUES(10911, 12, 12);
+INSERT INTO `item_mods` VALUES(10911, 28, 5);
+INSERT INTO `item_mods` VALUES(10911, 170, 10);
+
+INSERT INTO `item_mods` VALUES(11580, 2, 12);
+INSERT INTO `item_mods` VALUES(11580, 27, -2);
+
+INSERT INTO `item_mods` VALUES(16002, 374, 5);
+INSERT INTO `item_mods` VALUES(16002, 491, 5);
 
 REPLACE INTO `item_mods` VALUES(18852, 289, 50); -- MODDED OCTAVE CLUB
+
