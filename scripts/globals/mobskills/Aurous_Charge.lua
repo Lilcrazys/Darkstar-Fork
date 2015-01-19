52ea3cfb35b853b6c3ac4372549f09bdc85f94d3
TeoTwawki <arawn.annwn@yandex.com>
2015-01-19 14:44:27 -0500

    Deleted completely wrong scripts for several etudes  - start over, look at existing etudes that DON'T crash the server. Don't use MOD_WHATEVERS that don't exist in BOTH of status.lua and modifer.h !

--- scripts/globals/mobskills/Aurous_Charge.lua
+++ scripts/globals/mobskills/Aurous_Charge.lua
@@ -1,10 +1,8 @@
 ---------------------------------------------------
---  Deathgnash
 --
---  Description:  Reduces target's HP to 5% of its maximum value, ignores Utsusemi  ,Bind (30 sec)
---  Type: Magical
---  
-
+-- Aurous Charge
+--
+--
 ---------------------------------------------------
 
 require("/scripts/globals/settings");
@@ -17,19 +15,20 @@ function onMobSkillCheck(target,mob,skill)
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-
+    local typeEffect = EFFECT_BIND;
     local targetcurrentHP = target:getHP();
     local targetmaxHP = target:getMaxHP(); 
-    local hpset=targetmaxHP*0.50;
-   	local typeEffect = EFFECT_BIND;
-   	MobStatusEffectMove(mob, target, typeEffect, 1, 0, 30);
-	
-      if(HP > 1)then     
+    local hpset = targetmaxHP*0.50;
+
+    MobStatusEffectMove(mob, target, typeEffect, 1, 0, 30);
+
+    if(HP > 1)then
         dmg= hpset;
-      else
-        dmg=0;
-	  end
-	  
-	target:delHP(dmg);
+    else
+        dmg = 0;
+    end
+
+    target:delHP(dmg);
+
     return dmg;
 end
