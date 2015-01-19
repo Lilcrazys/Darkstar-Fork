7b1f020465e29eb12714fa97898c4c0a64f20f3a
Loki79 <keegancarr79@gmail.com>
2015-01-19 16:22:35 -0600

    Removed HF from healing tp move....?  why was it there?  i dont know...

--- scripts/globals/mobskills/Mighty_Guard.lua
+++ scripts/globals/mobskills/Mighty_Guard.lua
@@ -10,32 +10,19 @@ require("/scripts/globals/monstertpmoves");
 ---------------------------------------------------
 
 function onMobSkillCheck(target,mob,skill)
-   --if (mob:hasStatusEffect(EFFECT_HUNDRED_FISTS)) then
-	--	return 1;
-	--else
-
-	   return 0;
-
---end
-   -- return 1;
 end;
 
 function onMobWeaponSkill(target, mob, skill)
 
     local potency = skill:getParam();
 
-local typeEffect = EFFECT_HUNDRED_FISTS;
- 	local power = 1;
-	local duration = 240;
-    MobPhysicalStatusEffectMove(mob, target, skill, typeEffect, power, 0, duration);
-
     if(potency == 0) then
     	potency = 20;
     end
 
-    potency = potency - math.random(0, potency/4);
+    potency = potency - math.random(0, potency/1);
 
     skill:setMsg(MSG_SELF_HEAL);
 
-    return MobHealMove(mob, mob:getMaxHP() * potency / 20);
+    return MobHealMove(mob, mob:getMaxHP() * potency / 10);
 end;
--- scripts/globals/mobskills/Stygian_Sphere.lua
+++ scripts/globals/mobskills/Stygian_Sphere.lua
@@ -17,16 +17,11 @@ function onMobWeaponSkill(target, mob, skill)
 
     local potency = skill:getParam();
 
-local typeEffect = EFFECT_HUNDRED_FISTS;
- 	local power = 1;
-	local duration = 240;
-    MobPhysicalStatusEffectMove(mob, target, skill, typeEffect, power, 0, duration);
-
     if(potency == 0) then
     	potency = 15;
     end
 
-    potency = potency - math.random(0, potency/4);
+    potency = potency - math.random(0, potency/1);
 
     skill:setMsg(MSG_SELF_HEAL);
 
