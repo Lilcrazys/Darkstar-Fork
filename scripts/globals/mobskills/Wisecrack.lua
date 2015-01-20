3f13d78937f033eedaa7baac377b30e850b79c87
TeoTwawki <arawn.annwn@yandex.com>
2015-01-19 22:59:45 -0500

    Altering untested fix...No idea if it even works, I pastebin'd to IRC and somebody pushed it.

--- scripts/globals/mobskills/Wisecrack.lua
+++ scripts/globals/mobskills/Wisecrack.lua
@@ -2,26 +2,24 @@
 -- Charm enemy
 ---------------------------------------------------
 
-require("/scripts/globals/settings");
-require("/scripts/globals/status");
-require("/scripts/globals/monstertpmoves");
+require("scripts/globals/settings");
+require("scripts/globals/status");
+require("scripts/globals/monstertpmoves");
 
 ---------------------------------------------------
 
 function onMobSkillCheck(target,mob,skill)
-	return 0;
+    return 0;
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-	if (target:isPC()) then
+    if (target:isPC()) then
         local typeEffect = EFFECT_CHARM_I;
         skill:setMsg(MobStatusEffectMove(mob, target, typeEffect, 1, 0, 60));
- 
         mob:resetEnmity(target);
- 
-		return typeEffect;
+        return typeEffect;
     else
         mob:resetEnmity(target);
-		return 0;
+        return 0;
     end
 end
