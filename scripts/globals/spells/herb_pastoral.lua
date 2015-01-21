f19ead04f4b9eafa88ed3a9d108d2d6e2be44378
TeoTwawki <arawn.annwn@yandex.com>
2015-01-21 14:12:20 -0500

    Attemtp to fix broken herb_pastoral.lua

--- scripts/globals/spells/herb_pastoral.lua
+++ scripts/globals/spells/herb_pastoral.lua
@@ -16,18 +16,12 @@ function onMagicCastingCheck(caster,target,spell)
 end;
 
 function onSpellCast(caster,target,spell)
-	-- Mostly Guestimate ??
 	local duration = 120;
 	local power = 20;
- 	duration = duration + (duration * (caster:getMod(MOD_SONG_DURATION)/100));
- 	duration = duration + (duration * ((caster:getMod(MOD_ALL_SONGS) * 10)/100));
-	
-	power = power + (caster:getMod(MOD_ALL_SONGS) * 4);
-	
-    -- Until someone finds a way to delete Effects by tier we should not allow bard spells to stack.
-    -- Since all the tiers use the same effect buff it is hard to delete a specific one.
-    target:delStatusEffect(EFFECT_PASTORAL);
-    target:addStatusEffect(EFFECT_PASTORAL,power,0,duration);
-    spell:setMsg(230);
-    return EFFECT_PASTORAL;
+
+	duration = duration * (caster:getMod(MOD_SONG_DURATION_BONUS)/100)
+
+	target:addBardSong(caster,EFFECT_PASTORAL,power,0,duration,caster:getID(), 0, 1);
+
+	return EFFECT_PASTORAL;
 end;
