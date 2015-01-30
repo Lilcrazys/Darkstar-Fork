d9af9f6c3100350176a3820a1d94676dcdce534b
Loki79 <keegancarr79@gmail.com>
2015-01-29 21:05:16 -0600

    TP Fix

--- scripts/globals/mobskills/Tidal_Guillotine.lua
+++ scripts/globals/mobskills/Tidal_Guillotine.lua
@@ -19,19 +19,19 @@ end;
 
 function onMobWeaponSkill(target, mob, skill)
 
+
     local targetcurrentHP = target:getHP();
     local targetmaxHP = target:getMaxHP(); 
-    local hpset=targetmaxHP*0.00;
-   	local typeEffect = EFFECT_KO;
-		
-	MobStatusEffectMove(mob, target, typeEffect, 1, 0, 30);
+    local hpset=targetmaxHP*0.25;
+   	local typeEffect = EFFECT_BIND;
+   	MobStatusEffectMove(mob, target, typeEffect, 1, 0, 30);
 	
-	if(targetcurrentHP > hpset)then     
-		dmg= targetcurrentHP - hpset;
-	else
-		dmg=0;
-	end
+      if(targetcurrentHP > hpset)then     
+        dmg = targetcurrentHP - hpset;
+      else
+        dmg = 0;
+	  end
 	  
-		target:delHP(dmg);
-	return dmg;
+	target:delHP(dmg);
+    return dmg;
 end
