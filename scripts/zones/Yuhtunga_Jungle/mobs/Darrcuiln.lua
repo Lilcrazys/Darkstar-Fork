ae76336766190c00e7af5f539f7f9d38416ea576
Loki79 <keegancarr79@gmail.com>
2015-02-01 09:57:12 -0600

    NM balancing

--- scripts/zones/Yuhtunga_Jungle/mobs/Darrcuiln.lua
+++ scripts/zones/Yuhtunga_Jungle/mobs/Darrcuiln.lua
@@ -12,6 +12,8 @@ function onMobInitialize(mob)
     mob:addMod(MOD_TRIPLE_ATTACK,10)
     mob:setMod(MOD_REGEN, 30);
     mob:addMod(MOD_MDEF,100);
+    mob:addMod(MOD_DEF,-100);	
+    mob:addMod(MOD_EVA,-100);	
 end;
 
 -----------------------------------
