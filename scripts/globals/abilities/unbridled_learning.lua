5e26cf7d7930e9faf8d91f378490e3f1e26c4279
Loki79 <keegancarr79@gmail.com>
2015-02-13 16:11:10 -0600

    this was almost too simple...

--- scripts/globals/abilities/unbridled_learning.lua
+++ scripts/globals/abilities/unbridled_learning.lua
@@ -10,15 +10,9 @@ require("scripts/globals/status");
 -----------------------------------
 
 function onAbilityCheck(player,target,ability)  
-    if player:hasStatusEffect(EFFECT_UNBRIDLED_LEARNING) then
-		return MSGBASIC_EFFECT_ALREADY_ACTIVE, 0;
-	end
 	return 0,0;
 end;
 
 function OnUseAbility(player, target, ability)
-    if (player:getMainJob() == JOB_BLU and player:getMainLvl() >= 95) then
-	    player:addStatusEffect(EFFECT_UNBRIDLED_LEARNING,1,0,120);
-		
-	end
+	player:addStatusEffect(EFFECT_UNBRIDLED_LEARNING,1,0,120);
 end;
