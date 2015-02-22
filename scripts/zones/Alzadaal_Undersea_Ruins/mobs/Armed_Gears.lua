e406477dba5f089a65257d57f9733387c7289790
TeoTwawki <arawn.annwn@yandex.com>
2015-02-21 21:38:13 -0500

    More typos.

--- scripts/zones/Alzadaal_Undersea_Ruins/mobs/Armed_Gears.lua
+++ scripts/zones/Alzadaal_Undersea_Ruins/mobs/Armed_Gears.lua
@@ -19,7 +19,7 @@ end;
 function onMobDeath(mob,killer)
     local SPELL_ID = 712;
     local CHANCE = 25;
-    if (math.random(0,99) < CHANCE and killer:getMainJob() == JOB_BLU) and killer:player:hasSpell(SPELL_ID) == false) then
+    if (math.random(0,99) < CHANCE and killer:getMainJob() == JOB_BLU and killer:hasSpell(SPELL_ID) == false) then
         killer:addSpell(SPELL_ID);
     end
 end;
--- scripts/zones/Dragons_Aery/mobs/Nidhogg.lua
+++ scripts/zones/Dragons_Aery/mobs/Nidhogg.lua
@@ -45,7 +45,7 @@ end;
 function onMobDeath(mob, killer)
     local SPELL_ID = 738;
     local CHANCE = 25;
-    if (math.random(0,99) < CHANCE and killer:getMainJob() == JOB_BLU) and killer:hasSpell(SPELL_ID) == false) then
+    if (math.random(0,99) < CHANCE and killer:getMainJob() == JOB_BLU and killer:hasSpell(SPELL_ID) == false) then
         killer:addSpell(SPELL_ID);
     end
 
