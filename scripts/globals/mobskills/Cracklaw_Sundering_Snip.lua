88509ead90c04460c3631186608e2f1e20fea5f2
Loki79 <keegancarr79@gmail.com>
2015-02-27 15:13:52 -0600

    Cracklaw_Riptide_Eupnea.lua Cracklaw_Sundering_Snip.lua
    Lowered DMG to not be insanely stupid.

--- scripts/globals/mobskills/Cracklaw_Sundering_Snip.lua
+++ scripts/globals/mobskills/Cracklaw_Sundering_Snip.lua
@@ -11,23 +11,23 @@ require("/scripts/globals/status");
 require("/scripts/globals/monstertpmoves");
 ---------------------------------------------
 function onMobSkillCheck(target,mob,skill)
-	return 0;
+    return 0;
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-	local typeEffect = EFFECT_AMNESIA;
-    MobPhysicalStatusEffectMove(mob, target, skill, typeEffect, 5, 3, 90);
-	local numhits = 1;
-	local accmod = 1;
-	local dmgmod = math.random(4,6)+math.random();
-	local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_DMG_VARIES,1,2,3);
-	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_SLASH,MOBPARAM_WIPE_SHADOWS,info.hitslanded);
+    local typeEffect = EFFECT_AMNESIA;
+    MobPhysicalStatusEffectMove(mob, target, skill, typeEffect, 5, 0, 60);
+    local numhits = 1;
+    local accmod = 1;
+    local dmgmod = math.random(4,6)+math.random();
+    local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_DMG_VARIES,1,2,3);
+    local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_SLASH,MOBPARAM_WIPE_SHADOWS,info.hitslanded);
 
     -- keep damage around 700
     if(dmg < 1000) then
-        dmg = dmg * 1.7;
+        dmg = dmg * 0.7;
     end
 
-	target:delHP(dmg);
-	return dmg;
+    target:delHP(dmg);
+    return dmg;
 end
