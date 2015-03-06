f50672d64e705f0636f1df3f5e5ee4ee11fd15bb
TeoTwawki <arawn.annwn@yandex.com>
2015-03-06 12:14:14 -0500

    Temp bypass cs to avoid a client disconnection from large packet when using elevator in Upper Delkfutt's Tower.

--- scripts/zones/Upper_Delkfutts_Tower/npcs/qm3.lua
+++ scripts/zones/Upper_Delkfutts_Tower/npcs/qm3.lua
@@ -21,7 +21,8 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-	player:startEvent(0x0011);
+	player:setPos(-355, -144, 90, 64, 158);
+	-- player:startEvent(0x0011);
 end;
 
 -----------------------------------
