47339fd8dfe48b1cfb47a47fe2e4ffa6dd30f104
Loki79 <keegancarr79@gmail.com>
2015-03-09 12:04:21 -0500

    Terror Adjustment - Added Resit

--- scripts/globals/spells/bluemagic/absolute_terror.lua
+++ scripts/globals/spells/bluemagic/absolute_terror.lua
@@ -10,27 +10,27 @@ require("scripts/globals/bluemagic");
 -----------------------------------------
 
 function onMagicCastingCheck(caster,target,spell)
-	return 0;
+    return 0;
 end;
 
 function onSpellCast(caster,target,spell)
-	local duration = 30;
+    local duration = 15;
 
-	-- local dINT = caster:getStat(MOD_MND) - target:getStat(MOD_MND);
-	-- local resist = applyResistance(caster,spell,target,caster:getStat(MOD_INT) - target:getStat(MOD_INT),BLUE_SKILL,1.0);
-	-- if(resist > 0.625) then
-		-- resisted!
-		-- spell:setMsg(85);
-		-- return 0;
-	-- end
+    local dINT = caster:getStat(MOD_MND) - target:getStat(MOD_MND);
+    local resist = applyResistance(caster,spell,target,caster:getStat(MOD_INT) - target:getStat(MOD_INT),BLUE_SKILL,1.0);
+    if(resist > 0.725) then
+        resisted!
+        spell:setMsg(85);
+        return 0;
+    end
 
-	if(target:hasStatusEffect(EFFECT_TERROR) == true) then
-		-- no effect
-		spell:setMsg(75);
-	else
-		target:addStatusEffect(EFFECT_TERROR,1,0,duration);
-		spell:setMsg(236);
-	end
+    if(target:hasStatusEffect(EFFECT_TERROR) == true) then
+        -- no effect
+        spell:setMsg(75);
+    else
+        target:addStatusEffect(EFFECT_TERROR,1,0,duration);
+        spell:setMsg(236);
+    end
 
-	return EFFECT_TERROR;
+    return EFFECT_TERROR;
 end;
