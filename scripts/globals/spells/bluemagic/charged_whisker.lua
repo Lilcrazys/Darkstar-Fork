ff7912696acc03029f8e8aad649c71dbf50f8a78
Loki79 <keegancarr79@gmail.com>
2015-03-20 08:34:01 -0500

    spell adjustments

--- scripts/globals/spells/bluemagic/charged_whisker.lua
+++ scripts/globals/spells/bluemagic/charged_whisker.lua
@@ -12,18 +12,18 @@ require("scripts/globals/bluemagic");
 -----------------------------------------
 
 function onMagicCastingCheck(caster,target,spell)
-	return 0;
+    return 0;
 end;
 
 function onSpellCast(caster,target,spell)
-	local params = {};
+    local params = {};
 
-	local multi = 7.56;
+    local multi = 1;
     -- This data should match information on http://wiki.ffxiclopedia.org/wiki/Calculating_Blue_Magic_Damage
-	params.multiplier = multi; params.tMultiplier = 1.0; params.duppercap = 69;
-	params.str_wsc = 0.0; params.dex_wsc = 0.5; params.vit_wsc = 0.0; params.agi_wsc = 0.0; params.int_wsc = 0.3; params.mnd_wsc = 0.0; params.chr_wsc = 0.0;
+    params.multiplier = multi; params.tMultiplier = 4.5; params.duppercap = 69;
+    params.str_wsc = 0.0; params.dex_wsc = 0.5; params.vit_wsc = 0.0; params.agi_wsc = 0.0; params.int_wsc = 2.0; params.mnd_wsc = 0.0; params.chr_wsc = 0.0;
     damage = BlueMagicalSpell(caster, target, spell, params, DEX_BASED);
     damage = BlueFinalAdjustments(caster, target, spell, damage, params);
-	
+
     return damage;
 end;
--- scripts/globals/spells/bluemagic/diffusion_ray.lua
+++ scripts/globals/spells/bluemagic/diffusion_ray.lua
@@ -13,7 +13,7 @@ require("scripts/globals/bluemagic");
 -----------------------------------------
 
 function onMagicCastingCheck(caster,target,spell)
-	return 0;
+    return 0;
 end;
 
 function onSpellCast(caster,target,spell)
@@ -21,10 +21,10 @@ function onSpellCast(caster,target,spell)
     -- This data should match information on http://wiki.ffxiclopedia.org/wiki/Calculating_Blue_Magic_Damage
     params.tpmod = TPMOD_ATTACK; params.dmgtype = DMGTYPE_BLUNT; params.scattr = SC_IMPACTION;
         params.numhits = 1;
-        params.multiplier = 2.95; params.tp150 = 3.50; params.tp300 = 4.0; params.azuretp = 1.5; params.duppercap = 90;
-        params.str_wsc = 0.7; params.dex_wsc = 0.3; params.vit_wsc = 0.0; params.agi_wsc = 0.0; params.int_wsc = 0.0; params.mnd_wsc = 0.0; params.chr_wsc = 0.0;
+        params.multiplier = 5.0; params.tp150 = 3.50; params.tp300 = 4.0; params.azuretp = 1.5; params.duppercap = 90;
+        params.str_wsc = 0.7; params.dex_wsc = 0.3; params.vit_wsc = 0.0; params.agi_wsc = 0.0; params.int_wsc = 2.0; params.mnd_wsc = 0.0; params.chr_wsc = 0.0;
     damage = BluePhysicalSpell(caster, target, spell, params);
     damage = BlueFinalAdjustments(caster, target, spell, damage, params);
-   
+
     return damage;
 end;
