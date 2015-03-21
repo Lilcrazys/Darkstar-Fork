05311fa39cadc70a396d04e84a5a41fdd4d888b3
Loki79 <keegancarr79@gmail.com>
2015-03-21 10:59:47 -0500

    ..

--- scripts/globals/spells/bluemagic/charged_whisker.lua
+++ scripts/globals/spells/bluemagic/charged_whisker.lua
@@ -18,7 +18,7 @@ end;
 function onSpellCast(caster,target,spell)
     local params = {};
 
-    local multi = 3;
+    local multi = 3.5;
     -- This data should match information on http://wiki.ffxiclopedia.org/wiki/Calculating_Blue_Magic_Damage
     params.multiplier = multi; params.tMultiplier = 4.5; params.duppercap = 69;
     params.str_wsc = 0.0; params.dex_wsc = 0.5; params.vit_wsc = 0.0; params.agi_wsc = 0.0; params.int_wsc = 2.0; params.mnd_wsc = 0.0; params.chr_wsc = 0.0;
