7dfab6e334eb99cb334bca4c4725d68897581e84
TeoTwawki <arawn.annwn@yandex.com>
2015-03-25 16:54:50 -0400

    Updated sql batch file.

--- sql/Auto-SQL-Updater.cmd
+++ sql/Auto-SQL-Updater.cmd
@@ -25,7 +25,62 @@ ECHO Un-renaming custom tables to allow execution...
 ren *_custom.txt *_custom.sql
 REM FOR %%X IN (*_custom.sql) DO mysql dspdb -h localhost -u root -proot < %%X
 FOR %%X IN (*_custom.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
+ECHO ------------------------------------------------------------------
+REM Do not add tables below this line unless 100% positive that no character/account data will be harmed
+REM ------------------------------------------------------------------
+ECHO Importing misc non character data tables...
+REM FOR %%X IN (augments.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (augments.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
+REM FOR %%X IN (synth_recipes.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (synth_recipes.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
+REM FOR %%X IN (zone_settings.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (zone_settings.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
+REM FOR %%X IN (triggers.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (triggers.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
 ECHO ---------------------------------
+ECHO Importing ability/trait/ws/merit tables...
+REM FOR %%X IN (abilities.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (abilities.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
+REM FOR %%X IN (abilities_charges.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (abilities_charges.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
+REM FOR %%X IN (traits.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (traits.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
+REM FOR %%X IN (weapon_skills.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (weapon_skills.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
+REM FOR %%X IN (merits.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (merits.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
+ECHO ---------------------------------
+ECHO Importing spell tables...
+REM FOR %%X IN (spell_list.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (spell_list.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
+REM FOR %%X IN (blue_spell_list.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (blue_spell_list.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
+REM FOR %%X IN (blue_spell_mods.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (blue_spell_mods.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
+REM FOR %%X IN (blue_traits.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (blue_traits.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
+ECHO ---------------------------------
+ECHO Importing bcnm and instance tables...
+REM FOR %%X IN (bcnm_battlefield.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (bcnm_battlefield.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
+REM FOR %%X IN (bcnm_info.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (bcnm_info.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
+REM FOR %%X IN (bcnm_loot.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (bcnm_loot.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
+REM FOR %%X IN (bcnm_treasure_chests.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (bcnm_treasure_chests.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
+REM FOR %%X IN (instance_entities.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (instance_entities.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
+REM FOR %%X IN (instance_list.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (instance_list.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
+ECHO ---------------------------------
+ECHO Importing guild tables...
+REM FOR %%X IN (guild_item_points.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (guild_item_points.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
+REM FOR %%X IN (guild_shops.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (guild_shops.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
+REM FOR %%X IN (guilds.sql) DO mysql dspdb -h localhost -u root -proot < %%X
+FOR %%X IN (guilds.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -proot < %%X
 ECHO Finished!
 PAUSE
 PAUSE
