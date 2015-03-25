043ebe42d915b006628d44a7574bcecbcf788b51
TeoTwawki <arawn.annwn@yandex.com>
2015-03-25 18:58:12 -0400

    New GM commands, mainly for macro use with wallhack enabled.

---
+++ scripts/commands/down.lua
@@ -0,0 +1,31 @@
+---------------------------------------------------------------------------------------------------
+-- func: @down <optional target> <optional number>
+-- auth: TeoTwawki
+-- desc: Alters vertical coordinate
+---------------------------------------------------------------------------------------------------
+
+cmdprops =
+{
+    permission = 1,
+    parameters = "si"
+};
+
+function onTrigger(player, target, number)
+    local VALUE = targ:getYPos()
+
+    if (target == nil) then
+        target = player:getName();
+    end
+
+    local targ = GetPlayerByName( target );
+    if (targ == nil) then
+        player:PrintToPlayer( string.format( "Player named '%s' not found!", target ) );
+        return
+    end
+
+    if (number ~= nil and number > 0) then
+        VALUE = VALUE -number;
+    end
+
+    targ:setPos(targ:getXPos(), VALUE, targ:getZPos(), targ:getRotPos());
+end;
