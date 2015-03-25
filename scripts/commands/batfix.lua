090e22ced25608bfd14357b44e63b9988c0b3e88
TeoTwawki <arawn.annwn@yandex.com>
2015-03-25 19:19:10 -0400

    Ok how the crap have you been fixing things this was used for when it was broken, and also why did nobody say anything!

--- scripts/commands/batfix.lua
+++ scripts/commands/batfix.lua
@@ -31,11 +31,11 @@ function onTrigger(player, fix, target)
 		return;
 	end
 
-	if (fix = "fixtrialkills") then
+	if (fix == "fixtrialkills") then
 		require("scripts/globals/custom_trial");
 		doCustomTrial(0, target, true, false)
 		player:PrintToPlayer("If the target player had their trial item equipped, their kill counts and completion status should now be accurate");
-	elseif (fix = "finishtrial") then
+	elseif (fix == "finishtrial") then
 		target:setVar("TRIAL_COMPLETE", 1);
 		player:PrintToPlayer("The target players custom trial NPCs should now see the trial requirements have been met.");
 	elseif (fix == "99cap") then
