c71e95f7a49606017d0bbd00a410cd708ca35a34
Loki79 <keegancarr79@gmail.com>
2015-03-28 09:04:29 -0500

    typo fix

--- scripts/zones/Maquette_Abdhaljs-Legion/mobs/Paramount_Gallu.lua
+++ scripts/zones/Maquette_Abdhaljs-Legion/mobs/Paramount_Gallu.lua
@@ -17,7 +17,7 @@ function onMobInitialize(mob)
     mob:setMobMod(MOBMOD_ADD_EFFECT,mob:getShortID());
     mob:setMobMod(MOBMOD_MAIN_2HOUR, 1);
     mob:setMobMod(MOBMOD_SUB_2HOUR, 1);
-    mob:setMobMod(MOBMOD_MAIN_2HOUR, 1);
+    mob:setMobMod(MOBMOD_DRAW_IN, 2);
 end;
 
 -----------------------------------
