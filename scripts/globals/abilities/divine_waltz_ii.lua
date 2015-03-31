d63f18feb1a8dfaf75fd8759170be139f54d3e94
TeoTwawki <arawn.annwn@yandex.com>
2015-03-31 18:12:31 -0400

    Corrected Divine Waltz TP cost, added script for Divine Waltz II
    Conflicts:
    	scripts/globals/abilities/divine_waltz_ii.lua

--- scripts/globals/abilities/divine_waltz_ii.lua
+++ scripts/globals/abilities/divine_waltz_ii.lua
@@ -1,41 +1,71 @@
 -----------------------------------
--- Ability: Divine Waltz
+-- Ability: Divine Waltz II
 -----------------------------------
- 
+
 require("scripts/globals/settings");
 require("scripts/globals/status");
 
 -----------------------------------
--- OnUseAbility
+-- onUseAbility
 -----------------------------------
 
 function onAbilityCheck(player,target,ability)
-	if (player:getTP() < 80) then
-		return MSGBASIC_UNABLE_TO_USE_JA2,0;
-	else
-		player:delTP(80);
-		return 0,0;
-	end
+    if (target:getHP() == 0) then
+        return MSGBASIC_CANNOT_ON_THAT_TARG,0;
+    elseif (player:hasStatusEffect(EFFECT_SABER_DANCE)) then
+        return MSGBASIC_UNABLE_TO_USE_JA2, 0;
+    elseif (player:hasStatusEffect(EFFECT_TRANCE)) then
+        return 0,0;
+    elseif (player:getTP() < 80) then
+        return MSGBASIC_NOT_ENOUGH_TP,0;
+    else
+        -- Apply waltz recast modifiers
+        if (player:getMod(MOD_WALTZ_RECAST)~=0) then
+            local recastMod = -130 * (player:getMod(MOD_WALTZ_RECAST)); -- 650 ms per 5% (per merit)
+            if (recastMod < 0) then
+                --TODO
+            end
+        end
+        return 0,0;
+    end
 end;
 
-function OnUseAbility(player, target, ability)
+function onUseAbility(player, target, ability)
+    -- Only remove TP if the player doesn't have Trance, and only deduct once instead of for each target.
+    if (player:getID() == target:getID() and player:hasStatusEffect(EFFECT_TRANCE) == false) then
+        player:delTP(80);
+    end;
 
-	--Grabbing variables.
-	local vit = target:getStat(MOD_VIT);
-	local chr = player:getStat(MOD_CHR);
-	local cure = 0;
-	
-	cure = (vit+chr)*0.39+295;
+    -- Grabbing variables.
+    local vit = target:getStat(MOD_VIT);
+    local chr = player:getStat(MOD_CHR);
+    local mjob = player:getMainJob(); --19 for DNC main.
+    local sjob = player:getSubJob();
+    local cure = 0;
 
-	cure = cure + (cure * (player:getMod(MOD_WALTZ_POTENCY)/100));
-	cure = cure + (cure * (target:getMod(MOD_WALTZ_RCVD)/100));
+    -- Performing sj mj check.
+    if (mjob == 19) then
+        cure = (vit+chr)*0.75+270;
+    end
 
-	--Applying server mods....
-	cure = cure * CURE_POWER;
+    if (sjob == 19) then
+        cure = (vit+chr)*0.175+270;
+    end
 
-	player:addHP(cure);
-	player:updateEnmityFromCure(target,cure*(240 / ( ( 31 * target:getMainLvl() / 50 ) + 6 )),cure*(40 / ( ( 31 * target:getMainLvl() / 50 ) + 6 )));
-	
-	return cure;
-	
+    -- Apply waltz modifiers
+    cure = math.floor(cure * (1.0 + (player:getMod(MOD_WALTZ_POTENTCY)/100)));
+
+    -- Cap the final amount to max HP.
+    if ((target:getMaxHP() - target:getHP()) < cure) then
+        cure = (target:getMaxHP() - target:getHP());
+    end
+
+    -- Applying server mods....
+    cure = cure * CURE_POWER;
+
+    target:restoreHP(cure);
+    target:wakeUp();
+    player:updateEnmityFromCure(target,cure);
+
+    return cure;
 end;
