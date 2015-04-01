da275578199067f0c91a59d5b8ea99f3b4fa7ace
TeoTwawki <arawn.annwn@yandex.com>
2015-03-31 21:05:12 -0400

    Fix missing return in Depuration.lua and lowered AV's regain slightly (he's to spammy for alliance testing).

--- scripts/globals/mobskills/Depuration.lua
+++ scripts/globals/mobskills/Depuration.lua
@@ -1,11 +1,11 @@
 ---------------------------------------------
---  Depuration
---  Family: Aern
---  Type: Healing
---  Can be dispelled: N/A
---  Utsusemi/Blink absorb: N/A
---  Range: Self
---  Notes: Erases all negative effects on the mob. Aerns will generally not attempt to use this ability if no erasable effects exist on them.
+-- Depuration
+-- Family: Aern
+-- Type: Healing
+-- Can be dispelled: N/A
+-- Utsusemi/Blink absorb: N/A
+-- Range: Self
+-- Notes: Erases all negative effects on the mob. Aerns will generally not attempt to use this ability if no erasable effects exist on them.
 ---------------------------------------------
 
 require("/scripts/globals/settings");
@@ -15,19 +15,17 @@ require("/scripts/globals/monstertpmoves");
 ---------------------------------------------
 
 function onMobSkillCheck(target,mob,skill)
-    
-	local dispel = target:eraseStatusEffect();
-	
-	if(dispel ~= EFFECT_NONE) then
+    local dispel = target:eraseStatusEffect();
+
+    if( dispel ~= EFFECT_NONE) then
         return 0;
     end
 
     return 1;
-
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-
     mob:eraseAllStatusEffect();
 
+    return 0;
 end;
