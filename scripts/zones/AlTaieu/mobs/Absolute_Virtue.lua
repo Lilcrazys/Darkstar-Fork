2f0f67b203a79052477be978810fa15ace64ae0e
TeoTwawki <arawn.annwn@yandex.com>
2015-03-31 20:05:00 -0400

    AV fixes

--- scripts/zones/AlTaieu/mobs/Absolute_Virtue.lua
+++ scripts/zones/AlTaieu/mobs/Absolute_Virtue.lua
@@ -51,6 +51,8 @@ end;
 
 ------------------------------------
 -- onSpellPrecast
+------------------------------------
+
 function onSpellPrecast(mob, spell)
     if (spell:getID() == 218) then
         spell:setAoE(SPELLAOE_RADIAL);
@@ -61,261 +63,88 @@ function onSpellPrecast(mob, spell)
     end
 end;
 
+------------------------------------
+-- onMonsterMagicPrepare
+------------------------------------
+
+function onMonsterMagicPrepare(caster, target)
+    if (caster:hasStatusEffect(EFFECT_MANAFONT)) then
+        return 218;
+    elseif (caster:hasStatusEffect(EFFECT_SOUL_VOICE)) then
+        return 466;
+    end
+end;
 -----------------------------------
 -- onMobFight Action
 -----------------------------------
 
 function onMobFight(mob, target)
-    local AV_2hr_Used = 0;
+    local DID2HR = 0;
+    local RND = math.random(1,12);
+    local AV2HR = nil;
 
-    if (mob:getLocalVar("AV_2hr_Used") ~= nil) then
-        AV_2hr_Used_Used = mob:getLocalVar("AV_2hr_Used");
+    if (RND) == 1) then
+        AV2HR = 432;
+    elseif (RND) == 2) then
+        AV2HR = 433;
+    elseif (RND) == 3) then
+        AV2HR = 434;
+    elseif (RND) == 4) then
+        AV2HR = 435;
+    elseif (RND) == 5) then
+        AV2HR = 436;
+    elseif (RND) == 6) then
+        AV2HR = 437;
+    elseif (RND) == 7) then
+        AV2HR = 438;
+    elseif (RND) == 8) then
+        AV2HR = 439;
+    elseif (RND) == 9) then
+        AV2HR = 440;
+    elseif (RND) == 10) then
+        AV2HR = 474;
+    elseif (RND) == 11) then
+        AV2HR = 475;
+    elseif (RND) == 12) then
+        AV2HR = 479;
     end
 
-    if (mob:getHPP() <= 10 and AV_2hr_Used == 8) then
-        if (math.random(1,12) == 1) then
-            mob:useMobAbility(432);
-        elseif (math.random(1,12) == 2) then
-            mob:useMobAbility(433);
-        elseif (math.random(1,12) == 3) then
-            mob:useMobAbility(434);
-        elseif (math.random(1,12) == 4) then
-            mob:useMobAbility(435);
-        elseif (math.random(1,12) == 5) then
-            mob:useMobAbility(436);
-        elseif (math.random(1,12) == 6) then
-            mob:useMobAbility(437);
-        elseif (math.random(1,12) == 7) then
-            mob:useMobAbility(438);
-        elseif (math.random(1,12) == 8) then
-            mob:useMobAbility(439);
-        elseif (math.random(1,12) == 9) then
-            mob:useMobAbility(440);
-        elseif (math.random(1,12) == 10) then
-            mob:useMobAbility(474);
-        elseif (math.random(1,12) == 11) then
-            mob:useMobAbility(475);
-        elseif (math.random(1,12) == 12) then
-            mob:useMobAbility(479);
+    if (mob:getLocalVar("DID2HR") ~= nil) then
+        DID2HR_Used = mob:getLocalVar("DID2HR");
+    end
+
+    if (AV2HR ~= nil) then
+        if (mob:getHPP() <= 10 and DID2HR == 8) then
+            mob::useMobAbility(AV2hr);
             mob:setLocalVar("AV_2hr", 9);
-        end
-    elseif (mob:getHPP() <= 20 and AV_2hr_Used == 7) then
-        if (math.random(1,12) == 1) then
-            mob:useMobAbility(432);
-        elseif (math.random(1,12) == 2) then
-            mob:useMobAbility(433);
-        elseif (math.random(1,12) == 3) then
-            mob:useMobAbility(434);
-        elseif (math.random(1,12) == 4) then
-            mob:useMobAbility(435);
-        elseif (math.random(1,12) == 5) then
-            mob:useMobAbility(436);
-        elseif (math.random(1,12) == 6) then
-            mob:useMobAbility(437);
-        elseif (math.random(1,12) == 7) then
-            mob:useMobAbility(438);
-        elseif (math.random(1,12) == 8) then
-            mob:useMobAbility(439);
-        elseif (math.random(1,12) == 9) then
-            mob:useMobAbility(440);
-        elseif (math.random(1,12) == 10) then
-            mob:useMobAbility(474);
-        elseif (math.random(1,12) == 11) then
-            mob:useMobAbility(475);
-        elseif (math.random(1,12) == 12) then
-            mob:useMobAbility(479);
+        elseif (mob:getHPP() <= 20 and DID2HR == 7) then
+            mob::useMobAbility(AV2hr);
             mob:setLocalVar("AV_2hr", 8);
-        end
-    elseif (mob:getHPP() <= 30 and AV_2hr_Used == 6) then
-        if (math.random(1,12) == 1) then
-            mob:useMobAbility(432);
-        elseif (math.random(1,12) == 2) then
-            mob:useMobAbility(433);
-        elseif (math.random(1,12) == 3) then
-            mob:useMobAbility(434);
-        elseif (math.random(1,12) == 4) then
-            mob:useMobAbility(435);
-        elseif (math.random(1,12) == 5) then
-            mob:useMobAbility(436);
-        elseif (math.random(1,12) == 6) then
-            mob:useMobAbility(437);
-        elseif (math.random(1,12) == 7) then
-            mob:useMobAbility(438);
-        elseif (math.random(1,12) == 8) then
-            mob:useMobAbility(439);
-        elseif (math.random(1,12) == 9) then
-            mob:useMobAbility(440);
-        elseif (math.random(1,12) == 10) then
-            mob:useMobAbility(474);
-        elseif (math.random(1,12) == 11) then
-            mob:useMobAbility(475);
-        elseif (math.random(1,12) == 12) then
+        elseif (mob:getHPP() <= 30 and DID2HR == 6) then
             mob:useMobAbility(479);
             mob:setLocalVar("AV_2hr", 7);
-        end
-    elseif (mob:getHPP() <= 40 and AV_2hr_Used == 5) then
-        if (math.random(1,12) == 1) then
-            mob:useMobAbility(432);
-        elseif (math.random(1,12) == 2) then
-            mob:useMobAbility(433);
-        elseif (math.random(1,12) == 3) then
-            mob:useMobAbility(434);
-        elseif (math.random(1,12) == 4) then
-            mob:useMobAbility(435);
-        elseif (math.random(1,12) == 5) then
-            mob:useMobAbility(436);
-        elseif (math.random(1,12) == 6) then
-            mob:useMobAbility(437);
-        elseif (math.random(1,12) == 7) then
-            mob:useMobAbility(438);
-        elseif (math.random(1,12) == 8) then
-            mob:useMobAbility(439);
-        elseif (math.random(1,12) == 9) then
-            mob:useMobAbility(440);
-        elseif (math.random(1,12) == 10) then
-            mob:useMobAbility(474);
-        elseif (math.random(1,12) == 11) then
-            mob:useMobAbility(475);
-        elseif (math.random(1,12) == 12) then
+        elseif (mob:getHPP() <= 40 and DID2HR == 5) then
             mob:useMobAbility(479);
             mob:setLocalVar("AV_2hr", 6);
-        end
-    elseif (mob:getHPP() <= 50 and AV_2hr_Used == 4) then
-        if (math.random(1,12) == 1) then
-            mob:useMobAbility(432);
-        elseif (math.random(1,12) == 2) then
-            mob:useMobAbility(433);
-        elseif (math.random(1,12) == 3) then
-            mob:useMobAbility(434);
-        elseif (math.random(1,12) == 4) then
-            mob:useMobAbility(435);
-        elseif (math.random(1,12) == 5) then
-            mob:useMobAbility(436);
-        elseif (math.random(1,12) == 6) then
-            mob:useMobAbility(437);
-        elseif (math.random(1,12) == 7) then
-            mob:useMobAbility(438);
-        elseif (math.random(1,12) == 8) then
-            mob:useMobAbility(439);
-        elseif (math.random(1,12) == 9) then
-            mob:useMobAbility(440);
-        elseif (math.random(1,12) == 10) then
-            mob:useMobAbility(474);
-        elseif (math.random(1,12) == 11) then
-            mob:useMobAbility(475);
-        elseif (math.random(1,12) == 12) then
+        elseif (mob:getHPP() <= 50 and DID2HR == 4) then
             mob:useMobAbility(479);
             mob:setLocalVar("AV_2hr", 5);
-        end
-    elseif (mob:getHPP() <= 60 and AV_2hr_Used == 3) then
-        if (math.random(1,12) == 1) then
-            mob:useMobAbility(432);
-        elseif (math.random(1,12) == 2) then
-            mob:useMobAbility(433);
-        elseif (math.random(1,12) == 3) then
-            mob:useMobAbility(434);
-        elseif (math.random(1,12) == 4) then
-            mob:useMobAbility(435);
-        elseif (math.random(1,12) == 5) then
-            mob:useMobAbility(436);
-        elseif (math.random(1,12) == 6) then
-            mob:useMobAbility(437);
-        elseif (math.random(1,12) == 7) then
-            mob:useMobAbility(438);
-        elseif (math.random(1,12) == 8) then
-            mob:useMobAbility(439);
-        elseif (math.random(1,12) == 9) then
-            mob:useMobAbility(440);
-        elseif (math.random(1,12) == 10) then
-            mob:useMobAbility(474);
-        elseif (math.random(1,12) == 11) then
-            mob:useMobAbility(475);
-        elseif (math.random(1,12) == 12) then
+        elseif (mob:getHPP() <= 60 and DID2HR == 3) then
             mob:useMobAbility(479);
             mob:setLocalVar("AV_2hr", 4);
-        end
-    elseif (mob:getHPP() <= 70 and AV_2hr_Used == 2) then
-        if (math.random(1,12) == 1) then
-            mob:useMobAbility(432);
-        elseif (math.random(1,12) == 2) then
-            mob:useMobAbility(433);
-        elseif (math.random(1,12) == 3) then
-            mob:useMobAbility(434);
-        elseif (math.random(1,12) == 4) then
-            mob:useMobAbility(435);
-        elseif (math.random(1,12) == 5) then
-            mob:useMobAbility(436);
-        elseif (math.random(1,12) == 6) then
-            mob:useMobAbility(437);
-        elseif (math.random(1,12) == 7) then
-            mob:useMobAbility(438);
-        elseif (math.random(1,12) == 8) then
-            mob:useMobAbility(439);
-        elseif (math.random(1,12) == 9) then
-            mob:useMobAbility(440);
-        elseif (math.random(1,12) == 10) then
-            mob:useMobAbility(474);
-        elseif (math.random(1,12) == 11) then
-            mob:useMobAbility(475);
-        elseif (math.random(1,12) == 12) then
+        elseif (mob:getHPP() <= 70 and DID2HR == 2) then
             mob:useMobAbility(479);
             mob:setLocalVar("AV_2hr", 3);
-        end
-    elseif (mob:getHPP() <= 80 and AV_2hr_Used == 1) then
-        if (math.random(1,12) == 1) then
-            mob:useMobAbility(432);
-        elseif (math.random(1,12) == 2) then
-            mob:useMobAbility(433);
-        elseif (math.random(1,12) == 3) then
-            mob:useMobAbility(434);
-        elseif (math.random(1,12) == 4) then
-            mob:useMobAbility(435);
-        elseif (math.random(1,12) == 5) then
-            mob:useMobAbility(436);
-        elseif (math.random(1,12) == 6) then
-            mob:useMobAbility(437);
-        elseif (math.random(1,12) == 7) then
-            mob:useMobAbility(438);
-        elseif (math.random(1,12) == 8) then
-            mob:useMobAbility(439);
-        elseif (math.random(1,12) == 9) then
-            mob:useMobAbility(440);
-        elseif (math.random(1,12) == 10) then
-            mob:useMobAbility(474);
-        elseif (math.random(1,12) == 11) then
-            mob:useMobAbility(475);
-        elseif (math.random(1,12) == 12) then
+        elseif (mob:getHPP() <= 80 and DID2HR == 1) then
             mob:useMobAbility(479);
             mob:setLocalVar("AV_2hr", 2);
-        end
-    elseif (mob:getHPP() <= 90 and AV_2hr_Used == 0) then
-        if (math.random(1,12) == 1) then
-            mob:useMobAbility(432);
-        elseif (math.random(1,12) == 2) then
-            mob:useMobAbility(433);
-        elseif (math.random(1,12) == 3) then
-            mob:useMobAbility(434);
-        elseif (math.random(1,12) == 4) then
-            mob:useMobAbility(435);
-        elseif (math.random(1,12) == 5) then
-            mob:useMobAbility(436);
-        elseif (math.random(1,12) == 6) then
-            mob:useMobAbility(437);
-        elseif (math.random(1,12) == 7) then
-            mob:useMobAbility(438);
-        elseif (math.random(1,12) == 8) then
-            mob:useMobAbility(439);
-        elseif (math.random(1,12) == 9) then
-            mob:useMobAbility(440);
-        elseif (math.random(1,12) == 10) then
-            mob:useMobAbility(474);
-        elseif (math.random(1,12) == 11) then
-            mob:useMobAbility(475);
-        elseif (math.random(1,12) == 12) then
+        elseif (mob:getHPP() <= 90 and DID2HR == 0) then
             mob:useMobAbility(479);
             mob:setLocalVar("AV_2hr", 1);
         end
-    elseif (mob:getBattleTime() > 3600 and mob:getLocalVar("RAGED") == 0) then
+    end
+
+    if (mob:getBattleTime() > 3600 and mob:getLocalVar("RAGED") == 0) then
         mob:addStatusEffectEx(EFFECT_RAGE,0,1,0,0);
         mob:setLocalVar("RAGED", 1);
     end
