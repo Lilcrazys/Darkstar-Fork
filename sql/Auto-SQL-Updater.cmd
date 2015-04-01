31f34a985afe02947474fe89037002dcab1aef5e
TeoTwawki <arawn.annwn@yandex.com>
2015-04-01 16:56:02 -0400

    Fixed an issue in the sql batch file

--- sql/Auto-SQL-Updater.cmd
+++ sql/Auto-SQL-Updater.cmd
@@ -3,31 +3,10 @@ ECHO Before using this you must first edit this file to use the database name,
 ECHO MySQL user and MySQL password to match what your server actually uses.
 ECHO Please try NOT to accidently commit that information to your repository.
 ECHO ---------------------------------
-ECHO This file will ONLY reimport:
-ECHO item tables, mob tables, npc tables, and custom tables.
-ECHO ALL OTHER TABLES MUST BE MANUALLY HANDLED.
 PAUSE
 ECHO Renaming custom tables to prevent early execution...
 ren *_custom.sql *_custom.txt
 ECHO ---------------------------------
-ECHO importing item tables
-REM FOR %%X IN (item*.sql) DO mysql dspdb -h localhost -u root -pIr0nd00r < %%X
-FOR %%X IN (item*.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -pIr0nd00r < %%X
-ECHO ---------------------------------
-ECHO importing mob tables
-REM FOR %%X IN (mob*.sql) DO mysql dspdb -h localhost -u root -pIr0nd00r < %%X
-FOR %%X IN (mob*.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -pIr0nd00r < %%X
-ECHO ---------------------------------
-REM FOR %%X IN (npc*.sql) DO mysql dspdb -h localhost -u root -pIr0nd00r < %%X
-FOR %%X IN (npc*.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -pIr0nd00r < %%X
-ECHO ---------------------------------
-ECHO Un-renaming custom tables to allow execution...
-ren *_custom.txt *_custom.sql
-REM FOR %%X IN (*_custom.sql) DO mysql dspdb -h localhost -u root -pIr0nd00r < %%X
-FOR %%X IN (*_custom.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -pIr0nd00r < %%X
-ECHO ------------------------------------------------------------------
-REM Do not add tables below this line unless 100% positive that no character/account data will be harmed
-REM ------------------------------------------------------------------
 ECHO Importing misc non character data tables...
 REM FOR %%X IN (augments.sql) DO mysql dspdb -h localhost -u root -pIr0nd00r < %%X
 FOR %%X IN (augments.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -pIr0nd00r < %%X
@@ -83,6 +62,24 @@ REM FOR %%X IN (guild_shops.sql) DO mysql dspdb -h localhost -u root -pIr0nd00r 
 FOR %%X IN (guild_shops.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -pIr0nd00r < %%X
 REM FOR %%X IN (guilds.sql) DO mysql dspdb -h localhost -u root -pIr0nd00r < %%X
 FOR %%X IN (guilds.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -pIr0nd00r < %%X
+ECHO ------------------------------------------------------------------
+REM Do not add tables unless 100% positive that no character/account data will be harmed
+REM ------------------------------------------------------------------
+ECHO importing item tables
+REM FOR %%X IN (item*.sql) DO mysql dspdb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (item*.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -pIr0nd00r < %%X
+ECHO ---------------------------------
+ECHO importing mob tables
+REM FOR %%X IN (mob*.sql) DO mysql dspdb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (mob*.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -pIr0nd00r < %%X
+ECHO ---------------------------------
+REM FOR %%X IN (npc*.sql) DO mysql dspdb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (npc*.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -pIr0nd00r < %%X
+ECHO ---------------------------------
+ECHO Un-renaming custom tables to allow execution...
+ren *_custom.txt *_custom.sql
+REM FOR %%X IN (*_custom.sql) DO mysql dspdb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (*_custom.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -pIr0nd00r < %%X
 ECHO Finished!
 PAUSE
 PAUSE
