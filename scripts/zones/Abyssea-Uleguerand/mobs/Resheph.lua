26c0905c30c92b2aa3997aa43f096383d1baf5ee
Loki79 <keegancarr79@gmail.com>
2015-04-06 18:32:10 -0500

    apex_arrow.lua dropped slightly and Lunar adjustments

--- scripts/zones/Abyssea-Uleguerand/mobs/Resheph.lua
+++ scripts/zones/Abyssea-Uleguerand/mobs/Resheph.lua
@@ -60,7 +60,7 @@ end;
 
 function onMobDeath(mob,killer)
     local CHANCE = 15;
-    local LUNAR = 25;
+    local LUNAR = 50;
 
     if (math.random(0,99) < CHANCE and killer:hasKeyItem(ATMA_OF_THE_PLAGUEBRINGER) == false) then
         killer:addKeyItem(ATMA_OF_THE_PLAGUEBRINGER);
