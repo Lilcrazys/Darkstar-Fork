857e08eecb4531a0db530e27a54f9616bfd62714
Loki79 <keegancarr79@gmail.com>
2015-04-07 09:06:31 -0500

    quick dmg reduction

--- scripts/globals/mobskills/Raksha_Vengeance.lua
+++ scripts/globals/mobskills/Raksha_Vengeance.lua
@@ -13,16 +13,16 @@ require("/scripts/globals/monstertpmoves");
 
 ---------------------------------------------
 function onMobSkillCheck(target,mob,skill)
-	return 0;
+    return 0;
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-	local numhits = 2;
-	local accmod = 10;
-	local dmgmod = 4;
-	local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_NO_EFFECT);
-	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_BLUNT,info.hitslanded);
-	target:delHP(dmg);
+    local numhits = 1;
+    local accmod = 10;
+    local dmgmod = 4;
+    local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_NO_EFFECT);
+    local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_BLUNT,info.hitslanded);
+    target:delHP(dmg);
     MobStatusEffectMove(mob, target, EFFECT_WEAKNESS, 1, 0, 90);
-	return dmg;
+    return dmg;
 end;
