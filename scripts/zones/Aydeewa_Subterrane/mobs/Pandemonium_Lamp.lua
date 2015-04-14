84b0d26dda4705a34db3d7357b1105dd6fe26c11
Loki79 <keegancarr79@gmail.com>
2015-04-14 17:00:27 -0500

    almost forgot the lamps!

---
+++ scripts/zones/Aydeewa_Subterrane/mobs/Pandemonium_Lamp.lua
@@ -0,0 +1,41 @@
+-----------------------------------
+-- Area: Arrapago Reef
+--  Mob: Nosferatu Bats
+-----------------------------------
+
+require("scripts/globals/status");
+
+-----------------------------------
+-- onMobSpawn Action
+-----------------------------------
+
+function onMobSpawn(mob)
+    -- setMod
+
+    mob:setMod(MOD_REFRESH, 250);
+    mob:setMod(MOD_REGAIN, 30);
+    mob:setMod(MOD_UFASTCAST, 45);
+    mob:setMod(MOD_MACC,825);
+    mob:setMod(MOD_MATT,75);
+    mob:setMod(MOD_DOUBLE_ATTACK, 10);
+
+
+    -- addMod
+    mob:addMod(MOD_MDEF,30);
+    mob:addMod(MOD_DEF,50);
+end;
+
+-----------------------------------
+-- onMobEngaged Action
+-----------------------------------
+
+function onMobEngaged(mob,target)
+
+end;
+
+-----------------------------------
+-- onMobDeath
+-----------------------------------
+
+function onMobDeath(mob, killer)
+end;
