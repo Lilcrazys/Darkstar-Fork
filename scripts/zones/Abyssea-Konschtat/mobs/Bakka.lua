58e8a9e1e7ae3adcb23931e483e835a47f71ddfe
Loki79 <keegancarr79@gmail.com>
2015-04-18 09:30:26 -0500

    fixed the omni episode 645

--- scripts/zones/Abyssea-Konschtat/mobs/Bakka.lua
+++ scripts/zones/Abyssea-Konschtat/mobs/Bakka.lua
@@ -2,10 +2,7 @@
 --  Area: Abyssea - Konschtat (15)
 --   Mob: Bakka
 -----------------------------------
-package.loaded["scripts/globals/abyssea"] = nil;
------------------------------------
 
-require("scripts/zones/Abyssea-Konschtat/textIDs");
 require("scripts/globals/abyssea");
 require("scripts/globals/status");
 
@@ -21,8 +18,8 @@ end;
 -----------------------------------
 
 function onMobSpawn(mob)
-	mob:addMod(MOD_MATT,90);
-	mob:addMod(MOD_MACC,300);
+    mob:setMod(MOD_MATT,75);
+    mob:setMod(MOD_MACC,700);
 end;
 
 -----------------------------------
@@ -30,7 +27,6 @@ end;
 -----------------------------------
 
 function onMobEngaged(mob,target)
-	WeaknessTriggerSet(mob,target,ws,spell);
 end;
 
 -----------------------------------
@@ -38,7 +34,6 @@ end;
 -----------------------------------
 
 function onMobFight(mob,target)
-	TriggerHint(mob,target);
 end;
 
 -----------------------------------
@@ -48,33 +43,4 @@ end;
 function onMobDeath(mob,killer)
 end;
 
-function onMobDeathEx(mob, killer, isWeaponSkillKill, action)
-	-- local keyId = KEYITEM_ID_HERE;
-	if (killer:getVar("RED_PROC") == 1) then
-		-- if (killer:hasKeyItem(keyId) == false) then    --[[ Uncomment to use]]--
-			-- killer:addKeyItem(keyId);
-			-- killer:messageSpecial( KEYITEM_OBTAINED, keyId );
-		-- end
-	end
 
-	if (killer:getVar("BLUE_PROC") == 1) then
-		-- SetDropRate(dropID,itemID,newrate);
-		-- SetDropRate(dropID,itemID,newrate);
-		-- SetDropRate(dropID,itemID,newrate);	--[[ Uncomment to use]]--
-		-- SetDropRate(dropID,itemID,newrate);
-		-- SetDropRate(dropID,itemID,newrate);
-	end
-
-	if (killer:getVar("YELLOW_PROC") == 1) then
-		-- SetDropRate(dropID,itemID,newrate);
-		-- SetDropRate(dropID,itemID,newrate);
-		-- SetDropRate(dropID,itemID,newrate);	--[[ Uncomment to use]]--
-		-- SetDropRate(dropID,itemID,newrate);
-		-- SetDropRate(dropID,itemID,newrate);
-	end
-	-- DoExp(mob,killer);
-	-- DoCruor(mob,killer);
-    -- DoLights(killer,mob,action)
-	-- PyxisSpawn(mob,killer,npc);
-
-end;
