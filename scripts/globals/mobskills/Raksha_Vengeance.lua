f15279a0bcccb240fe3a7b1674312594c8ba6b97
Loki79 <keegancarr79@gmail.com>
2015-04-18 19:48:25 -0500

    TP fixes

--- scripts/globals/mobskills/Raksha_Vengeance.lua
+++ scripts/globals/mobskills/Raksha_Vengeance.lua
@@ -19,10 +19,10 @@ end;
 function onMobWeaponSkill(target, mob, skill)
     local numhits = 1;
     local accmod = 10;
-    local dmgmod = 4;
+    local dmgmod = 1;
     local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_NO_EFFECT);
     local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_BLUNT,info.hitslanded);
     target:delHP(dmg);
-    MobStatusEffectMove(mob, target, EFFECT_WEAKNESS, 1, 0, 90);
+    MobStatusEffectMove(mob, target, EFFECT_WEAKNESS, 1, 0, 60);
     return dmg;
 end;
