c485493331fe292997f31aafd3ab56b4777db9f2
Loki79 <keegancarr79@gmail.com>
2015-04-18 22:44:42 -0500

    ..

--- scripts/globals/mobskills/Glassy_Nova.lua
+++ scripts/globals/mobskills/Glassy_Nova.lua
@@ -26,7 +26,7 @@ function onMobWeaponSkill(target, mob, skill)
 
     local numhits = 1;
     local accmod = 1;
-    local dmgmod = 1;
+    local dmgmod = 3;
     local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_NO_EFFECT);
     local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_SLASH,MOBPARAM_WIPE_SHADOWS,info.hitslanded);
     target:delHP(dmg);
