49ae6a0daa004e6f1b8eff779d86a81dff26cfce
Loki79 <keegancarr79@gmail.com>
2015-04-18 22:38:29 -0500

    adjustments

--- scripts/globals/mobskills/Biting_Abrogation.lua
+++ scripts/globals/mobskills/Biting_Abrogation.lua
@@ -3,7 +3,7 @@
 --
 --  Description:  Reduces target's HP to 5% of its maximum value, ignores Utsusemi  ,Bind (30 sec)
 --  Type: Magical
---  
+--
 
 ---------------------------------------------------
 
@@ -20,18 +20,18 @@ end;
 function onMobWeaponSkill(target, mob, skill)
 
     local targetcurrentHP = target:getHP();
-    local targetmaxHP = target:getMaxHP(); 
-    local hpset=targetmaxHP*0.05;
-   	local typeEffect = EFFECT_BIND;
-		
-	MobStatusEffectMove(mob, target, typeEffect, 1, 0, 30);
-	
-	if(targetcurrentHP > hpset)then     
-		dmg= targetcurrentHP - hpset;
-	else
-		dmg=0;
-	end
-	  
-		target:delHP(dmg);
-	return dmg;
+    local targetmaxHP = target:getMaxHP();
+    local hpset=targetmaxHP*0.50;
+    local typeEffect = EFFECT_BIND;
+
+    MobStatusEffectMove(mob, target, typeEffect, 1, 0, 30);
+
+    if(targetcurrentHP > hpset)then
+        dmg= targetcurrentHP - hpset;
+    else
+        dmg=0;
+    end
+
+        target:delHP(dmg);
+    return dmg;
 end
