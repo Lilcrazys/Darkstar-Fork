5919158f43be0d0c405a1b81091a7aa593a28952
TeoTwawki <arawn.annwn@yandex.com>
2015-04-29 13:02:56 -0400

    Fix Mighty Guard

--- scripts/globals/mobskills/Mighty_Guard.lua
+++ scripts/globals/mobskills/Mighty_Guard.lua
@@ -1,33 +1,17 @@
 ---------------------------------------------------
---  Catharsis
 --
---  Description: Restores HP.
+-- Mighty Guard
 --
 ---------------------------------------------------
-require("/scripts/globals/settings");
-require("/scripts/globals/status");
-require("/scripts/globals/monstertpmoves");
+require("scripts/globals/settings");
+require("scripts/globals/status");
+require("scripts/globals/monstertpmoves");
 ---------------------------------------------------
 
 function onMobSkillCheck(target,mob,skill)
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-
-    local potency = skill:getParam();
-    local mobhp = mob:getHPP();
-
-    if mobhp > 50 then
-        return 0;
-    else
-
-    if(potency == 0) then
-        potency = 05;
-    end
-
-
     skill:setMsg(MSG_SELF_HEAL);
-
-    return MobHealMove(mob, mob:getMaxHP() * potency / 05);
-    end;
+    return MobHealMove(mob, mob:getMaxHP() * 0.5);
 end;
