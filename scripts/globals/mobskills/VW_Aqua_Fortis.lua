75a933a6352bae300d979deb359b9eb3dc991bdf
Loki79 <keegancarr79@gmail.com>
2015-04-29 20:54:40 -0500

    balancing

--- scripts/globals/mobskills/VW_Aqua_Fortis.lua
+++ scripts/globals/mobskills/VW_Aqua_Fortis.lua
@@ -20,8 +20,8 @@ function onMobWeaponSkill(target, mob, skill)
 
 	MobStatusEffectMove(mob, target, typeEffect, 30, 0, 60);
 
-	local dmgmod = 8;
-	local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*2.5,ELE_WATER,dmgmod,TP_NO_EFFECT);
+	local dmgmod = 9;
+	local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*5,ELE_WATER,dmgmod,TP_NO_EFFECT);
 	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_WATER,MOBPARAM_WIPE_SHADOWS);
 	target:delHP(dmg);
     MobStatusEffectMove(mob, target, EFFECT_POISON, 30, 100, 60);	
