5174aebf72292afc059ebec170257b49d8e0e8ba
Loki79 <keegancarr79@gmail.com>
2015-05-02 21:24:01 -0500

    adjustments

--- scripts/globals/mobskills/VW_Dissolve.lua
+++ scripts/globals/mobskills/VW_Dissolve.lua
@@ -14,9 +14,10 @@ function onMobSkillCheck(target,mob,skill)
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-	local dmgmod = 10;
-	local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg(),ELE_DARK,dmgmod,TP_MAB_BONUS,1);
-	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_DARK,MOBPARAM_IGNORE_SHADOWS);
+
+    local dmgmod = 3;
+    local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*5,ELE_DARK,dmgmod,TP_MAB_BONUS,1);
+    local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_DARK,MOBPARAM_IGNORE_SHADOWS);
 
     if(target:isUndead() == false) then
         target:delHP(dmg);
@@ -25,5 +26,7 @@ function onMobWeaponSkill(target, mob, skill)
     else
         skill:setMsg(MSG_NO_EFFECT);
     end
-	return dmg;
+
+    return dmg;
+end;
 end;
