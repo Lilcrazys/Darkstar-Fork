9dbb899270045ed81c6eb2185df84b15ed832518
Loki79 <keegancarr79@gmail.com>
2015-05-05 15:28:00 -0500

    adjustments

--- scripts/globals/mobskills/Gallu_Frontal.lua
+++ scripts/globals/mobskills/Gallu_Frontal.lua
@@ -15,7 +15,7 @@ function onMobSkillCheck(target,mob,skill)
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-    local dmgmod = 1;
+    local dmgmod = 2;
     local POW = 0.74
     -- target:PrintToPlayer(string.format("ATT %u ", mob:getStat(MOD_ATT)));
     -- target:PrintToPlayer(string.format("DEF %u ", target:getStat(MOD_DEF)));
