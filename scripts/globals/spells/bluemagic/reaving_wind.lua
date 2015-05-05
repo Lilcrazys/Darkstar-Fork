357eeeba82efdbfda881955ca2c9c8a2eca2ec2c
TeoTwawki <arawn.annwn@yandex.com>
2015-05-04 22:30:09 -0400

    readability/consistency

--- scripts/globals/spells/bluemagic/reaving_wind.lua
+++ scripts/globals/spells/bluemagic/reaving_wind.lua
@@ -1,24 +1,32 @@
 -----------------------------------------
 -- Spell: Reaving Wind
+--
 -----------------------------------------
+
 require("scripts/globals/magic");
 require("scripts/globals/status");
 require("scripts/globals/bluemagic");
+
 -----------------------------------------
--- OnSpellCast
+-- onMagicCastingCheck
 -----------------------------------------
 
 function onMagicCastingCheck(caster,target,spell)
 	return 0;
 end;
 
-function onSpellCast(caster,target,spell)
+-----------------------------------------
+-- OnSpellCast
+-----------------------------------------
 
-    if(target:getTP() == 0) then
+function onSpellCast(caster,target,spell)
+    -- Todo: monster affinity should modify the amount by 25%
+    if (target:getTP() == 0) then
         spell:setMsg(75); 
     else
         target:addTP(-100);
         spell:setMsg(431);
     end
+
     return tp;
 end;
