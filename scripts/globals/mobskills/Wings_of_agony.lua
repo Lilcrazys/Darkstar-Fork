0aa56495363b96c5130509617f75ba6f72782d90
Loki79 <keegancarr79@gmail.com>
2015-05-05 14:57:09 -0500

    adjsutments

--- scripts/globals/mobskills/Wings_of_agony.lua
+++ scripts/globals/mobskills/Wings_of_agony.lua
@@ -16,7 +16,7 @@ function onMobWeaponSkill(target, mob, skill)
     MobStatusEffectMove(mob, target, EFFECT_PARALYSIS, 40, 0, 120);
     MobStatusEffectMove(mob, target, EFFECT_SLEEP_II, 40, 0, 60);
 
-    local dmgmod = 1;
+    local dmgmod = 1.5;
     local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*5,ELE_WIND,dmgmod,TP_NO_EFFECT);
     local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_WIND,MOBPARAM_WIPE_SHADOWS);
     target:delHP(dmg);
--- scripts/globals/mobskills/Wings_of_woe.lua
+++ scripts/globals/mobskills/Wings_of_woe.lua
@@ -16,7 +16,7 @@ function onMobWeaponSkill(target, mob, skill)
     MobStatusEffectMove(mob, target, EFFECT_PLAGUE, 40, 0, 90);
     MobStatusEffectMove(mob, target, EFFECT_SILENCE, 40, 0, 90);
 
-    local dmgmod = 1;
+    local dmgmod = 1.5;
     local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*5,ELE_WIND,dmgmod,TP_NO_EFFECT);
     local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_WIND,MOBPARAM_WIPE_SHADOWS);
     target:delHP(dmg);
