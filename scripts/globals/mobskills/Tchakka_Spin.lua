0491d8ba05f80c666e85d21bb561298565060cf8
Loki79 <keegancarr79@gmail.com>
2015-05-07 19:21:05 -0500

    ..

--- scripts/globals/mobskills/Tchakka_Spin.lua
+++ scripts/globals/mobskills/Tchakka_Spin.lua
@@ -20,5 +20,6 @@ function onMobWeaponSkill(target, mob, skill)
 	local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*2.5,ELE_WATER,dmgmod,TP_NO_EFFECT);
 	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_WATER,MOBPARAM_WIPE_SHADOWS);
 	target:delHP(dmg);
+	skill:setMsg(264)
 	return dmg;
 end;
