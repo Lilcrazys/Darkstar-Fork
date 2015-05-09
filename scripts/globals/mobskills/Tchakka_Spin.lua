3f50b0d9171d1355afacb4a025f351f81663a11a
Loki79 <keegancarr79@gmail.com>
2015-05-09 16:02:56 -0500

    balancing

--- scripts/globals/mobskills/Tchakka_Spin.lua
+++ scripts/globals/mobskills/Tchakka_Spin.lua
@@ -14,9 +14,9 @@ end;
 function onMobWeaponSkill(target, mob, skill)
 	local typeEffect = EFFECT_SILENCE;
 
-	MobStatusEffectMove(mob, target, typeEffect, 1, 0, 10);
+	MobStatusEffectMove(mob, target, typeEffect, 1, 0, 5);
 
-	local dmgmod = 1.5;
+	local dmgmod = 1;
 	local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*2.5,ELE_WATER,dmgmod,TP_NO_EFFECT);
 	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_WATER,MOBPARAM_WIPE_SHADOWS);
 	target:delHP(dmg);
