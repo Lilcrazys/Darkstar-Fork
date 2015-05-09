621db013f5ffe9b0e20633f1b1716b2a6187e48e
Loki79 <keegancarr79@gmail.com>
2015-05-08 21:57:59 -0500

    Kumhau.lua final adjustments

--- scripts/zones/Uleguerand_Range/mobs/Kumhau.lua
+++ scripts/zones/Uleguerand_Range/mobs/Kumhau.lua
@@ -20,12 +20,12 @@ end;
 
 function onMobSpawn(mob)
     -- setMod
-    mob:setMod(MOD_REGEN, 100);
     mob:setMod(MOD_MACC, 900);
     mob:setMod(MOD_MATT, 100);
     mob:SetMobSkillAttack(true); -- Enable Special Animation for melee attacks.
     mob:setMobMod(MOBMOD_MAGIC_COOL, 25);
     mob:setMod(MOD_ACC,1450);
+    mob:setMod(MOD_DEF,1100);    
 end;
 
 -----------------------------------
