87a17cad64899d1cc81272790ed999777fb830be
Loki79 <keegancarr79@gmail.com>
2015-05-08 21:12:38 -0500

    adjsutments

--- scripts/globals/mobskills/Kumhau_Left.lua
+++ scripts/globals/mobskills/Kumhau_Left.lua
@@ -12,11 +12,8 @@ function onMobSkillCheck(target,mob,skill)
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-	local typeEffect = EFFECT_STUN;
 
-	MobStatusEffectMove(mob, target, typeEffect, 1, 0, 3);
-
-	local dmgmod = 1.25;
+	local dmgmod = 1;
 	local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*2.5,ELE_ICE,dmgmod,TP_NO_EFFECT);
 	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_ICE,MOBPARAM_WIPE_SHADOWS);
 	target:delHP(dmg);
--- scripts/globals/mobskills/Kumhau_Right.lua
+++ scripts/globals/mobskills/Kumhau_Right.lua
@@ -13,7 +13,7 @@ end;
 
 function onMobWeaponSkill(target, mob, skill)
 
-	local dmgmod = 1.5;
+	local dmgmod = 1;
 	local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*2.5,ELE_ICE,dmgmod,TP_NO_EFFECT);
 	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_ICE,MOBPARAM_WIPE_SHADOWS);
 	target:delHP(dmg);
