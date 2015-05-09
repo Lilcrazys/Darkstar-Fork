0d70201aeabfd71641b286135140dbe24faafaf2
Loki79 <keegancarr79@gmail.com>
2015-05-08 19:40:54 -0500

    fix

--- scripts/globals/mobskills/Mighty_Guard.lua
+++ scripts/globals/mobskills/Mighty_Guard.lua
@@ -9,12 +9,10 @@ require("scripts/globals/monstertpmoves");
 ---------------------------------------------------
 
 function onMobSkillCheck(target,mob,skill)
+	retur 0;
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-
-    mob:addStatusEffectEx(EFFECT_PHYSICAL_SHIELD,0,1,0,60)
-    mob:addStatusEffectEx(EFFECT_REGAIN,10,3,60)
     skill:setMsg(MSG_SELF_HEAL);
     return MobHealMove(mob, mob:getMaxHP() * 0.05);
 end;
