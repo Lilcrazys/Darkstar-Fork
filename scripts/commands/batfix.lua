c3c30da33ab252ef53440f94923e28186619834e
TeoTwawki <arawn.annwn@yandex.com>
2015-05-10 17:12:23 -0400

    Sigh. Same bat-fixes, same bat-mistakes!

--- scripts/commands/batfix.lua
+++ scripts/commands/batfix.lua
@@ -32,7 +32,7 @@ function onTrigger(player, fix, target)
 	end
 
 	if (fix == "fixtrialkills") then
-		require("scripts/globals/custom_trial");
+		require("scripts/globals/custom_trials");
 		doCustomTrial(0, target, true, false)
 		player:PrintToPlayer("If the target player had their trial item equipped, their kill counts and completion status should now be accurate");
 	elseif (fix == "finishtrial") then
