339f76e7a558fbf86611b251b06cec00f8c1eaed
Loki79 <keegancarr79@gmail.com>
2015-05-11 11:09:47 -0500

    Kumhau framework

--- scripts/globals/mobskills/Incinerating_Lahar.lua
+++ scripts/globals/mobskills/Incinerating_Lahar.lua
@@ -22,7 +22,7 @@ function onMobWeaponSkill(target, mob, skill)
 
     MobStatusEffectMove(mob, target, typeEffect, 1, 3, 30);
 
-    local dmgmod = 1;
+    local dmgmod = 1.25;
     local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*5,ELE_FIRE,dmgmod,TP_NO_EFFECT);
     local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_FIRE,MOBPARAM_WIPE_SHADOWS);
     target:delHP(dmg);
--- scripts/globals/mobskills/Volcanic_Stasis.lua
+++ scripts/globals/mobskills/Volcanic_Stasis.lua
@@ -32,7 +32,7 @@ function onMobWeaponSkill(target, mob, skill)
 
     MobPhysicalStatusEffectMove(mob, target, skill, typeEffect, 5, 3, 15);
 
-    local dmgmod = 2;
+    local dmgmod = 1.5;
     local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*3,ELE_FIRE,dmgmod,TP_MAB_BONUS,1);
     local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_LIGHT,MOBPARAM_IGNORE_SHADOWS);
     target:delHP(dmg);
