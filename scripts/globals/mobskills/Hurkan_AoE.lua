0a7a876ead70bc74046c1db953805b86280bc85e
TeoTwawki <arawn.annwn@yandex.com>
2015-05-13 19:15:20 -0400

    Oh, two of them were missing that param. Hurkan_AoE.lua this time. @Loki79 you may want to change this further

--- scripts/globals/mobskills/Hurkan_AoE.lua
+++ scripts/globals/mobskills/Hurkan_AoE.lua
@@ -14,7 +14,7 @@ end;
 function onMobWeaponSkill(target, mob, skill)
     local dmgmod = 2;
     local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*2.5,ELE_THUNDER,dmgmod,TP_NO_EFFECT);
-    local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_THUNDER);
+    local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_THUNDER,MOBPARAM_2_SHADOW);
 
     MobStatusEffectMove(mob, target, EFFECT_BLINDNESS, 30, 0, 10);
     target:addTP(-1);
