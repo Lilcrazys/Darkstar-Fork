46fd3264fc28eecc05d99cf1cf038b2adcb50dc3
Loki79 <keegancarr79@gmail.com>
2015-05-13 17:45:16 -0500

    fix

--- scripts/globals/mobskills/Hurkan_AoE.lua
+++ scripts/globals/mobskills/Hurkan_AoE.lua
@@ -19,7 +19,6 @@ function onMobWeaponSkill(target, mob, skill)
 	local dmgmod = 2;
 	local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*2.5,ELE_THUNDER,dmgmod,TP_NO_EFFECT);
 	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_THUNDER);
-	target:delHP(dmg);
     skill:setMsg(264);
 
     target:addTP(-1);
