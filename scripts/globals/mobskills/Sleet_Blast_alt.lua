5f3adafd3b1adc167c1ca62a6c6da48725b10ed9
Loki79 <keegancarr79@gmail.com>
2015-05-15 20:13:10 -0500

    sleet blast adjustments

--- scripts/globals/mobskills/Sleet_Blast_alt.lua
+++ scripts/globals/mobskills/Sleet_Blast_alt.lua
@@ -20,7 +20,9 @@ function onMobWeaponSkill(target, mob, skill)
 	local dmgmod = 1;
 	local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*3,ELE_ICE,dmgmod,TP_NO_EFFECT);
 	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_ICE,MOBPARAM_IGNORE_SHADOWS);
-	target:delHP(dmg);
-    skill:setMsg(1);
+         target:delHP(dmg);
+         skill:setMsg(1);
+         target:addTP(-1);
+         mob:addTP(1);
 	return dmg;
 end;
