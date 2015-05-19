7a29a9f0bbc64db16d41df3791c7cccb839c7302
TeoTwawki <arawn.annwn@yandex.com>
2015-05-19 07:02:22 -0400

    Added missing 2nd tier carol song scroll to item_usable_custom.sql..Should port these scroll entries to darkstar later so they can be removed form custom file and placed in item_usable.sql

--- sql/item_usable_custom.sql
+++ sql/item_usable_custom.sql
@@ -44,5 +44,7 @@ REPLACE INTO `item_usable` VALUES (4144,'hi-elixir',1,20,34,9,0,0,0,0);
 REPLACE INTO `item_usable` VALUES (4145,'elixir',1,20,34,9,0,0,0,0);
 
 INSERT INTO `item_usable` VALUES (4985,'scroll_of_horde_lullaby_ii',1,1,15,5,0,0,0,0);
-
+INSERT INTO `item_usable` VALUES (5055,'scroll_of_ice_carol_ii',1,1,15,5,0,0,0,0);
+INSERT INTO `item_usable` VALUES (5060,'scroll_of_light_carol_ii',1,1,15,5,0,0,0,0);
+INSERT INTO `item_usable` VALUES (5061,'scroll_of_dark_carol_ii',1,1,15,5,0,0,0,0);
 INSERT INTO `item_usable` VALUES (5086,'scroll_of_regen_v',1,1,11,5,0,0,0,0);
