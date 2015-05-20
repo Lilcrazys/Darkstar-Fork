f8eabcc65a4bb0796d3f4b28b100d8558926af87
TeoTwawki <arawn.annwn@yandex.com>
2015-05-20 19:40:34 -0400

    Redid the batch script that generates the additional map servers.

--- batch/SplitGameServer.cmd
+++ batch/SplitGameServer.cmd
@@ -3,29 +3,17 @@ echo This will create/recreate map individual map servers from DSGame-server.exe
 PAUSE
 cd ..
 echo ------------------------
-if exist DSGame-Abyssea.exe del DSGame-Abyssea.exe
-if not exist DSGame-Abyssea.exe copy DSGame-server.exe /b DSGame-Abyssea.exe /b
-echo ------------------------
-if exist DSGame-DynaLimbus.exe del DSGame-DynaLimbus.exe
-if not exist DSGame-DynaLimbus.exe copy DSGame-server.exe /b DSGame-DynaLimbus.exe /b
-echo ------------------------
-if exist DSGame-BCNM.exe del DSGame-BCNM.exe
-if not exist DSGame-BCNM.exe copy DSGame-server.exe /b DSGame-BCNM.exe /b
-echo ------------------------
+echo Deleteing old EXE's...
 if exist DSGame-GM.exe del DSGame-GM.exe
-if not exist DSGame-GM.exe copy DSGame-server.exe /b DSGame-GM.exe /b
+if exist DSGame-BCNM.exe del DSGame-BCNM.exe
+if exist DSGame-DynaLimbus.exe del DSGame-DynaLimbus.exe
+if exist DSGame-Abyssea.exe del DSGame-Abyssea.exe
 echo ------------------------
-if exist DSGame-Abyssea.pdb del DSGame-Abyssea.pdb
-if not exist DSGame-Abyssea.pdb copy DSGame-server.pdb /b DSGame-Abyssea.pdb /b
-echo ------------------------
-if exist DSGame-DynaLimbus.pdb del DSGame-DynaLimbus.pdb
-if not exist DSGame-DynaLimbus.pdb copy DSGame-server.pdb /b DSGame-DynaLimbus.pdb /b
-echo ------------------------
-if exist DSGame-BCNM.pdb del DSGame-BCNM.exe
-if not exist DSGame-BCNM.pdb copy DSGame-server.exe /b DSGame-BCNM.pdb /b
-echo ------------------------
-if exist DSGame-GM.pdb del DSGame-GM.pdb
-if not exist DSGame-GM.pdb copy DSGame-server.pdb /b DSGame-GM.pdb /b
+echo Creating new EXE's...
+if not exist DSGame-GM.exe copy DSGame-server.exe /b DSGame-GM.exe /bexe /b
+if not exist DSGame-BCNM.exe copy DSGame-server.exe /b DSGame-BCNM.exe /b
+if not exist DSGame-DynaLimbus.exe copy DSGame-server.exe /b DSGame-DynaLimbus.
+if not exist DSGame-Abyssea.exe copy DSGame-server.exe /b DSGame-Abyssea.exe /b
 echo ------------------------
 echo Done.
 PAUSE
