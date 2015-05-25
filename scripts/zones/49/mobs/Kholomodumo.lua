82d888113e8f6b3d28c014ee12727a89bae5f5a6
Loki79 <keegancarr79@gmail.com>
2015-05-25 07:12:51 -0500

    typos fix

--- scripts/zones/49/mobs/Kholomodumo.lua
+++ scripts/zones/49/mobs/Kholomodumo.lua
@@ -58,14 +58,14 @@ end;
 -----------------------------------
 
 function onSpikesDamage(mob,target,damage)
-    if (mob:getLocalVat("CurseSpikes") == 1) then
+    if (mob:getLocalVar("CurseSpikes") == 1) then
         if (mob:getLocalVar("SpikesDuration") < mob:getBattleTime()) then
-            mob:setLocalVat("CurseSpikes", 0);
-            mob:setLocalVat("SpikesDuration", 0);
+            mob:setLocalVar("CurseSpikes", 0);
+            mob:setLocalVar("SpikesDuration", 0);
         else
             target:addStatusEffect(EFFECT_CURSE, 25, 0, 60);
             -- Not implemented yet -> target:addStatusEffectEx(EFFECT_CURSE_II, EFFECT_CURSE, 25, 0, 60);
-            return SUBEFFECT_ICE_SPIKES, 166, EFFECT_CURSE;
+            return SUBEFFECT_CURSE_SPIKES, 166, EFFECT_CURSE;
         end
     else
         return 0, 0, 0;
