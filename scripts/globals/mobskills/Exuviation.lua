1c9efb637c9b3c63440b84985b7c1787da9f363c
Loki79 <keegancarr79@gmail.com>
2015-06-03 15:38:57 -0500

    iLV skill removed from item mods

--- scripts/globals/mobskills/Exuviation.lua
+++ scripts/globals/mobskills/Exuviation.lua
@@ -14,15 +14,6 @@ function onMobSkillCheck(target,mob,skill)
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-    local potency = skill:getParam();
-
-    if(potency == 0) then
-        potency = 13;
-    end
-
-    potency = potency - math.random(0, potency/4);
-
     skill:setMsg(MSG_SELF_HEAL);
-
-    return MobHealMove(mob, mob:getMaxHP() * potency / 100);
+    return MobHealMove(mob, mob:getMaxHP() * 0.075);
 end;
