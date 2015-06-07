bddfcf895312b5b538509af61ff7ba2565c4b648
TeoTwawki <arawn.annwn@yandex.com>
2015-06-07 04:21:34 -0400

    Stopped Sub-zero_Smash.lua from crashing server. Its not readily apparent why this was crashing on the isBehind() check, will have to ask a DarkStar dev about it.

--- scripts/globals/mobskills/Sub-zero_Smash.lua
+++ scripts/globals/mobskills/Sub-zero_Smash.lua
@@ -4,18 +4,18 @@
 -- Range: 10' cone
 ---------------------------------------------------
 
-require("/scripts/globals/settings");
-require("/scripts/globals/status");
-require("/scripts/globals/monstertpmoves");
+require("scripts/globals/status");
+require("scripts/globals/monstertpmoves");
 
 ---------------------------------------------------
 
 function onMobSkillCheck(target,mob,skill)
-	if (target:isBehind() == true) then
-		return 0;
-	else 
-		return 1;
-	end
+    -- This was crashing...I dunno why.
+    -- if (target:isBehind()) then
+        return 0;
+    -- else
+        -- return 1;
+    -- end
 end;
 
 function onMobWeaponSkill(target, mob, skill)
@@ -23,13 +23,11 @@ function onMobWeaponSkill(target, mob, skill)
     local accmod = 1;
     local dmgmod = 2.4;
     local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_NO_EFFECT);
-
     local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_SLASH,info.hitslanded);
-
     local typeEffect = EFFECT_PARALYSIS;
 
     MobPhysicalStatusEffectMove(mob, target, skill, typeEffect, 20, 0, 120);
-
     target:delHP(dmg);
+
     return dmg;
 end;
