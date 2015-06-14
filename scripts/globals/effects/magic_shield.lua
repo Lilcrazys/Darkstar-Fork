559d5355c4b2861ec83c908d6b1900d9311b6729
TeoTwawki <arawn.annwn@yandex.com>
2015-06-13 20:47:55 -0400

    Nerf f--king exploitable blu spell

--- scripts/globals/effects/magic_shield.lua
+++ scripts/globals/effects/magic_shield.lua
@@ -13,6 +13,8 @@ require("scripts/globals/status");
 function onEffectGain(target,effect)
     if (effect:getPower() < 2) then
         target:addMod(MOD_UDMGMAGIC, -256);
+    elseif (effect:getPower() == 100)
+        target:addMod(MOD_UDMGMAGIC, -64); -- damned blue spell
     else
         target:addMod(MOD_MAGIC_ABSORB, 100);
     end
@@ -32,6 +34,8 @@ end;
 function onEffectLose(target,effect)
     if (effect:getPower() < 2) then
         target:delMod(MOD_UDMGMAGIC, -256);
+    elseif (effect:getPower() == 100)
+        target:delMod(MOD_UDMGMAGIC, -64); -- damned blue spell
     else
         target:delMod(MOD_MAGIC_ABSORB, 100);
     end
