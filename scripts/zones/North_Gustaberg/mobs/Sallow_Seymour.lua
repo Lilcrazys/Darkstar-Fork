f595b509e5ea607e6437c431c1cc3d7612cd2608
Loki79 <keegancarr79@gmail.com>
2015-06-17 19:25:04 -0500

    missed one

--- scripts/zones/North_Gustaberg/mobs/Sallow_Seymour.lua
+++ scripts/zones/North_Gustaberg/mobs/Sallow_Seymour.lua
@@ -7,6 +7,7 @@ require("scripts/globals/titles");
 require("scripts/globals/status");
 require("scripts/globals/magic");
 require("scripts/globals/utils");
+require("scripts/globals/keyitems");
 
 
 -----------------------------------
