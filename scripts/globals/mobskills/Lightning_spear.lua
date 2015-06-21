80926e8f0bcd935900e88b0120b7c2cc584fe343
TeoTwawki <arawn.annwn@yandex.com>
2015-06-21 08:38:15 -0400

    Disabled Asheron Kick till I am able to look into wtf is going on with that isBehind() function

--- scripts/globals/mobskills/Lightning_spear.lua
+++ scripts/globals/mobskills/Lightning_spear.lua
@@ -5,20 +5,19 @@
 require("scripts/globals/settings");
 require("scripts/globals/status");
 require("scripts/globals/monstertpmoves");
-
 ---------------------------------------------
+
 function onMobSkillCheck(target,mob,skill)
-	return 0;
+    return 0;
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-    local typeEffect = EFFECT_AMNESIA;
-
-    MobStatusEffectMove(mob, target, typeEffect, 10, 3, 60);
-
     local dmgmod = 1;
     local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*4,ELE_THUNDER,dmgmod,TP_NO_EFFECT);
     local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_THUNDER,MOBPARAM_WIPE_SHADOWS);
+
+    MobStatusEffectMove(mob, target, EFFECT_AMNESIA, 10, 0, 60);
     target:delHP(dmg);
+
     return dmg;
 end;
--- scripts/globals/mobskills/Rampant_stance.lua
+++ scripts/globals/mobskills/Rampant_stance.lua
@@ -5,22 +5,21 @@
 require("scripts/globals/settings");
 require("scripts/globals/status");
 require("scripts/globals/monstertpmoves");
-
 ---------------------------------------------
+
 function onMobSkillCheck(target,mob,skill)
     return 0;
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-	local numhits = 1;
-	local accmod = 1;
-	local dmgmod = 2.8;
-	local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_NO_EFFECT);
-	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_BLUNT,info.hitslanded);
+    local numhits = 1;
+    local accmod = 1;
+    local dmgmod = 2.8;
+    local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_NO_EFFECT);
+    local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_BLUNT,info.hitslanded);
 
-    local typeEffect = EFFECT_STUN;
-    MobPhysicalStatusEffectMove(mob, target, skill, typeEffect, 30, 0, 10);
+    MobPhysicalStatusEffectMove(mob, target, skill, EFFECT_STUN, 30, 0, 10);
+    target:delHP(dmg);
 
-	target:delHP(dmg);
-	return dmg;
+    return dmg;
 end;
