3bfed2709bf78a803d762af06bc3df0af77d7bd3
TeoTwawki <arawn.annwn@yandex.com>
2015-07-21 06:46:54 -0400

    Attempt to fix some VW bullshit

--- scripts/zones/Outer_Horutoto_Ruins/mobs/Voidwrought.lua
+++ scripts/zones/Outer_Horutoto_Ruins/mobs/Voidwrought.lua
@@ -1,6 +1,7 @@
 -----------------------------------
 -- Area: ?
---  VNM: Lorbulcrud
+-- VWNM: Lorbulcrud
+-- @pos ? ? ? ?
 -----------------------------------
 
 require("scripts/globals/titles");
@@ -14,12 +15,16 @@ require("scripts/globals/keyitems");
 -----------------------------------
 
 function onMobInitialize(mob)
+    -- setMobMod
     mob:setMobMod(MOBMOD_MAGIC_COOL, 45);
 
     -- addMod
     mob:addMod(MOD_MDEF,40);
     mob:addMod(MOD_DEF,80);
     mob:addMod(MOD_ATT,150);
+
+    -- other
+    mob:SetMobSkillAttack(true); -- Enable Special Animation for melee attacks.
 end;
 
 -----------------------------------
@@ -35,9 +40,8 @@ function onMobSpawn(mob)
     mob:setMod(MOD_MACC,1950);
     mob:setMod(MOD_MATT,105);
 
-    -- other
-    mob:SetMobSkillAttack(true); -- Enable Special Animation for melee attacks.
-    mob:setLocalVar("depopTime", os.time(t) + 1800);  -- despawn in 30 min
+    -- Vars
+    mob:setLocalVar("depopTimer", os.time());
 end;
 -----------------------------------
 -- onMobEngage Action
@@ -51,10 +55,10 @@ end;
 -----------------------------------
 
 function onMobFight(mob, target)
-    local depopTime = mob:getLocalVar("depopTime");
-
-    if (os.time(t) > depopTime) then
+    if (os.time() - mob:getLocalVar("depopTimer") > 1800) then
+        -- despawn in 30 min
         DespawnMob(mob:getID());
+        break;
     end
 end;
 
@@ -63,9 +67,11 @@ end;
 -----------------------------------
 
 function onMobDeath(mob, killer)
-    if (killer:hasKeyItem(JADE_STRATUM_ABYSSITE_IV)) then -- Voidwrought Kill
-        if (killer:getMaskBit(killer:getVar("VW_3_NATIONS"), 0) == false) then
-            killer:setMaskBit(killer:getVar("VW_3_NATIONS"),"VW_3_NATIONS",0,true);
+    if (player:getQuestStatus(CRYSTAL_WAR, GUARDIAN_OF_THE_VOID) == QUEST_AVAILABLE) then
+        if (killer:hasKeyItem(JADE_STRATUM_ABYSSITE_IV)) then
+            if (killer:getMaskBit(killer:getVar("VW_3_NATIONS"), 0) == false) then
+                killer:setMaskBit(killer:getVar("VW_3_NATIONS"),"VW_3_NATIONS",0,true);
+            end
         end
     end
     killer:addCurrency("bayld", 125);
