7ee76b49fa27a7e4674f2a56cf27630e46188b71
TeoTwawki <arawn.annwn@yandex.com>
2015-07-22 22:46:02 -0400

    Dupe SQL line fix in mob_skill.sql

--- sql/mob_skill.sql
+++ sql/mob_skill.sql
@@ -1767,7 +1767,6 @@ INSERT INTO `mob_skill` VALUES (1190,123,1064,'Sickle_Slash',0,7.0,2000,1500,4,0
 INSERT INTO `mob_skill` VALUES (1188,124,1062,'Vorpal_Blade',0,7.0,2000,1500,4,0,0,0); -- only used in human form
 INSERT INTO `mob_skill` VALUES (1185,124,1065,'Actinic_Burst',1,10.0,2000,1500,4,0,0,0);
 INSERT INTO `mob_skill` VALUES (438,124,432,'Invincible',0,7.0,2000,0,1,2,0,0);
-INSERT INTO `mob_skill` VALUES (1188,124,1062,'Vorpal_Blade',0,7.0,2000,1500,4,0,0,0); -- only used in human form
 
 -- Euvhi
 INSERT INTO `mob_skill` VALUES (1194,109,1067,'Viscid_Nectar',4,10.0,2000,1000,4,0,0,0);
