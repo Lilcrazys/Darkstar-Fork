5cd3efa99ff19d0d104cf02e3a7aecce7a15dbe5
TeoTwawki <arawn.annwn@yandex.com>
2015-07-28 02:23:18 -0400

    Typo

--- batch/SplitGameServer.cmd
+++ batch/SplitGameServer.cmd
@@ -10,9 +10,9 @@ if exist DSGame-DynaLimbus.exe del DSGame-DynaLimbus.exe
 if exist DSGame-Abyssea.exe del DSGame-Abyssea.exe
 echo ------------------------
 echo Creating new EXE's...
-if not exist DSGame-GM.exe copy DSGame-server.exe /b DSGame-GM.exe /bexe /b
+if not exist DSGame-GM.exe copy DSGame-server.exe /b DSGame-GM.exe /b
 if not exist DSGame-BCNM.exe copy DSGame-server.exe /b DSGame-BCNM.exe /b
-if not exist DSGame-DynaLimbus.exe copy DSGame-server.exe /b DSGame-DynaLimbus.exe
+if not exist DSGame-DynaLimbus.exe copy DSGame-server.exe /b DSGame-DynaLimbus.exe /b
 if not exist DSGame-Abyssea.exe copy DSGame-server.exe /b DSGame-Abyssea.exe /b
 echo ------------------------
 echo Done.
