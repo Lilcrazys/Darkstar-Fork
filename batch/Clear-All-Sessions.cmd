f68266ab721b97f900e34641e3d44f1e3de43b80
TeoTwawki <arawn.annwn@yandex.com>
2015-07-29 16:38:12 -0400

    New auto script to clear sessions for next time map servers get changed.

---
+++ batch/Clear-All-Sessions.cmd
@@ -0,0 +1,15 @@
+@ECHO OFF
+cd ..\..\sql
+ECHO This file must NOT be used whiel the server is live with players logged in!
+ECHO ONLY use this when server is completely down! Lobby AND all Map Server!
+ECHO ------------------------------------------------------------------
+PAUSE
+ECHO NOT RESPONSIBLE FOR ANY DAMAGE CAUSED BY IGNORING THAT!
+PAUSE
+ECHO Resetting party and session tables.
+FOR %%X IN (.\custom\SessionNuke.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (accounts_sessions.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (accounts_parties.sql) DO ECHO Importing %%X & mysql dspdb -h localhost -u root -pIr0nd00r < %%X
+ECHO Finished!
+PAUSE
+PAUSE
---
+++ sql/custom/SessionNuke.sql
@@ -0,0 +1,5 @@
+
+-- accounts_parties must be destroyed first but created last.
+DROP TABLE IF EXISTS `accounts_parties`;
+DROP TABLE IF EXISTS `accounts_sessions`;
+-- accounts_sessions must be destroyed last but created first.
