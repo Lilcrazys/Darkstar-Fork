b62eb6877ac4548f349982c381a376360ff0ddb1
Loki79 <keegancarr79@gmail.com>
2015-08-08 22:40:52 -0500

    forgot one

--- scripts/commands/legion5.lua
+++ scripts/commands/legion5.lua
@@ -23,6 +23,7 @@ function onTrigger(player)
        SpawnMob('17526852'); -- Paramount_Harpeia
        SpawnMob('17526853'); -- Paramount_Mantis
        SpawnMob('17526854'); -- Paramount_Ironclad
+       SpawnMob('17526856'); -- Paramount_Ironclad
        player:setPos(-20, 12, 68, 64, 183);
     end
 end;
