3b5b3f72a1dfb8fadd5b4653a4e2efbd15f245c7
Loki79 <keegancarr79@gmail.com>
2015-08-31 01:50:34 -0500

    yeti shit and dagger script

---
+++ scripts/zones/Beaucedine_Glacier/mobs/Umagrhk.lua
@@ -0,0 +1,93 @@
+-----------------------------------
+-- Area: Someplace
+-- VWNM: Umagrhk
+-----------------------------------
+
+require("scripts/globals/status");
+require("scripts/globals/magic");
+require("scripts/globals/utils");
+require("scripts/globals/keyitems");
+
+-----------------------------------
+-- onMobInitialize Action
+-----------------------------------
+
+function onMobInitialize(mob)
+    mob:setMobMod(MOBMOD_DRAW_IN, 2);
+end;
+
+-----------------------------------
+-- onMobSpawn Action
+-----------------------------------
+
+function onMobSpawn(mob)
+    -- setMod
+    mob:setMod(MOD_REGEN, 100);
+    mob:setMod(MOD_REGAIN, 15);
+    mob:setMod(MOD_MACC,2500);
+    mob:setMod(MOD_MATT,100);
+    mob:SetMobSkillAttack(true);
+    mob:setMod(MOD_HUMANOID_KILLER,8);
+    mob:setMod(MOD_HASTE_ABILITY, 30);
+    mob:setMod(MOD_STUNRES,70);
+    mob:setMod(MOD_PARALYZERES,30);
+    mob:setMod(MOD_TERRORRES, 10000);
+end;
+
+-----------------------------------
+-- onMobDisEngage Action
+-----------------------------------
+
+function onMobDisEngage(mob, target)
+    mob:delStatusEffect(EFFECT_RAGE);
+end;
+
+-----------------------------------
+-- onMobEngage Action
+-----------------------------------
+
+function onMobEngage(mob, target)
+end;
+
+-----------------------------------
+-- onMobWeaponSkill Action
+-----------------------------------
+
+function onMobWeaponSkill(target, mob, skill)
+end;
+
+-----------------------------------
+-- onMobFight Action
+-----------------------------------
+
+function onMobFight(mob, target)
+    local BattleTime = mob:getBattleTime();
+
+    if (BattleTime - os.time() > 3600 and mob:getLocalVar("RAGED") == 0) then
+        mob:addStatusEffectEx(EFFECT_RAGE,0,1,0,0);
+        mob:setLocalVar("RAGED", 1);
+    end
+end;
+
+-----------------------------------
+-- onMobDrawIn
+-----------------------------------
+
+function onMobDrawIn(mob, target)
+    mob:useMobAbility(2670); -- Beastruction
+end;
+
+-----------------------------------
+-- onSpellCast
+-----------------------------------
+
+function onSpellCast(caster,target,spell)
+end;
+
+
+-----------------------------------
+-- onMobDeath
+-----------------------------------
+
+function onMobDeath(mob, killer)
+end;
