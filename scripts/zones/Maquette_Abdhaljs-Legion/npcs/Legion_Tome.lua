db59b9774dc2af94a825bd2bf834561c92c6f03c
Loki79 <keegancarr79@gmail.com>
2015-09-09 12:23:41 -0500

    take 3

--- scripts/zones/Maquette_Abdhaljs-Legion/npcs/Legion_Tome.lua
+++ scripts/zones/Maquette_Abdhaljs-Legion/npcs/Legion_Tome.lua
@@ -19,7 +19,50 @@ require("scripts/zones/Maquette_Abdhaljs-Legion/TextIDs");
 -----------------------------------
 
 function onTrade(player,npc,trade)
+    local HALL = 0;
 
+    if (trade:getGil(1000000)) then
+        if (trade:hasItemQty(4104,1) and trade:getItemCount() == 1) then -- gil + fire cluster to pop legion hall An
+            SpawnMob(17526785); -- Lofty_Behemoth
+            SpawnMob(17526786); -- Lofty_Wyrm
+            SpawnMob(17526787); -- Lofty_Adamantoise
+            player:setPos(-180, 12, 212, 192, 183);
+            HALL = 1
+        elseif (trade:hasItemQty(3529,1) and trade:getItemCount() == 1) then -- gil + lofty trophy to pop legion hall Ki
+            SpawnMob(17526805); -- Mired_Cerberus
+            SpawnMob(17526806); -- Mired_Khimaira
+            SpawnMob(17526807); -- Mired_Hydra
+            player:setPos(130, 12, 220, 0, 183);
+            HALL = 2
+        elseif (trade:hasItemQty(3530,1) and trade:getItemCount() == 1) then -- gil + mired trophy to pop legion hall Im
+            SpawnMob(17526819); -- Soaring_Corse
+            SpawnMob(17526820); -- Soaring_Dvergr
+            SpawnMob(17526821); -- Soaring_Vampyr
+            player:setPos(140, 12, -132, 64, 183);
+            HALL = 3
+        elseif (trade:hasItemQty(3531,1) and trade:getItemCount() == 1) then -- gil + soaring trophy to pop legion hall Muru
+            SpawnMob(17526833); -- Veiled_Amphiptere
+            SpawnMob(17526834); -- Veiled_Ixion
+            SpawnMob(17526835); -- Veiled_Sandworm
+            player:setPos(-170, 12, -140, 128, 183);
+            HALL = 4
+        elseif (trade:hasItemQty(3532,1) and trade:getItemCount() == 1) then -- gil + veiled trophy to pop legion hall Mul
+            SpawnMob(17526851); -- Paramount_Naraka
+            SpawnMob(17526852); -- Paramount_Harpeia
+            SpawnMob(17526853); -- Paramount_Mantis
+            SpawnMob(17526854); -- Paramount_Ironclad
+            player:setPos(-20, 12, 68, 64, 183);
+            HALL = 5
+        end
+
+        if (HALL > 0) then
+            player:delGil(1000000);
+            player:PrintToPlayer( "You will now be transported to Legion" ); -- needs to be spoof saying....
+            player:tradeComplete();
+        end
+    else
+        player:PrintToPlayer( "You do not meet the requirements to launch Legion!" );
+    end
 end;
 
 -----------------------------------
