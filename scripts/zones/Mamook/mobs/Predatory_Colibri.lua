6555a3247454c55fa11e5f16810580287740efed
Loki79 <keegancarr79@gmail.com>
2015-11-15 11:46:10 -0600

    VW Chapter 5 adjustments

--- scripts/zones/Mamook/mobs/Predatory_Colibri.lua
+++ scripts/zones/Mamook/mobs/Predatory_Colibri.lua
@@ -29,9 +29,9 @@ function onMobSpawn(mob)
     -- setMod
     mob:setMod(MOD_REGAIN, 10);
     mob:setMod(MOD_REFRESH, 250);
-    mob:setMod(MOD_UFASTCAST, 55);
+    mob:setMod(MOD_UFASTCAST, 25);
     mob:setMod(MOD_MACC,1950);
-    mob:setMod(MOD_MATT,90);
+    mob:setMod(MOD_MATT,75);
     mob:setMod(MOD_DOUBLE_ATTACK,25);
 end;
 
