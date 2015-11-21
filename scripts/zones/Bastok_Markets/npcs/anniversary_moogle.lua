28b7b2a14929bc353d49ccfecdb58d965523fe8b
Loki79 <keegancarr79@gmail.com>
2015-11-21 16:31:35 -0600

    Derp Fix!  Corrected buff durations.

--- scripts/zones/Bastok_Markets/npcs/anniversary_moogle.lua
+++ scripts/zones/Bastok_Markets/npcs/anniversary_moogle.lua
@@ -13,7 +13,7 @@ require("scripts/globals/spoofchat");
 -- onTrigger Action
 -----------------------------------
 
-function onTrigger(player,npc) 
+function onTrigger(player,npc)
 
     local annibuff = math.random(1,10);
 
@@ -29,55 +29,55 @@ function onTrigger(player,npc)
 
     if (player:getVar("ANNIBUFF_TIMER") < os.time()) then
         if (annibuff == 1) then
-            player:addStatusEffect(EFFECT_FLEE,1,0,30);
+            player:addStatusEffect(EFFECT_FLEE,1,0,1800);
             player:setVar("ANNIBUFF_TIMER", os.time()+3600);
             -- add costume of something....??
         elseif (annibuff == 2) then
-            player:addStatusEffect(EFFECT_REGEN, 10,1,30);
-            player:addStatusEffect(EFFECT_REFRESH, 10,1,30);
+            player:addStatusEffect(EFFECT_REGEN, 10,1,1800);
+            player:addStatusEffect(EFFECT_REFRESH, 10,1,1800);
             player:setVar("ANNIBUFF_TIMER", os.time()+3600);
         elseif (annibuff == 3) then
-            player:addStatusEffect(EFFECT_REGAIN, 100,1,30);
+            player:addStatusEffect(EFFECT_REGAIN, 100,1,1800);
             player:setVar("ANNIBUFF_TIMER", os.time()+3600);
         elseif (annibuff == 4) then
-            player:addStatusEffect(EFFECT_MAX_HP_BOOST,40,0,30);
-            player:addStatusEffect(EFFECT_MAX_MP_BOOST,40,0,30);
+            player:addStatusEffect(EFFECT_MAX_HP_BOOST,40,0,1800);
+            player:addStatusEffect(EFFECT_MAX_MP_BOOST,40,0,1800);
             player:setVar("ANNIBUFF_TIMER", os.time()+3600);
         elseif (annibuff == 5) then
-            player:addStatusEffect(EFFECT_STR_BOOST,50,0,30);
-            player:addStatusEffect(EFFECT_DEX_BOOST,50,0,30);
-            player:addStatusEffect(EFFECT_VIT_BOOST,50,0,30);
-            player:addStatusEffect(EFFECT_AGI_BOOST,50,0,30);
-            player:addStatusEffect(EFFECT_INT_BOOST,50,0,30);
-            player:addStatusEffect(EFFECT_MND_BOOST,50,0,30);
-            player:addStatusEffect(EFFECT_CHR_BOOST,50,0,30);
+            player:addStatusEffect(EFFECT_STR_BOOST,50,0,1800);
+            player:addStatusEffect(EFFECT_DEX_BOOST,50,0,1800);
+            player:addStatusEffect(EFFECT_VIT_BOOST,50,0,1800);
+            player:addStatusEffect(EFFECT_AGI_BOOST,50,0,1800);
+            player:addStatusEffect(EFFECT_INT_BOOST,50,0,1800);
+            player:addStatusEffect(EFFECT_MND_BOOST,50,0,1800);
+            player:addStatusEffect(EFFECT_CHR_BOOST,50,0,1800);
             player:setVar("ANNIBUFF_TIMER", os.time()+3600);
         elseif (annibuff == 6) then
-            player:addStatusEffect(EFFECT_CHAINSPELL, 1,0,15);
+            player:addStatusEffect(EFFECT_CHAINSPELL, 1,0,900);
             player:setVar("ANNIBUFF_TIMER", os.time()+3600);
         elseif (annibuff == 7) then
-            player:addStatusEffect(EFFECT_HUNDRED_FISTS,1,0,15);
+            player:addStatusEffect(EFFECT_HUNDRED_FISTS,1,0,900);
             player:setVar("ANNIBUFF_TIMER", os.time()+3600);
         elseif (annibuff == 8) then
-            player:addStatusEffect(EFFECT_MIGHTY_STRIKES,1,0,15);
+            player:addStatusEffect(EFFECT_MIGHTY_STRIKES,1,0,900);
             player:setVar("ANNIBUFF_TIMER", os.time()+3600);
         elseif (annibuff == 9) then
-            player:addStatusEffect(EFFECT_MANAFONT,1,0,15);
+            player:addStatusEffect(EFFECT_MANAFONT,1,0,900);
             player:setVar("ANNIBUFF_TIMER", os.time()+3600);
         elseif (annibuff == 10) then
-            player:addStatusEffect(EFFECT_MAX_HP_BOOST,1000,0,15);
-            player:addStatusEffect(EFFECT_MAX_MP_BOOST,1000,0,15);
-            player:addStatusEffect(EFFECT_SENTINEL,100,0,15);
-            player:addStatusEffect(EFFECT_MIGHTY_STRIKES,1,0,15);
-            player:addStatusEffect(EFFECT_HUNDRED_FISTS,1,0,15);
-            player:addStatusEffect(EFFECT_CHAINSPELL,1,0,15);
-            player:addStatusEffect(EFFECT_PERFECT_DODGE,1,0,15);
-            player:addStatusEffect(EFFECT_INVINCIBLE,1,0,15);
-            player:addStatusEffect(EFFECT_MANAFONT,1,0,15);
-            player:addStatusEffect(EFFECT_REGAIN,150,1,15);
-            player:addStatusEffect(EFFECT_REFRESH,99,0,15);
-            player:addStatusEffect(EFFECT_REGEN,99,0,15);
-            player:addStatusEffectEx(EFFECT_SJCAP_BOOST,EFFECT_TRANSCENDENCY,1,0,15)
+            player:addStatusEffect(EFFECT_MAX_HP_BOOST,1000,0,900);
+            player:addStatusEffect(EFFECT_MAX_MP_BOOST,1000,0,900);
+            player:addStatusEffect(EFFECT_SENTINEL,100,0,900);
+            player:addStatusEffect(EFFECT_MIGHTY_STRIKES,1,0,900);
+            player:addStatusEffect(EFFECT_HUNDRED_FISTS,1,0,900);
+            player:addStatusEffect(EFFECT_CHAINSPELL,1,0,900);
+            player:addStatusEffect(EFFECT_PERFECT_DODGE,1,0,900);
+            player:addStatusEffect(EFFECT_INVINCIBLE,1,0,900);
+            player:addStatusEffect(EFFECT_MANAFONT,1,0,900);
+            player:addStatusEffect(EFFECT_REGAIN,150,1,900);
+            player:addStatusEffect(EFFECT_REFRESH,99,0,900);
+            player:addStatusEffect(EFFECT_REGEN,99,0,900);
+            player:addStatusEffectEx(EFFECT_SJCAP_BOOST,EFFECT_TRANSCENDENCY,1,0,900)
             player:setVar("ANNIBUFF_TIMER", os.time()+3600);
         end
     else
