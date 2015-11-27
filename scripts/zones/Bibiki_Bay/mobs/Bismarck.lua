9aff957524d3eeb7e4c217a797f720e13ee19d7b
TeoTwawki <arawn.annwn@yandex.com>
2015-11-27 14:08:46 -0500

    MY HANDS ARE TYPING WORDS

--- scripts/zones/Bibiki_Bay/mobs/Bismarck.lua
+++ scripts/zones/Bibiki_Bay/mobs/Bismarck.lua
@@ -1,11 +1,9 @@
 -----------------------------------
--- Area: ?
+-- Area: Bibiki Bay
 -- VWNM: Bismark
 -----------------------------------
 
 require("scripts/globals/status");
-require("scripts/globals/magic");
-require("scripts/globals/utils");
 require("scripts/globals/keyitems");
 require("scripts/globals/quests");
 
@@ -18,7 +16,7 @@ function onMobInitialize(mob)
     mob:setMobMod(MOBMOD_MAGIC_COOL, 45);
 
     -- addMod
-    mob:addMod(MOD_MDEF,50);
+    mob:addMod(MOD_MDEF, 50);
 end;
 
 -----------------------------------
@@ -30,9 +28,9 @@ function onMobSpawn(mob)
     mob:setMod(MOD_REGEN, 50);
     mob:setMod(MOD_REGAIN, 10);
     mob:setMod(MOD_REFRESH, 250);
-    mob:setMod(MOD_MACC,1950);
-    mob:setMod(MOD_MATT,80);
-    mob:setMod(MOD_CRITHITRATE,25);
+    mob:setMod(MOD_MACC, 1950);
+    mob:setMod(MOD_MATT, 80);
+    mob:setMod(MOD_CRITHITRATE, 25);
 
     -- Other
     mob:SetMobSkillAttack(true);
@@ -58,9 +56,14 @@ end;
 
 function onMobFight(mob, target)
 
-    local lastpet = mob:getLocalVar("pop_pet");
+    local popTimerDelay = 90; -- For easy adjustment.
+    local popTime = mob:getLocalVar("nextPetPop");
+    local rndPos = math.random(0,2); -- So they aren't all unforgettably stacked..
+    if (rndPos == 2) then
+        rdnPos = -1;
+    end
 
-    if (os.time() - lastpet > 90) then
+    if (os.time() > popTime) then
         local pet1 = GetMobAction(mob:getID()+1);
         local pet2 = GetMobAction(mob:getID()+2);
         local pet3 = GetMobAction(mob:getID()+3);
@@ -72,58 +75,60 @@ function onMobFight(mob, target)
         local pet9 = GetMobAction(mob:getID()+9);
         local pet10 = GetMobAction(mob:getID()+10);
 
-        if (pet1 == ACTION_NONE or pet1 == ACTION_SPAWN) then
-            SpawnMob(mob:getID()+1, 300):updateEnmity(target);
-            pet1:setPos(mob:getXpos(), mob:getYPos(), mob:getYPos());
-            mob:setLocalVar("pop_pet", os.time());
-        elseif (pet2 == ACTION_NONE or pet2 == ACTION_SPAWN) then
-            SpawnMob(mob:getID()+2, 300):updateEnmity(target);
-            pet2:setPos(mob:getXpos(), mob:getYPos(), mob:getYPos());
-            mob:setLocalVar("pop_pet", os.time());
-        elseif (pet3 == ACTION_NONE or pet3 == ACTION_SPAWN) then
-            SpawnMob(mob:getID()+3, 300):updateEnmity(target);
-            pet3:setPos(mob:getXpos(), mob:getYPos(), mob:getYPos());
-            mob:setLocalVar("pop_pet", os.time());
-        elseif (pet4 == ACTION_NONE or pet4 == ACTION_SPAWN) then
-            SpawnMob(mob:getID()+4, 300):updateEnmity(target);
-            pet4:setPos(mob:getXpos(), mob:getYPos(), mob:getYPos());
-            mob:setLocalVar("pop_pet", os.time());
-        elseif (pet5 == ACTION_NONE or pet5 == ACTION_SPAWN) then
-            SpawnMob(mob:getID()+5, 300):updateEnmity(target);
-            pet5:setPos(mob:getXpos(), mob:getYPos(), mob:getYPos());
-            mob:setLocalVar("pop_pet", os.time());
-        elseif (pet6 == ACTION_NONE or pet6 == ACTION_SPAWN) then
-            SpawnMob(mob:getID()+6, 300):updateEnmity(target);
-            pet6:setPos(mob:getXpos(), mob:getYPos(), mob:getYPos());
-            mob:setLocalVar("pop_pet", os.time());
-        elseif (pet7 == ACTION_NONE or pet7 == ACTION_SPAWN) then
-            SpawnMob(mob:getID()+6, 300):updateEnmity(target);
-            pet7:setPos(mob:getXpos(), mob:getYPos(), mob:getYPos());
-            mob:setLocalVar("pop_pet", os.time());
-        elseif (pet8 == ACTION_NONE or pet8 == ACTION_SPAWN) then
-            SpawnMob(mob:getID()+6, 300):updateEnmity(target);
-            pet8:setPos(mob:getXpos(), mob:getYPos(), mob:getYPos());
-            mob:setLocalVar("pop_pet", os.time());
-        elseif (pet9 == ACTION_NONE or pet9 == ACTION_SPAWN) then
-            SpawnMob(mob:getID()+6, 300):updateEnmity(target);
-            pet9:setPos(mob:getXpos(), mob:getYPos(), mob:getYPos());
-            mob:setLocalVar("pop_pet", os.time());
-        elseif (pet10 == ACTION_NONE or pet10 == ACTION_SPAWN) then
-            SpawnMob(mob:getID()+6, 300):updateEnmity(target);
-            pet10:setPos(mob:getXpos(), mob:getYPos(), mob:getYPos());
-            mob:setLocalVar("pop_pet", os.time());
+        if (pet1 == ACTION_NONE) then
+            SpawnMob(mob:getID()+1):updateEnmity(target);
+            pet1:setPos(mob:getXpos()+rndPos, mob:getYPos(), mob:getYPos()+rndPos);
+            mob:setLocalVar("nextPetPop", os.time()+popTimerDelay);
+        elseif (pet2 == ACTION_NONE) then
+            SpawnMob(mob:getID()+2):updateEnmity(target);
+            pet2:setPos(mob:getXpos()+rndPos, mob:getYPos(), mob:getYPos()+rndPos);
+            mmob:setLocalVar("nextPetPop", os.time()+popTimerDelay);
+        elseif (pet3 == ACTION_NONE) then
+            SpawnMob(mob:getID()+3):updateEnmity(target);
+            pet3:setPos(mob:getXpos()+rndPos, mob:getYPos(), mob:getYPos()+rndPos);
+            mmob:setLocalVar("nextPetPop", os.time()+popTimerDelay);
+        elseif (pet4 == ACTION_NONE) then
+            SpawnMob(mob:getID()+4):updateEnmity(target);
+            pet4:setPos(mob:getXpos()+rndPos, mob:getYPos(), mob:getYPos()+rndPos);
+            mmob:setLocalVar("nextPetPop", os.time()+popTimerDelay);
+        elseif (pet5 == ACTION_NONE) then
+            SpawnMob(mob:getID()+5):updateEnmity(target);
+            pet5:setPos(mob:getXpos()+rndPos, mob:getYPos(), mob:getYPos()+rndPos);
+            mmob:setLocalVar("nextPetPop", os.time()+popTimerDelay);
+        elseif (pet6 == ACTION_NONE) then
+            SpawnMob(mob:getID()+6):updateEnmity(target);
+            pet6:setPos(mob:getXpos()+rndPos, mob:getYPos(), mob:getYPos()+rndPos);
+            mmob:setLocalVar("nextPetPop", os.time()+popTimerDelay);
+        elseif (pet7 == ACTION_NONE) then
+            SpawnMob(mob:getID()+7):updateEnmity(target);
+            pet7:setPos(mob:getXpos()+rndPos, mob:getYPos(), mob:getYPos()+rndPos);
+            mmob:setLocalVar("nextPetPop", os.time()+popTimerDelay);
+        elseif (pet8 == ACTION_NONE) then
+            SpawnMob(mob:getID()+8):updateEnmity(target);
+            pet8:setPos(mob:getXpos()+rndPos, mob:getYPos(), mob:getYPos()+rndPos);
+            mmob:setLocalVar("nextPetPop", os.time()+popTimerDelay);
+        elseif (pet9 == ACTION_NONE) then
+            SpawnMob(mob:getID()+9):updateEnmity(target);
+            pet9:setPos(mob:getXpos()+rndPos, mob:getYPos(), mob:getYPos()+rndPos);
+            mmob:setLocalVar("nextPetPop", os.time()+popTimerDelay);
+        elseif (pet10 == ACTION_NONE) then
+            SpawnMob(mob:getID()+10):updateEnmity(target);
+            pet10:setPos(mob:getXpos()+rndPos, mob:getYPos(), mob:getYPos()+rndPos);
+            mmob:setLocalVar("nextPetPop", os.time()+popTimerDelay);
         end
     end
 end;
+
 -----------------------------------
 -- onSpellPrecast
 -----------------------------------
 
 function onSpellPrecast(mob, spell)
-    local power = math.random(75,150);
+    local power = math.random(75,150); -- Them be some strong spikes!
     mob:addStatusEffect(EFFECT_SHOCK_SPIKES,power,0,30);
     mob:getStatusEffect(EFFECT_SHOCK_SPIKES):setFlag(32);
 end;
+
 -----------------------------------
 -- onMobDeath
 -----------------------------------
