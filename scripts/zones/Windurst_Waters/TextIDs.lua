ebe9d9fe0b2dba9a6d271f794232785eea7e323a
TeoTwawki <arawn.annwn@yandex.com>
2015-11-30 19:46:37 -0500

    Client Update

--- scripts/zones/Windurst_Waters/TextIDs.lua
+++ scripts/zones/Windurst_Waters/TextIDs.lua
@@ -5,85 +5,85 @@
            ITEM_OBTAINED = 6543; -- Obtained: <<<Unknown Parameter (Type: 80) 1>>><<<Possible Special Code: 01>>><<<Possible Special Code: 05>>>
             GIL_OBTAINED = 6544; -- Obtained <<<Numeric Parameter 0>>> gil.
         KEYITEM_OBTAINED = 6546; -- Obtained key item: <<<Unknown Parameter (Type: 80) 1>>>
-           HOMEPOINT_SET = 6632; -- Home point set!
-  FISHING_MESSAGE_OFFSET = 7029; -- You can't fish here.
-         COOKING_SUPPORT = 7128; -- Your ?Multiple Choice (Parameter 1)?[fishing/woodworking/smithing/goldsmithing/clothcraft/leatherworking/bonecraft/alchemy/cooking] skills went up ...
-GUILD_TERMINATE_CONTRACT = 7142; -- You have terminated your trading contract with the ?Multiple Choice (Parameter 1)?[Fishermen's/Carpenters'/Blacksmiths'/Goldsmiths'/Weavers'/Tanners'/Boneworkers'/Alchemists'/Culinarians'] Guild and formed a new one with the ?Multiple Choice (Parameter 0)?[Fishermen's/Carpenters'/Blacksmiths'/Goldsmiths'/Weavers'/Tanners'/Boneworkers'/Alchemists'/Culinarians'] Guild.?Prompt?
-      GUILD_NEW_CONTRACT = 7150; -- You have formed a new trading contract with the ?Multiple Choice (Parameter 0)?[Fishermen's/Carpenters'/Blacksmiths'/Goldsmiths'/Weavers'/Tanners'/Boneworkers'/Alchemists'/Culinarians'] Guild.?Prompt?
-     NO_MORE_GP_ELIGIBLE = 7157; -- You are not eligible to receive guild points at this time.
-             GP_OBTAINED = 7146; -- Obtained <<<Numeric Parameter 0>>> guild points.
-      NOT_HAVE_ENOUGH_GP = 7163; -- You do not have enough guild points.
+           HOMEPOINT_SET = 6634; -- Home point set!
+  FISHING_MESSAGE_OFFSET = 7039; -- You can't fish here.
+         COOKING_SUPPORT = 7138; -- Your ?Multiple Choice (Parameter 1)?[fishing/woodworking/smithing/goldsmithing/clothcraft/leatherworking/bonecraft/alchemy/cooking] skills went up ...
+GUILD_TERMINATE_CONTRACT = 7152; -- You have terminated your trading contract with the ?Multiple Choice (Parameter 1)?[Fishermen's/Carpenters'/Blacksmiths'/Goldsmiths'/Weavers'/Tanners'/Boneworkers'/Alchemists'/Culinarians'] Guild and formed a new one with the ?Multiple Choice (Parameter 0)?[Fishermen's/Carpenters'/Blacksmiths'/Goldsmiths'/Weavers'/Tanners'/Boneworkers'/Alchemists'/Culinarians'] Guild.?Prompt?
+      GUILD_NEW_CONTRACT = 7160; -- You have formed a new trading contract with the ?Multiple Choice (Parameter 0)?[Fishermen's/Carpenters'/Blacksmiths'/Goldsmiths'/Weavers'/Tanners'/Boneworkers'/Alchemists'/Culinarians'] Guild.?Prompt?
+     NO_MORE_GP_ELIGIBLE = 7167; -- You are not eligible to receive guild points at this time.
+             GP_OBTAINED = 7156; -- Obtained <<<Numeric Parameter 0>>> guild points.
+      NOT_HAVE_ENOUGH_GP = 7173; -- You do not have enough guild points.
 
 
 -- Other
 NOTHING_OUT_OF_ORDINARY =  6557; -- There is nothing out of the ordinary here.
-      DIABOLOS_UNLOCKED = 11863; -- You are now able to summon Diabolos!
+      DIABOLOS_UNLOCKED = 11873; -- You are now able to summon Diabolos!
 
 -- Conquest System
-CONQUEST = 9187; -- You've earned conquest points!
+CONQUEST = 9197; -- You've earned conquest points!
 
 -- Mission Dialogs
-YOU_ACCEPT_THE_MISSION = 6717; -- You have accepted the mission.
+YOU_ACCEPT_THE_MISSION = 6727; -- You have accepted the mission.
 
 -- Shop Texts
-        KOPOPO_SHOP_DIALOG = 7816; -- Cooking is as much an art as music and painting are. Can your taste buds appreciate the full value of our works of art?
-  CHOMOJINJAHL_SHOP_DIALOG = 7821; -- The qualities needed to be a good cook are strong arms, a sense of taste, and devotion.
+        KOPOPO_SHOP_DIALOG = 7826; -- Cooking is as much an art as music and painting are. Can your taste buds appreciate the full value of our works of art?
+  CHOMOJINJAHL_SHOP_DIALOG = 7831; -- The qualities needed to be a good cook are strong arms, a sense of taste, and devotion.
 
-        ENSASA_SHOP_DIALOG = 8888; -- Welcome to my little catalyst shop, where you'll find a range of general and unusual goods!
-   UPIHKHACHLA_SHOP_DIALOG = 8889; -- For adventurerrrs on the go, Ensasa's Catalyst Shop is the place for all you need in generrral goods!
- SHOHRUNTUHRUN_SHOP_DIALOG = 8891; -- Oh, hidey-widey! This is the Federal Magic Reservey-wervey. What can I do for you today-oway?
-  HIKOMUMAKIMU_SHOP_DIALOG = 8892; -- Welcome to the Federal Magic Reserve, the only place in the Federation where high-level magic is allowed to be sold.
-     OREZEBREZ_SHOP_DIALOG = 8893; -- Welcome to Baren-Moren's, makers of the finest headwear. Our slogan is: he smarter the hat, the smarter the head.
-       TAAJIJI_SHOP_DIALOG = 8895; -- May I take your order, please...
-NESSRUGETOMALL_SHOP_DIALOG = 11434; -- Welcome to the Rarab Tail Hostelry.
+        ENSASA_SHOP_DIALOG = 8898; -- Welcome to my little catalyst shop, where you'll find a range of general and unusual goods!
+   UPIHKHACHLA_SHOP_DIALOG = 8899; -- For adventurerrrs on the go, Ensasa's Catalyst Shop is the place for all you need in generrral goods!
+ SHOHRUNTUHRUN_SHOP_DIALOG = 8901; -- Oh, hidey-widey! This is the Federal Magic Reservey-wervey. What can I do for you today-oway?
+  HIKOMUMAKIMU_SHOP_DIALOG = 8902; -- Welcome to the Federal Magic Reserve, the only place in the Federation where high-level magic is allowed to be sold.
+     OREZEBREZ_SHOP_DIALOG = 8903; -- Welcome to Baren-Moren's, makers of the finest headwear. Our slogan is: he smarter the hat, the smarter the head.
+       TAAJIJI_SHOP_DIALOG = 8905; -- May I take your order, please...
+NESSRUGETOMALL_SHOP_DIALOG = 11444; -- Welcome to the Rarab Tail Hostelry.
 
-    MAQUMOLPIH_OPEN_DIALOG = 8896; -- Psst... Check out these things my suppliers in Aragoneu dug up.
-  MAQUMOLPIH_CLOSED_DIALOG = 8897; -- Sorrrry, but I'm waiting on my next shipment from Aragoneu, so I'm all out of things to sell you at the moment.
-    BAEHUFAEHU_OPEN_DIALOG = 8898; -- Can I interest you in some of Sarutabaruta's wares? Come on, have a look, and see how I fares!
-  BAEHUFAEHU_CLOSED_DIALOG = 8899; -- Sorry-dorry, but I'm taking a breaky-wakey! (Or, as you'll be knowing,  since control of Sarutabaruta was lost, I'm out of stock, so go on, get going!)
+    MAQUMOLPIH_OPEN_DIALOG = 8906; -- Psst... Check out these things my suppliers in Aragoneu dug up.
+  MAQUMOLPIH_CLOSED_DIALOG = 8907; -- Sorrrry, but I'm waiting on my next shipment from Aragoneu, so I'm all out of things to sell you at the moment.
+    BAEHUFAEHU_OPEN_DIALOG = 8908; -- Can I interest you in some of Sarutabaruta's wares? Come on, have a look, and see how I fares!
+  BAEHUFAEHU_CLOSED_DIALOG = 8909; -- Sorry-dorry, but I'm taking a breaky-wakey! (Or, as you'll be knowing,  since control of Sarutabaruta was lost, I'm out of stock, so go on, get going!)
 
-      AHYEEKIH_OPEN_DIALOG = 8900; -- Psst... Wanna buy somethin' cheap from Kolshushu?
-    AHYEEKIH_CLOSED_DIALOG = 8901; -- Hee-hee-hee... Can you hang on a while? I can start selling you good stuff from Kolshushu once I'm ready.
+      AHYEEKIH_OPEN_DIALOG = 8910; -- Psst... Wanna buy somethin' cheap from Kolshushu?
+    AHYEEKIH_CLOSED_DIALOG = 8911; -- Hee-hee-hee... Can you hang on a while? I can start selling you good stuff from Kolshushu once I'm ready.
 
-        FOMINA_OPEN_DIALOG = 8902; -- Hello, adventurer! Can I interest you in something a little different--something from Elshimo?
-      FOMINA_CLOSED_DIALOG = 8903; -- Well, um, let me see... This should be a good spot to open shop. There are some wealthy-looking Tarutaru houses nearby. It's quiet and yet there're plenty of passers-by...
+        FOMINA_OPEN_DIALOG = 8912; -- Hello, adventurer! Can I interest you in something a little different--something from Elshimo?
+      FOMINA_CLOSED_DIALOG = 8913; -- Well, um, let me see... This should be a good spot to open shop. There are some wealthy-looking Tarutaru houses nearby. It's quiet and yet there're plenty of passers-by...
 
-         OTETE_OPEN_DIALOG = 8904; -- He-he-he... Hey! How's about... Items from Li'Telor that you can't do without? Reckon you could do, with one of these or two?
-       OTETE_CLOSED_DIALOG = 8905; -- Oh... Phew... My heart is so blue... Bluer than these flowers... Leave me be for a couple hours...
+         OTETE_OPEN_DIALOG = 8914; -- He-he-he... Hey! How's about... Items from Li'Telor that you can't do without? Reckon you could do, with one of these or two?
+       OTETE_CLOSED_DIALOG = 8915; -- Oh... Phew... My heart is so blue... Bluer than these flowers... Leave me be for a couple hours...
 
-      JOURILLE_OPEN_DIALOG = 8906; -- Greetings.  Can I interest you in some of these goods from Ronfaure...?
-    JOURILLE_CLOSED_DIALOG = 8907; -- Greetings! I am Jourille, your friendly neighborhood traveling merchant. I would most like to sell you something from Ronfaure right now, but I regret that I am waiting on my next shipment.
+      JOURILLE_OPEN_DIALOG = 8916; -- Greetings.  Can I interest you in some of these goods from Ronfaure...?
+    JOURILLE_CLOSED_DIALOG = 8917; -- Greetings! I am Jourille, your friendly neighborhood traveling merchant. I would most like to sell you something from Ronfaure right now, but I regret that I am waiting on my next shipment.
 
-   PRESTAPIQ_CLOSED_DIALOG = 10629; -- Goodebyongo! Wingdorsht tooo fhar awayz fhrum mai hormtowne! Dropt arll goodhys whylle ahn trripp!
-     PRESTAPIQ_OPEN_DIALOG = 10630; -- Helgohelgo! Me's bhrink goodhys arll ja wayz fhrum hormtowne ovf Morvalporlis!
+   PRESTAPIQ_CLOSED_DIALOG = 10639; -- Goodebyongo! Wingdorsht tooo fhar awayz fhrum mai hormtowne! Dropt arll goodhys whylle ahn trripp!
+     PRESTAPIQ_OPEN_DIALOG = 10640; -- Helgohelgo! Me's bhrink goodhys arll ja wayz fhrum hormtowne ovf Morvalporlis!
 
 -- Harvest Festival
-  TRICK_OR_TREAT = 10136; -- Trick or treat...
- THANK_YOU_TREAT = 10137; -- And now for your treat...
-  HERE_TAKE_THIS = 10138; -- Here, take this...
-IF_YOU_WEAR_THIS = 10139; -- If you put this on and walk around, something...unexpected might happen...
-       THANK_YOU = 10137; -- Thank you...
+  TRICK_OR_TREAT = 10146; -- Trick or treat...
+ THANK_YOU_TREAT = 10147; -- And now for your treat...
+  HERE_TAKE_THIS = 10148; -- Here, take this...
+IF_YOU_WEAR_THIS = 10149; -- If you put this on and walk around, something...unexpected might happen...
+       THANK_YOU = 10147; -- Thank you...
 
 -- conquest Base
 CONQUEST_BASE = 0;
 
 
 -- Voidwatch (NOTE: this is not a retail implementation!) These will need recheck/correction every time there is a client update.
-I_AM_A_VOIDWATCH_OFFICER = 16449; -- I am a Voidwatch officer dispatched here by the
-           I_BESEECH_YOU = 16450; -- I beseech you, friend. Lend your aid to our cause, lest our fair Vana'diel be engulfed in fire and brimstone.
- COMMEND_YOUR_ENTHUSIASM = 16452; -- While I commend your enthusiasm, only those who possess
-     PROOF_OF_ONES_VALOR = 16453; -- is considered proof of one's valor and prowess, and is awarded only to those who have distinguished themselves in the three nations.
-                ENLISTED = 16454; -- Enlisted in [San d'Oria/Bastok/Windurst]'s armed forces, you say?
-    MILITARY_AFFILIATION = 16455; -- I'm sorry, but only those with military affiliation may participate in Voidwatch Ops.
-ENTHUSIASM_IS_HEARTENING = 16456; -- While your enthusiasm is heartening, the Voidwatch requires men and women of tested steel.
-             AT_LEAST_75 = 16457; -- You must be at least level
-        STRATUM_ABYSSITE = 16460; -- Stratum abyssite is needed to do battle with a Voidwalker at one of the many planar rifts that have manifested across the land.
-   EACH_STONE_IS_ATTUNED = 16461; -- You can't simply use one wherever you like, mind you, for each stone is attuned only to rifts found in a certain region, or “jurisdiction.”
-     VOIDWATCH_OPERATION = 16462; -- Undertaking a Voidwatch operation in a specific jurisdiction requires that you obtain the corresponding stone from the presiding Voidwatch officer.
-THESE_STONES_ARE_CAPABLE = 16463; -- While these stones are capable of rendering Voidwalkers visible to their bearers, their powers can only be tapped when used in conjunction with
-             OPERATIONS1 = 16464; -- Operations are presently underway at [East Ronfaure/East Ronfaure/Ordelle's Caves/the Jugner Forest/the Jugner Forest/King Ranperre's Tomb/West Ronfaure/La Theine Plateau/Vunkerl Inlet]. While planar rifts have been reported in other areas, we've yet to discover how to engage Voidwalkers there.
-             OPERATIONS2 = 16465; -- Operations are presently underway at [North Gustaberg/North Gustaberg/the Gusgen Mines/the Pashhow Marshlands/the Pashhow Marshlands/Dangruf Wadi/South Gustaberg/the Konschtat Highlands/Grauberg]. While planar rifts have been reported in other areas, we've yet to discover how to engage Voidwalkers there.
-             OPERATIONS3 = 16466; -- Operations are presently underway at [West Sarutabaruta/West Sarutabaruta/the Maze of Shakhrami/the Meriphataud Mountains/the Meriphataud Mountains/the Outer Horutoto Ruins/East Sarutabaruta/Tahrongi Canyon/Fort Karugo-Narugo]. While planar rifts have been reported in other areas, we've yet to discover how to engage Voidwalkers there.
-                REQUEST1 = 16469; -- At this time, we request your participation in the operation at [East Ronfaure/East Ronfaure/Ordelle's Caves/the Jugner Forest/the Jugner Forest/King Ranperre's Tomb/West Ronfaure/La Theine Plateau/Vunkerl Inlet].
-                REQUEST2 = 16470; -- At this time, we request your participation in the operation at [North Gustaberg/North Gustaberg/the Gusgen Mines/the Pashhow Marshlands/the Pashhow Marshlands/Dangruf Wadi/South Gustaberg/the Konschtat Highlands/Grauberg].
-                REQUEST3 = 16471; -- At this time, we request your participation in the operation at [West Sarutabaruta/West Sarutabaruta/the Maze of Shakhrami/the Meriphataud Mountains/the Meriphataud Mountains/the Outer Horutoto Ruins/East Sarutabaruta/Tahrongi Canyon/Fort Karugo-Narugo].
+I_AM_A_VOIDWATCH_OFFICER = 16459; -- I am a Voidwatch officer dispatched here by the
+           I_BESEECH_YOU = 16460; -- I beseech you, friend. Lend your aid to our cause, lest our fair Vana'diel be engulfed in fire and brimstone.
+ COMMEND_YOUR_ENTHUSIASM = 16462; -- While I commend your enthusiasm, only those who possess
+     PROOF_OF_ONES_VALOR = 16463; -- is considered proof of one's valor and prowess, and is awarded only to those who have distinguished themselves in the three nations.
+                ENLISTED = 16464; -- Enlisted in [San d'Oria/Bastok/Windurst]'s armed forces, you say?
+    MILITARY_AFFILIATION = 16465; -- I'm sorry, but only those with military affiliation may participate in Voidwatch Ops.
+ENTHUSIASM_IS_HEARTENING = 16466; -- While your enthusiasm is heartening, the Voidwatch requires men and women of tested steel.
+             AT_LEAST_75 = 16467; -- You must be at least level
+        STRATUM_ABYSSITE = 16470; -- Stratum abyssite is needed to do battle with a Voidwalker at one of the many planar rifts that have manifested across the land.
+   EACH_STONE_IS_ATTUNED = 16471; -- You can't simply use one wherever you like, mind you, for each stone is attuned only to rifts found in a certain region, or “jurisdiction.”
+     VOIDWATCH_OPERATION = 16472; -- Undertaking a Voidwatch operation in a specific jurisdiction requires that you obtain the corresponding stone from the presiding Voidwatch officer.
+THESE_STONES_ARE_CAPABLE = 16473; -- While these stones are capable of rendering Voidwalkers visible to their bearers, their powers can only be tapped when used in conjunction with
+             OPERATIONS1 = 16474; -- Operations are presently underway at [East Ronfaure/East Ronfaure/Ordelle's Caves/the Jugner Forest/the Jugner Forest/King Ranperre's Tomb/West Ronfaure/La Theine Plateau/Vunkerl Inlet]. While planar rifts have been reported in other areas, we've yet to discover how to engage Voidwalkers there.
+             OPERATIONS2 = 16475; -- Operations are presently underway at [North Gustaberg/North Gustaberg/the Gusgen Mines/the Pashhow Marshlands/the Pashhow Marshlands/Dangruf Wadi/South Gustaberg/the Konschtat Highlands/Grauberg]. While planar rifts have been reported in other areas, we've yet to discover how to engage Voidwalkers there.
+             OPERATIONS3 = 16476; -- Operations are presently underway at [West Sarutabaruta/West Sarutabaruta/the Maze of Shakhrami/the Meriphataud Mountains/the Meriphataud Mountains/the Outer Horutoto Ruins/East Sarutabaruta/Tahrongi Canyon/Fort Karugo-Narugo]. While planar rifts have been reported in other areas, we've yet to discover how to engage Voidwalkers there.
+                REQUEST1 = 16479; -- At this time, we request your participation in the operation at [East Ronfaure/East Ronfaure/Ordelle's Caves/the Jugner Forest/the Jugner Forest/King Ranperre's Tomb/West Ronfaure/La Theine Plateau/Vunkerl Inlet].
+                REQUEST2 = 16480; -- At this time, we request your participation in the operation at [North Gustaberg/North Gustaberg/the Gusgen Mines/the Pashhow Marshlands/the Pashhow Marshlands/Dangruf Wadi/South Gustaberg/the Konschtat Highlands/Grauberg].
+                REQUEST3 = 16481; -- At this time, we request your participation in the operation at [West Sarutabaruta/West Sarutabaruta/the Maze of Shakhrami/the Meriphataud Mountains/the Meriphataud Mountains/the Outer Horutoto Ruins/East Sarutabaruta/Tahrongi Canyon/Fort Karugo-Narugo].
