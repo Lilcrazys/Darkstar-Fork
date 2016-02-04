73446a122d8e91cc962403af1672a9b29c585941
TeoTwawki <arawn.annwn@yandex.com>
2016-02-04 00:32:15 -0500

    Add mobskill list IDs in mob_pools_custom.sql, add new file mob_family_system_custom.sql ..

---
+++ sql/custom/mob_family_system_custom.sql
@@ -0,0 +1,9 @@
+-- ---------------------------------------------------------------------------
+-- This file adds onto the table "mob_family_system" created
+-- by the file mob_droplist.sql and must be imported/executed AFTER that file.
+-- ---------------------------------------------------------------------------
+
+-- INSERT INTO `mob_family_system` VALUES (familyID,familyName,systemID,systemName,melee-size,speed,HP,MP,STR,DEX,VIT,AGI,INT,MND,CHR,ATT,DEF,ACC,EVA,Slash,Pierce,H2H,Impact,Fire,Ice,Wind,Earth,Lightning,Water,Light,Dark,Element);
+-- New info below this line Sorted by familyID starting at 4000.
+
+INSERT INTO `mob_family_system` VALUES (4000,'Tome',18,'Unclassified',0,40,100,120,3,3,3,3,3,3,3,3,3,3,3,1,1,1,1,1,1,1,1,1,1,1,1,0);
