ac0bdc93456617558b9522d5feaddee0bc4cf036
TeoTwawki <arawn.annwn@yandex.com>
2016-03-04 14:32:32 -0500

    Adjust setmobstats command for test purposes

--- scripts/commands/setmobstats.lua
+++ scripts/commands/setmobstats.lua
@@ -1,24 +1,30 @@
 ---------------------------------------------------------------------------------------------------
--- func: @setmobstats <PERCENTAGE>
+-- func: @setmobstats <stat> <percentage>
 -- desc: Sets a players tp.
 ---------------------------------------------------------------------------------------------------
 
 cmdprops =
 {
     permission = 3,
-    parameters = "i"
+    parameters = "si"
 };
 
-function onTrigger(player, percent)
+function onTrigger(player, param, percent)
     local targ = player:getCursorTarget();
     if (targ == nil) then
         player:PrintToPlayer("Must select a monster using in game cursor first.");
         return;
     end
 
+    if (param == nil) then
+        player:PrintToPlayer("Must specify which to adjust: HP, MP, or TP.");
+        player:PrintToPlayer("@setmobstats <stat> <percentage>");
+        return;
+    end
+
     if (percent == nil) then
         player:PrintToPlayer("Must specify the percentage to set mobs hp/mp/tp to.");
-        player:PrintToPlayer("@setmobstats <PERCENTAGE>");
+        player:PrintToPlayer("@setmobstats <stat> <percentage>");
         return;
     end
 
@@ -31,7 +37,14 @@ function onTrigger(player, percent)
         percent == 1;
     end
 
-    targ:setHP(percent*(targ:getMaxHP()/100));
-    targ:setMP(percent*(targ:getMaxHP()/100));
-    targ:setTP(percent);
+    if (param == "hp" or param == "HP") then
+        targ:setHP(percent*(targ:getMaxHP()/100));
+    elseif (param == "mp" or param == "MP") then
+        targ:setMP(percent*(targ:getMaxHP()/100));
+    elseif (param == "tp" or param == "TP") then
+        targ:setTP(percent);
+    else
+        player:PrintToPlayer("Invalid stat specified!");
+        player:PrintToPlayer("@setmobstats <stat> <percentage>");
+    end
 end;
