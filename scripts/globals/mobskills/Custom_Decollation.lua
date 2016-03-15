9f0eaaa807900996b0bd51aba2a04061ed415919
TeoTwawki <arawn.annwn@yandex.com>
2016-03-14 22:38:36 -0400

    Fix error in mobskill script for Decollation causing a stack overflow.

--- scripts/globals/mobskills/Custom_Decollation.lua
+++ scripts/globals/mobskills/Custom_Decollation.lua
@@ -1,32 +1,24 @@
 ---------------------------------------------
---  Oblivion Smash
+-- Decollation
 --
---  Description: Deals damage to players within area of effect and inflicts blind, silence, bind, and weight.
---  Type: Physical
---  Utsusemi/Blink absorb:  2-3 shadows
---  Range: Unknown radial
---  Notes:
 ---------------------------------------------
-
+require("scripts/globals/monstertpmoves");
 require("scripts/globals/settings");
 require("scripts/globals/status");
-require("scripts/globals/monstertpmoves");
 ---------------------------------------------
 
-function onMobSkillCheck(target,mob,skill) onMobSkillCheck(target,mob,skill)
-	return 0;
+function onMobSkillCheck(target,mob,skill)
+    return 0;
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-	
-	local numhits = 3;
-	local accmod = 1;
-	local dmgmod = 2;
-	
-	local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_DMG_VARIES,1,2,3);
-	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_SLASH,info.hitslanded);
+    local numhits = 3;
+    local accmod = 1;
+    local dmgmod = 2;
 
-	target:delHP(dmg);
-	return dmg;
-	
+    local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_DMG_VARIES,1,2,3);
+    local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_SLASH,info.hitslanded);
+
+    target:delHP(dmg);
+    return dmg;
 end;
