84448527cd3cb0793aba2410be2d9dcd9a1dd754
TeoTwawki <arawn.annwn@yandex.com>
2016-03-16 02:04:53 -0400

    Move Tyrant Tusk to custom script.

--- scripts/globals/mobskills/Custom_Tyrant_Tusk.lua
+++ scripts/globals/mobskills/Custom_Tyrant_Tusk.lua
@@ -1,38 +1,59 @@
 ---------------------------------------------
---  Throat Stab
+-- Tyrant Tusk (Custom Version)
 --
---  Description: Deals damage to a single target reducing their HP to 5%. Resets enmity.
---  Type: Physical
---  Utsusemi/Blink absorb: No
---  Range: Single Target
---  Notes: Very short range, easily evaded by walking away from it.
+-- Damage and Bio. Additional effect Death if Bio
+-- is already on or if it knocks player into low HP. 
+--
+-- Those struck by this attack that would otherwise be hit to 
+-- below half of their maximum HP will be instantly KO'd instead
+-- of receiving any damage. For instance, a player at 1500/2000 HP
+-- that is hit by a 600-damage Tyrant Tusk would be KO'd by this 
+-- attack because their HP would have fallen below 1000. Equipment/atma with
+-- the "Resist Death" attribute is not shown to have any effect on this attack.
 ---------------------------------------------
+require("scripts/globals/monstertpmoves");
 require("scripts/globals/settings");
 require("scripts/globals/status");
-require("scripts/globals/monstertpmoves");
 require("scripts/globals/magic");
-
 ---------------------------------------------
+
 function onMobSkillCheck(target,mob,skill)
     return 0;
 end;
 
 function onMobWeaponSkill(target, mob, skill)
+    if (mob:getPool() == 4087) then -- Taweret, (other custom NM bugards using this need added to the check)
+        -- crazy Loki code here, with some Teo fixes to prevent nil values.
+        local currentHP = target:getHP();
+        local dmg = 0;
+        -- if have more hp then 25%, then reduce to 5%
+        if (currentHP / target:getMaxHP() > 0.25) then
+            dmg = currentHP * .95;
+            dmg = MobFinalAdjustments(dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_PIERCE,MOBPARAM_IGNORE_SHADOWS);
+        else
+            -- else you die
+            dmg = currentHP;
+        end
 
-    local currentHP = target:getHP();
-    -- remove all by 5%
-    local damage = 0;
+        mob:resetEnmity(target);
+        target:delHP(dmg);
 
-    -- if have more hp then 30%, then reduce to 5%
-    if (currentHP / target:getMaxHP() > 0.2) then
-        damage = currentHP * .95;
+        return dmg;
     else
-        -- else you die
-        damage = currentHP;
-    end
-    local dmg = MobFinalAdjustments(damage,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_PIERCE,MOBPARAM_IGNORE_SHADOWS);
+        -- DSP version doesn't exist yet, but this is retail(ish)
+        local dmgmod = 2;
+        local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*2,ELE_DARK,dmgmod,TP_MAB_BONUS,1);
+        local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_WIND,MOBPARAM_IGNORE_SHADOWS);
 
-    target:delHP(dmg);
-    mob:resetEnmity(target);
-    return dmg;
+        target:delHP(dmg);
+
+        if (target:getHPP() < 50 or target:hasStatsuEffect(EFFECT_BIO)) then
+            -- If damage takes you below 50% hp or you have Bio status, you die
+            skill:setMsg(20); 
+            target:setHP(0);
+            return 0;
+        else
+            return dmg;
+        end
+    end
 end;
