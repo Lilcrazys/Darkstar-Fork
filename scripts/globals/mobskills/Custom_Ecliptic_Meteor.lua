d3a3f79dfa37546d8c4071d95b1f97972a7c3a38
TeoTwawki <arawn.annwn@yandex.com>
2016-03-18 16:19:57 -0400

    Disable broken "Ecliptic Meteor" mobskill...

--- scripts/globals/mobskills/Custom_Ecliptic_Meteor.lua
+++ scripts/globals/mobskills/Custom_Ecliptic_Meteor.lua
@@ -1,27 +1,28 @@
 ---------------------------------------------
---  Ecliptic Meteor
+-- Ecliptic Meteor
 --
---  Description: Hardcore non-elemental damage
---  Type: Magical
---
+-- Description: Hardcore non-elemental damage
+-- Type: Magical
 --
 ---------------------------------------------
+require("scripts/globals/monstertpmoves");
 require("scripts/globals/settings");
 require("scripts/globals/status");
-require("scripts/globals/monstertpmoves");
 ---------------------------------------------
 
 function onMobSkillCheck(target,mob,skill)
-    return 0;
+    return 1;
 end;
 
 function onMobWeaponSkill(target, mob, skill)
 
+    --[[ broken code WHATS WRONG WITH YOU
     local dmgmod = 1;
     local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*6,-1,dmgmod,TP_NO_EFFECT);
     local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_DARK,MOBPARAM_IGNORE_SHADOWS);
     target:delHP(dmg);
-	local typeEffect = EFFECT_BIO
-	local typeEffect = EFFECT_POISON	
+    local typeEffect = EFFECT_BIO
+    local typeEffect = EFFECT_POISON
     return dmg;
+    ]]
 end;
--- scripts/globals/mobskills/Custom_Jump.lua
+++ scripts/globals/mobskills/Custom_Jump.lua
@@ -1,6 +1,6 @@
 ---------------------------------------------------
 -- Jump
--- Jumping Attack, only used by dragoo orcs.
+-- Jumping Attack, only used by dragoons.
 -- 100% TP: ??? / 200% TP: ??? / 300% TP: ???
 ---------------------------------------------------
 
@@ -11,6 +11,10 @@ require("scripts/globals/monstertpmoves");
 ---------------------------------------------------
 
 function onMobSkillCheck(target,mob,skill)
+    if (mob:getPool() == 9665) then -- The Jumping Crab
+        skill:setSkillAnimation(431);
+    end
+
     return 0;
 end;
 
@@ -20,11 +24,34 @@ function onMobWeaponSkill(target, mob, skill)
     local dmgmod = 2;
     local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_NO_EFFECT);
     local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_SLASH,info.hitslanded);
-    target:delHP(dmg);
 
-    if (dmg > 0) then
-        mob:addTP(10);
+    if (mob:getPool() == 9665) then -- The Jumping Crab
+        if (skill:getID() == IDHERE) then
+            numhits = 1;
+            accmod = 2;
+            dmgmod = 2.4;
+            info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_NO_EFFECT);
+            dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_SLASH,info.hitslanded);
+
+            if (dmg > 0) then
+                if (mob:hasStatusEffect(EFFECT_SPIRIT_SURGE)) then
+                    mob:resetEnmity(target);
+                    mob:addTP(3);
+                end
+                mob:addTP(12);
+            end
+
+            target:delHP(dmg);
+            return dmg;
+        end
+    else
+        -- DSP jump code
+        target:delHP(dmg);
+
+        if (dmg > 0) then
+            mob:addTP(10);
+        end
+
+        return dmg;
     end
-
-    return dmg;
 end;
