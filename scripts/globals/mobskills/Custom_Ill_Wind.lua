962f59ac8578869ff50d3c756bd2aa4deecfd79c
TeoTwawki <arawn.annwn@yandex.com>
2016-03-30 09:46:09 -0400

    Fix Custom_Ill_Wind.lua and delete Custom_VW_Ill_Wind.lua
    This also reverts commit f1c62117dbb39341479952e485e496c6403e1ee5 which was wrong in so many ways

--- scripts/globals/mobskills/Custom_Ill_Wind.lua
+++ scripts/globals/mobskills/Custom_Ill_Wind.lua
@@ -1,65 +1,62 @@
 ---------------------------------------------
---	Ill Wind
---	Description: Deals Wind damage to enemies within an area of effect. Additional effect: Dispel
---	Type: Magical
---	Utsusemi/Blink absorb: Wipes Shadows
---	Range: Unknown radial
---	Notes: Only used by Puks in Mamook, Besieged, and the following Notorious Monsters: Vulpangue, Nis Puk, Nguruvilu, Seps , Phantom Puk and Waugyl. Dispels one effect.
+-- Ill Wind
+-- Description: Deals Wind damage to enemies within an area of effect. Additional effect: Dispel
+-- Type: Magical
+-- Utsusemi/Blink absorb: Wipes Shadows
+-- Range: Unknown radial
+-- Notes: Only used by Puks in Mamook, Besieged, and the following Notorious Monsters: Vulpangue, Nis Puk, Nguruvilu, Seps , Phantom Puk and Waugyl. Dispels one effect.
 ---------------------------------------------
-
+require("scripts/globals/monstertpmoves");
 require("scripts/globals/settings");
 require("scripts/globals/status");
-require("scripts/globals/monstertpmoves");
-
 ---------------------------------------------
+
 function onMobSkillCheck(target,mob,skill)
-	-- This will need revisited if/when DSP ever implements this.
-	local result = 1;
-
-	if (mob:getHPP() <= 50) then
-		result = 0;
-	end
-
-	return result;
+    if (mob:getPool() == 4265 or mob:getPool() == 9802) then
+        -- Legion custom mobs
+        return 0;
+    else
+        -- DSP mobs
+        if (mob:getFamily() == 316 and mob:getModelId() == 1746) then
+            return 0;
+        else
+            return 1;
+        end
+    end
 end;
 
 function onMobWeaponSkill(target, mob, skill)
+    if (mob:getPool() == 9802) then
+        -- Sarimanok
+        local dmgmod = 2;
+        local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*5,ELE_WIND,dmgmod,TP_NO_EFFECT);
+        local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_WIND,MOBPARAM_WIPE_SHADOWS);
 
-	if (mob:getPool() == 4066 then -- Sarimanok
-	
-	local dmgmod = 1;
-	local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*2.5,ELE_WIND,dmgmod,TP_NO_EFFECT);
-	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_WIND,MOBPARAM_WIPE_SHADOWS);
+        target:dispelStatusEffect();
+        target:delHP(dmg);
 
-	target:dispelStatusEffect();
-	target:delHP(dmg);
+        return dmg;
+    elseif (mob:getPool() == 4265) then
+        -- Vulpangue
+        local dmgmod = 1;
+        local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*2.5,ELE_WIND,dmgmod,TP_NO_EFFECT);
+        local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_WIND,MOBPARAM_WIPE_SHADOWS);
 
-	--printf("[TP MOVE] Zone: %u Monster: %u Mob lvl: %u TP: %u TP Move: %u Damage: %u on Player: %u Level: %u HP: %u",mob:getZoneID(),mob:getID(),mob:getMainLvl(),skill:getTP(),skill:getID(),dmg,target:getID(),target:getMainLvl(),target:getMaxHP());
+        target:dispelStatusEffect();
+        target:delHP(dmg);
 
-	return dmg;
-	else
+        return dmg;
+    else
+        -- DSP code here exactly as DSP has it, minus the tabs.
+        local dmgmod = 1;
+        local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*2.5,ELE_WIND,dmgmod,TP_NO_EFFECT);
+        local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_WIND,MOBPARAM_WIPE_SHADOWS);
 
-function onMobSkillCheck(target,mob,skill)
-	if (mob:getFamily() == 316 and mob:getModelId() == 1746) then
-		return 0;
-	else
-		return 1;
-	end
-end;
+        target:dispelStatusEffect();
+        target:delHP(dmg);
 
-function onMobWeaponSkill(target, mob, skill)
-	local dmgmod = 1;
-	local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*2.5,ELE_WIND,dmgmod,TP_NO_EFFECT);
-	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_WIND,MOBPARAM_WIPE_SHADOWS);
+        --printf("[TP MOVE] Zone: %u Monster: %u Mob lvl: %u TP: %u TP Move: %u Damage: %u on Player: %u Level: %u HP: %u",mob:getZoneID(),mob:getID(),mob:getMainLvl(),skill:getTP(),skill:getID(),dmg,target:getID(),target:getMainLvl(),target:getMaxHP());
 
-	target:dispelStatusEffect();
-	target:delHP(dmg);
-
-	--printf("[TP MOVE] Zone: %u Monster: %u Mob lvl: %u TP: %u TP Move: %u Damage: %u on Player: %u Level: %u HP: %u",mob:getZoneID(),mob:getID(),mob:getMainLvl(),skill:getTP(),skill:getID(),dmg,target:getID(),target:getMainLvl(),target:getMaxHP());
-
-	return dmg;
-		
-		
-		return 0;
-	end	   
+        return dmg;
+    end
 end;
