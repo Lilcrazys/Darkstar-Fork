54aadf3f121a9900ddd0657cee82eca0af14d615
TeoTwawki <arawn.annwn@yandex.com>
2016-04-23 21:44:09 -0400

    YOU CANNOT DECLARE THE SAME VARIABLE TWICE! NOR THRICE! NOR CAN YOU ADD A TICK VALUE TO EFFECTS THAT DON'T ACTUALLY HAVE ONE! NOR CAN YOU USE MobStatusEffectMove IN PLACE OF MobBuffMove FFS!!

--- scripts/globals/mobskills/Custom_Carcharian_Verve.lua
+++ scripts/globals/mobskills/Custom_Carcharian_Verve.lua
@@ -1,23 +1,21 @@
 ---------------------------------------------
---  Carcharian_verve
---  Rockfin
+-- Carcharian_verve
+-- Rockfin
 ---------------------------------------------
+require("scripts/globals/monstertpmoves");
 require("scripts/globals/settings");
 require("scripts/globals/status");
-require("scripts/globals/monstertpmoves");
-
 ---------------------------------------------
+
 function onMobSkillCheck(target,mob,skill)
    return 0;
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-    local typeEffect = EFFECT_DEFENSE_BOOST;
-	local typeEffect = EFFECT_MAGIC_DEF_BOOST;	
-	local typeEffect = EFFECT_ATTACK_BOOST;	
 
-	MobStatusEffectMove(mob, target, typeEffect, 10, 3, 90);
- 
-    skill:setMsg(MSG_BUFF)
-    return typeEffect;
+    MobBuffMove(mob, EFFECT_DEFENSE_BOOST, 10, 0, 90);
+    MobBuffMove(mob, EFFECT_MAGIC_DEF_BOOST, 10, 0, 90);
+    skill:setMsg(MobBuffMove(mob, EFFECT_ATTACK_BOOST, 10, 0, 90));
+
+    return EFFECT_ATTACK_BOOST;
 end;
