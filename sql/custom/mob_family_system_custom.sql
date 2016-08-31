3eec82e8a33a3d00803c435dd236c7bb2823fb9c
TeoTwawki <arawn.annwn@yandex.com>
2016-08-31 19:08:36 -0400

    Revert mess Dave made. Soem work needs redoen now, because files were overwritten in Dave's commit alongside legit changes. REVIEW YOUR EFFING DIFF BEFORE COMMIT.

--- sql/custom/mob_family_system_custom.sql
+++ sql/custom/mob_family_system_custom.sql
@@ -3,7 +3,7 @@
 -- by the file mob_droplist.sql and must be imported/executed AFTER that file.
 -- ---------------------------------------------------------------------------
 
--- INSERT INTO `mob_family_system` VALUES (familyID,familyName,systemID,systemName,melee-size,speed,HP,MP,STR,DEX,VIT,AGI,INT,MND,CHR,ATT,DEF,ACC,EVA,Slash,Pierce,H2H,Impact,Fire,Ice,Wind,Earth,Lightning,Water,Light,Dark,Element);
+-- INSERT INTO `mob_family_system` VALUES (familyID,familyName,systemID,systemName,melee-size,speed,HP,MP,STR,DEX,VIT,AGI,INT,MND,CHR,ATT,DEF,ACC,EVA,Slash,Pierce,H2H,Impact,Fire,Ice,Wind,Earth,Lightning,Water,Light,Dark,Element,detects,charmable);
 -- New info below this line Sorted by familyID starting at 4000.
 
-INSERT INTO `mob_family_system` VALUES (4000,'Tome',18,'Unclassified',0,40,100,120,3,3,3,3,3,3,3,3,3,3,3,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0); -- Added new columns 0,0 at end of line to allow build
+INSERT INTO `mob_family_system` VALUES (4000,'Tome',18,'Unclassified',0,40,100,120,3,3,3,3,3,3,3,3,3,3,3,1,1,1,1,1,1,1,1,1,1,1,1,0,41,0);
