630429054dd00da01376f890e4e84b7bd24c15b5
TeoTwawki <arawn.annwn@yandex.com>
2016-09-12 12:34:55 -0400

    Ok, DSP does strange things with string so can't print the item name after all. It compiled, but would crash in the standard library from the abnormal string being sent to it.

--- src/map/packet_system.cpp
+++ src/map/packet_system.cpp
@@ -879,7 +879,7 @@ void SmallPacket0x028(map_session_data_t* session, CCharEntity* PChar, CBasicPac
         if (charutils::UpdateItem(PChar, container, slotID, -quantity) != 0)
         {
             // TODO: Break linkshell if the main shell was disposed of.
-            ShowNotice(CL_WHITE"Player %s DROPPING item %u %s \n" CL_RESET, PChar->GetName(), PItem->getID(), PItem->getName());
+            ShowNotice(CL_WHITE"Player %s DROPPING itemID %u \n" CL_RESET, PChar->GetName(), ItemID);
             PChar->pushPacket(new CMessageStandardPacket(nullptr, ItemID, quantity, 180));
             PChar->pushPacket(new CInventoryFinishPacket());
         }
