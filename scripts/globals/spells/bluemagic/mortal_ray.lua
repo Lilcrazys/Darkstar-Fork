a15788fd5f6615e327ad3710c3717db36999f352
TeoTwawki <arawn.annwn@yandex.com>
2016-09-30 18:56:21 -0400

    Wipe this mess out of scripts/globals/spells/bluemagic/mortal_ray.lua

--- scripts/globals/spells/bluemagic/mortal_ray.lua
+++ scripts/globals/spells/bluemagic/mortal_ray.lua
@@ -1,38 +1,37 @@
 -----------------------------------------
 -- Spell: Mortal Ray
 -----------------------------------------
-require("scripts/globals/magic");
-require("scripts/globals/status");
 require("scripts/globals/bluemagic");
+require("scripts/globals/status");
+require("scripts/globals/magic");
+
+-----------------------------------------
+-- onMagicCastingCheck
+-----------------------------------------
+
+function onMagicCastingCheck(caster,target,spell)
+    return 0;
+end;
+
 -----------------------------------------
 -- OnSpellCast
 -----------------------------------------
 
-function onMagicCastingCheck(caster,target,spell)
-	return 0;
-end;
-
 function onSpellCast(caster,target,spell)
+    local dINT = (caster:getStat(MOD_INT) - target:getStat(MOD_INT));
+    local resist = applyResistance(caster,spell,target,dINT,BLUE_SKILL,bonus);
 
+    if (target:isNM()) then -- Immune, cannot Doom NMs
+        spell:setMsg(75);
+    elseif (resist < 1) then -- Resisted
+        spell:setMsg(85);
+    else
+        if (target:addStatusEffect(EFFECT_DOOM,10, 3, 30)) then
+            spell:setMsg(237);
+        else
+            spell:setMsg(75); -- Already inflicted
+        end
+    end
 
-    local duration = 30;
-	local bonus = AffinityBonus(caster, spell:getElement());
-	local pINT = caster:getStat(MOD_INT);
-	local mINT = target:getStat(MOD_INT);
-	local dINT = (pINT - mINT);
-	local resm = applyResistance(caster,spell,target,dINT,BLUE_SKILL,bonus);
-	if (resm < 1.0425) then
-		spell:setMsg(85);--resist message
-		return EFFECT_DOOM;
-	end
-
-	duration = duration * resm;
-
-	if (target:addStatusEffect(EFFECT_DOOM,10,3,duration)) then
-		spell:setMsg(237);
-	else
-		spell:setMsg(75);
-	end
-
-	return EFFECT_DOOM;
+    return EFFECT_DOOM;
 end;
