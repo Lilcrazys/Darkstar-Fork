40997cd1bfddee7b59cfd2ce6c9333b9118c1edd
TeoTwawki <arawn.annwn@yandex.com>
2016-10-09 11:40:49 -0400

    New file to aid website tool in displaying scripted drops.

---
+++ sql/custom/mob_droplist_scripted_custom.sql
@@ -0,0 +1,14 @@
+-- ---------------------------------------------------------------------------
+-- This file list drops that are scripted, so that the website
+-- can properly display these items like ordinary drops.
+-- This should contain ONLY table structure followed by INSERT statements.
+-- ---------------------------------------------------------------------------
+DROP TABLE IF EXISTS `mob_droplist_scripted`;
+CREATE TABLE `mob_droplist_scripted` (
+  `dropId` smallint(5) unsigned NOT NULL,
+  `type` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `itemId` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `rate` smallint(4) unsigned NOT NULL DEFAULT '0',
+  KEY `dropId` (`dropId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AVG_ROW_LENGTH=9;
+-- INSERT INTO `mob_droplist_scripted` VALUES (dropId,type,itemId,rate); -- Item Name [Mob Name]
