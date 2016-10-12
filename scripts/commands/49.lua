de59c43b597dbb031506338b1baa49016b40b9b9
TeoTwawki <arawn.annwn@yandex.com>
2016-10-12 16:13:39 -0400

    Commit of hotfix I've lost 4 times..

--- scripts/commands/49.lua
+++ scripts/commands/49.lua
@@ -5,7 +5,7 @@
 
 cmdprops =
 {
-    permission = 1, // Disabled non GM use because of multi-boxing related server crash
+    permission = 1, -- Disabled non GM use because of multi-boxing related server crash
     parameters = ""
 };
 
