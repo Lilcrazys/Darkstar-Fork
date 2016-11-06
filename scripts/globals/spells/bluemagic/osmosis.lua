c1d64e5ff62926a70e828e31cd7a1f398f1b1a73
TeoTwawki <arawn.annwn@yandex.com>
2016-11-05 21:57:25 -0400

    Overhaul osmosis.lua (untested, Teo being naughty boy tonight)

--- scripts/globals/spells/bluemagic/osmosis.lua
+++ scripts/globals/spells/bluemagic/osmosis.lua
@@ -1,52 +1,58 @@
 --------------------------------------
--- 	Spell: Osmosis
--- 	Steals an enemy's attribute
---	Note: this does nothing but drain at he moment
---	as it has not been implimented yet
+-- Spell: Osmosis
+-- Steals an enemy's HP and one beneficial status effect. Ineffective against undead.
+-- Spell cost: 47 MP
+-- Monster Type: Vorageans
+-- Spell Type: Magical (Dark)
+-- Blue Magic Points: 5
+-- Stat Bonus: MND+3 CHR-2
+-- Level: 84
+-- Casting Time: 4 seconds
+-- Recast Time: 90 seconds
+-- Magic Bursts on: ?
+-- Combos: None
 --------------------------------------
- require("scripts/globals/magic");
-require("scripts/globals/status");
 require("scripts/globals/bluemagic");
+require("scripts/globals/status");
+require("scripts/globals/magic");
+require("scripts/globals/utils");
+
 -----------------------------------------
--- OnSpellCast
+-- onMagicCastingCheck
 -----------------------------------------
 
 function onMagicCastingCheck(caster,target,spell)
-	return 0;
+    return 0;
 end;
 
+-----------------------------------------
+-- onSpellCast
+-----------------------------------------
+
 function onSpellCast(caster,target,spell)
-
-	local dmg = 10 + (0.709 * (caster:getSkillLevel(BLUE_SKILL)) + caster:getMod(1 + BLUE_SKILL));
-	local resist = applyResistance(caster,spell,target,caster:getStat(MOD_MND)-target:getStat(MOD_MND),BLUE_SKILL,1.0);
-	dmg = dmg*resist;
-	dmg = addBonuses(caster,spell,target,dmg);
-	dmg = adjustForTarget(target,dmg,spell:getElement());
-	if (dmg > (caster:getSkillLevel(BLUE_SKILL) + 75)) then
-		dmg = (caster:getSkillLevel(BLUE_SKILL) + 75);
-	end
-
-    if (dmg < 0) then
-        dmg = 0
+    -- Hey, lets not do math if its immune anyway..
+    if (target:isUndead()) then
+        return 0;
     end
-    
-	if (target:isUndead()) or (effect == EFFECT_NONE) then
-	    spell:setMsg(75); 
-		return dmg;
-	end
 
-	dmg = finalMagicAdjustments(caster,target,spell,dmg);
+    local dINT = (caster:getStat(MOD_INT) - target:getStat(MOD_INT));
+    local resist = applyResistance(caster,spell,target,dINT,BLUE_SKILL,1.0);
+    local magicSkill = caster:getSkillLevel(BLUE_SKILL) + caster:getMod(MOD_BLUE);
+    local dmg = magicSkill*0.77;
+    local effect = EFFECT_NONE;
 
-	dmg = (dmg * DRAIN_POWER);
+    dmg = dmg * resist;
+    dmg = addBonuses(caster, spell, target, dmg);
+    dmg = adjustForTarget(target, dmg, spell:getElement());
+    dmg = BlueFinalAdjustments(caster,target,spell,dmg);
+    dmg = utils.clamp(dmg, 0, target:getHP()); -- Don't do negative dmg, don't do more than mobs HP..
 
-	if (effect == EFFECT_NONE) then
-	    spell:setMsg(75);
-	else	
-	    spell:setMsg(227);
-	end  
-	local effect = EFFECT_NONE;
-	effect = target:dispelStatusEffect();
-	caster:addHP(dmg);
-	return dmg;
+    if (resist > 0.0625) then
+        effect = target:dispelStatusEffect();
+        -- caster:addStatusEffect(effect,getPower,getTick,GetDuration,GetSubPower,getTier)
+    end
 
+    caster:addHP(dmg);
+
+    return dmg;
 end;
