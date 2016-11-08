2fae14b6c1213edd076426c8752b68ceff08ffc1
Dave <R3P0ffxi@gmail.com>
2016-11-08 18:00:22 -0500

    Adjusted legion entry cost to 60k per player

--- scripts/zones/Rolanberry_Fields/npcs/Legion_Tome.lua
+++ scripts/zones/Rolanberry_Fields/npcs/Legion_Tome.lua
@@ -22,14 +22,14 @@ function onTrade(player,npc,trade)
     print('gil:   ' .. trade:getGil());
     print('qty:   ' .. tostring(trade:hasItemQty(4104,1)));
     print('total: ' .. trade:getItemCount());
-    
-    if (trade:getGil() == 1000000) then
+
+    if (trade:getGil() == 60000) then
         if (trade:hasItemQty(4104,1) and trade:getItemCount() == 2) then -- gil + fire cluster to pop legion hall An
             SpawnMob(17526785); -- Lofty_Behemoth
             SpawnMob(17526786); -- Lofty_Wyrm
             SpawnMob(17526787); -- Lofty_Adamantoise
             -- player:PrintToPlayer( "You will now be transported to Legion" ); -- needs to be spoof saying....
-            player:delGil(1000000);
+            player:delGil(60000);
             player:tradeComplete();
             player:setPos(-180, 12, 212, 192, 183);
             -- HALL = 1;
@@ -38,7 +38,7 @@ function onTrade(player,npc,trade)
             SpawnMob(17526806); -- Mired_Khimaira
             SpawnMob(17526807); -- Mired_Hydra
             -- player:PrintToPlayer( "You will now be transported to Legion" ); -- needs to be spoof saying....
-            player:delGil(1000000);
+            player:delGil(60000);
             player:tradeComplete();
             player:setPos(130, 12, 220, 0, 183);
             -- HALL = 2;
@@ -47,7 +47,7 @@ function onTrade(player,npc,trade)
             SpawnMob(17526820); -- Soaring_Dvergr
             SpawnMob(17526821); -- Soaring_Vampyr
             -- player:PrintToPlayer( "You will now be transported to Legion" ); -- needs to be spoof saying....
-            player:delGil(1000000);
+            player:delGil(60000);
             player:tradeComplete();
             player:setPos(140, 12, -132, 64, 183);
             -- HALL = 3;
@@ -56,7 +56,7 @@ function onTrade(player,npc,trade)
             SpawnMob(17526834); -- Veiled_Ixion
             SpawnMob(17526835); -- Veiled_Sandworm
             -- player:PrintToPlayer( "You will now be transported to Legion" ); -- needs to be spoof saying....
-            player:delGil(1000000);
+            player:delGil(60000);
             player:tradeComplete();
             player:setPos(-170, 12, -140, 128, 183);
             -- HALL = 4;
@@ -67,7 +67,7 @@ function onTrade(player,npc,trade)
             SpawnMob(17526854); -- Paramount_Ironclad
             SpawnMob(17526880); -- 2 hour ele
             -- player:PrintToPlayer( "You will now be transported to Legion" ); -- needs to be spoof saying....
-            player:delGil(1000000);
+            player:delGil(60000);
             player:tradeComplete();
             player:setPos(-20, 12, 68, 64, 183);
             -- HALL = 5;
