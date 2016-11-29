299a2e0021aa1929ea016b9b583c3ca86668c54d
TeoTwawki <arawn.annwn@yandex.com>
2016-11-28 19:51:23 -0500

    Get rid of extra scripts for same mobskill..

--- scripts/globals/mobskills/Custom_Amorphic_scythe.lua
+++ scripts/globals/mobskills/Custom_Amorphic_scythe.lua
@@ -4,16 +4,20 @@ require("scripts/globals/monstertpmoves");
 ---------------------------------------------------
 
 function onMobSkillCheck(target,mob,skill)
-	return 0;
+    return 0;
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-	local numhits = 2;
-	local accmod = 1;
-	local dmgmod = 1;
-	local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_NO_EFFECT);
-	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_SLASH,info.hitslanded);
-	target:delHP(dmg);
+    local numhits = 1;
+    local accmod = 1;
+    local dmgmod = 2;
+    if (mob:getPool() == 5147) then -- Roly Poly (VWNM)
+        accmod = 10;
+        dmgmod = 3;
+    end
+    local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_NO_EFFECT);
+    local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_SLASH,info.hitslanded);
 
-	return dmg;
+    target:delHP(dmg);
+    return dmg;
 end;
