c167dd8f713931618a4393f0ea612940b8079966
Dave <R3P0ffxi@gmail.com>
2016-12-07 20:12:24 -0500

    Working tear fix for scripted drops needs adding to ALL VWNM

--- scripts/zones/East_Ronfaure/mobs/Sarimanok.lua
+++ scripts/zones/East_Ronfaure/mobs/Sarimanok.lua
@@ -36,84 +36,6 @@ function onMobSpawn(mob)
     mob:setMod(MOD_TRIPLE_ATTACK,10);
     mob:setMod(MOD_DEF,1500);
 
-    -- Vars
-    -- mob:setLocalVar("depopTime", os.time(t) + 1800);  -- despawn in 30 min
-    
-    local RND1 = math.random(1,8);
-    if (RND1 == 1) then
-        SetDropRate(9602,8919,50); -- Ifritear
-        SetDropRate(9602,8920,0); -- Leviatear
-        SetDropRate(9602,8921,0); -- Ramutear
-        SetDropRate(9602,8922,0); -- Garutear
-        SetDropRate(9602,8923,0); -- Titatear
-        SetDropRate(9602,8924,0); -- Shivatear
-        SetDropRate(9602,8925,0); -- Carbutear
-        SetDropRate(9602,8926,0); -- Fenritear
-    elseif (RND1 == 2) then
-        SetDropRate(9602,8919,0); -- Ifritear
-        SetDropRate(9602,8920,50); -- Leviatear
-        SetDropRate(9602,8921,0); -- Ramutear
-        SetDropRate(9602,8922,0); -- Garutear
-        SetDropRate(9602,8923,0); -- Titatear
-        SetDropRate(9602,8924,0); -- Shivatear
-        SetDropRate(9602,8925,0); -- Carbutear
-        SetDropRate(9602,8926,0); -- Fenritear
-    elseif (RND1 == 3) then
-        SetDropRate(9602,8919,0); -- Ifritear
-        SetDropRate(9602,8920,0); -- Leviatear
-        SetDropRate(9602,8921,50); -- Ramutear
-        SetDropRate(9602,8922,0); -- Garutear
-        SetDropRate(9602,8923,0); -- Titatear
-        SetDropRate(9602,8924,0); -- Shivatear
-        SetDropRate(9602,8925,0); -- Carbutear
-        SetDropRate(9602,8926,0); -- Fenritear
-    elseif (RND1 == 4) then
-        SetDropRate(9602,8919,0); -- Ifritear
-        SetDropRate(9602,8920,0); -- Leviatear
-        SetDropRate(9602,8921,0); -- Ramutear
-        SetDropRate(9602,8922,50); -- Garutear
-        SetDropRate(9602,8923,0); -- Titatear
-        SetDropRate(9602,8924,0); -- Shivatear
-        SetDropRate(9602,8925,0); -- Carbutear
-        SetDropRate(9602,8926,0); -- Fenritear
-    elseif (RND1 == 5) then
-        SetDropRate(9602,8919,0); -- Ifritear
-        SetDropRate(9602,8920,0); -- Leviatear
-        SetDropRate(9602,8921,0); -- Ramutear
-        SetDropRate(9602,8922,0); -- Garutear
-        SetDropRate(9602,8923,50); -- Titatear
-        SetDropRate(9602,8924,0); -- Shivatear
-        SetDropRate(9602,8925,0); -- Carbutear
-        SetDropRate(9602,8926,0); -- Fenritear
-    elseif (RND1 == 6) then
-        SetDropRate(9602,8919,0); -- Ifritear
-        SetDropRate(9602,8920,0); -- Leviatear
-        SetDropRate(9602,8921,0); -- Ramutear
-        SetDropRate(9602,8922,0); -- Garutear
-        SetDropRate(9602,8923,0); -- Titatear
-        SetDropRate(9602,8924,50); -- Shivatear
-        SetDropRate(9602,8925,0); -- Carbutear
-        SetDropRate(9602,8926,0); -- Fenritear
-    elseif (RND1 == 7) then
-        SetDropRate(9602,8919,0); -- Ifritear
-        SetDropRate(9602,8920,0); -- Leviatear
-        SetDropRate(9602,8921,0); -- Ramutear
-        SetDropRate(9602,8922,0); -- Garutear
-        SetDropRate(9602,8923,0); -- Titatear
-        SetDropRate(9602,8924,0); -- Shivatear
-        SetDropRate(9602,8925,50); -- Carbutear
-        SetDropRate(9602,8926,0); -- Fenritear
-    elseif (RND1 == 8) then
-        SetDropRate(9602,8919,0); -- Ifritear
-        SetDropRate(9602,8920,0); -- Leviatear
-        SetDropRate(9602,8921,0); -- Ramutear
-        SetDropRate(9602,8922,0); -- Garutear
-        SetDropRate(9602,8923,0); -- Titatear
-        SetDropRate(9602,8924,0); -- Shivatear
-        SetDropRate(9602,8925,0); -- Carbutear
-        SetDropRate(9602,8926,50); -- Fenritear
-    end    
-    
 end;
 
 -----------------------------------
@@ -152,6 +74,28 @@ end;
 -----------------------------------
 
 function onMobDeath(mob, player, isKiller)
+
+    if (isKiller == true) then
+        local RND1 = math.random(1,8);
+        if (RND1 == 1) then
+            player:addTreasure(8919); -- Ifritear
+        elseif (RND1 == 2) then
+            player:addTreasure(8920); -- Leviatear
+        elseif (RND1 == 3) then
+            player:addTreasure(8921); -- Ramutear
+        elseif (RND1 == 4) then
+            player:addTreasure(8922); -- Garutear
+        elseif (RND1 == 5) then
+            player:addTreasure(8923); -- Titatear
+        elseif (RND1 == 6) then
+            player:addTreasure(8924); -- Shivatear
+        elseif (RND1 == 7) then
+            player:addTreasure(8925); -- Carbutear
+        elseif (RND1 == 8) then
+            player:addTreasure(8926); -- Fenritear
+        end
+    end
+
     if (player:hasKeyItem(CRIMSON_STRATUM_ABYSSITE)) then -- Sarimanok Kill
         if (player:getMaskBit(player:getVar("CRIMSON_STRATUM"), 0) == false) then
             player:setMaskBit(player:getVar("CRIMSON_STRATUM"),"CRIMSON_STRATUM",0,true);
@@ -163,5 +107,5 @@ function onMobDeath(mob, player, isKiller)
             player:messageSpecial(KEYITEM_OBTAINED, CRIMSON_STRATUM_ABYSSITE_II);
         end
     end
-    player:addExp(10000);    
+    player:addExp(10000);
 end;
