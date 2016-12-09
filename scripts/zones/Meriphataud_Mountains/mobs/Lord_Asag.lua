263f431c0366558e80582377efe661e8ec9071c6
Dave <R3P0FFXI@gmail.com>
2016-12-09 13:00:22 -0500

    Added VWNM Scripted drops Yatagarasu.lua last NM adjusted

--- scripts/zones/Meriphataud_Mountains/mobs/Lord_Asag.lua
+++ scripts/zones/Meriphataud_Mountains/mobs/Lord_Asag.lua
@@ -41,81 +41,26 @@ function onMobSpawn(mob)
     -- Vars
     -- mob:setLocalVar("depopTime", os.time(t) + 1800);  -- despawn in 30 min
 
-    local RND1 = math.random(1,8);
-    if (RND1 == 1) then
-        SetDropRate(9613,8919,50); -- Ifritear
-        SetDropRate(9613,8920,0); -- Leviatear
-        SetDropRate(9613,8921,0); -- Ramutear
-        SetDropRate(9613,8922,0); -- Garutear
-        SetDropRate(9613,8923,0); -- Titatear
-        SetDropRate(9613,8924,0); -- Shivatear
-        SetDropRate(9613,8925,0); -- Carbutear
-        SetDropRate(9613,8926,0); -- Fenritear
-    elseif (RND1 == 2) then
-        SetDropRate(9613,8919,0); -- Ifritear
-        SetDropRate(9613,8920,50); -- Leviatear
-        SetDropRate(9613,8921,0); -- Ramutear
-        SetDropRate(9613,8922,0); -- Garutear
-        SetDropRate(9613,8923,0); -- Titatear
-        SetDropRate(9613,8924,0); -- Shivatear
-        SetDropRate(9613,8925,0); -- Carbutear
-        SetDropRate(9613,8926,0); -- Fenritear
-    elseif (RND1 == 3) then
-        SetDropRate(9613,8919,0); -- Ifritear
-        SetDropRate(9613,8920,0); -- Leviatear
-        SetDropRate(9613,8921,50); -- Ramutear
-        SetDropRate(9613,8922,0); -- Garutear
-        SetDropRate(9613,8923,0); -- Titatear
-        SetDropRate(9613,8924,0); -- Shivatear
-        SetDropRate(9613,8925,0); -- Carbutear
-        SetDropRate(9613,8926,0); -- Fenritear
-    elseif (RND1 == 4) then
-        SetDropRate(9613,8919,0); -- Ifritear
-        SetDropRate(9613,8920,0); -- Leviatear
-        SetDropRate(9613,8921,0); -- Ramutear
-        SetDropRate(9613,8922,50); -- Garutear
-        SetDropRate(9613,8923,0); -- Titatear
-        SetDropRate(9613,8924,0); -- Shivatear
-        SetDropRate(9613,8925,0); -- Carbutear
-        SetDropRate(9613,8926,0); -- Fenritear
-    elseif (RND1 == 5) then
-        SetDropRate(9613,8919,0); -- Ifritear
-        SetDropRate(9613,8920,0); -- Leviatear
-        SetDropRate(9613,8921,0); -- Ramutear
-        SetDropRate(9613,8922,0); -- Garutear
-        SetDropRate(9613,8923,50); -- Titatear
-        SetDropRate(9613,8924,0); -- Shivatear
-        SetDropRate(9613,8925,0); -- Carbutear
-        SetDropRate(9613,8926,0); -- Fenritear
-    elseif (RND1 == 6) then
-        SetDropRate(9613,8919,0); -- Ifritear
-        SetDropRate(9613,8920,0); -- Leviatear
-        SetDropRate(9613,8921,0); -- Ramutear
-        SetDropRate(9613,8922,0); -- Garutear
-        SetDropRate(9613,8923,0); -- Titatear
-        SetDropRate(9613,8924,50); -- Shivatear
-        SetDropRate(9613,8925,0); -- Carbutear
-        SetDropRate(9613,8926,0); -- Fenritear
-    elseif (RND1 == 7) then
-        SetDropRate(9613,8919,0); -- Ifritear
-        SetDropRate(9613,8920,0); -- Leviatear
-        SetDropRate(9613,8921,0); -- Ramutear
-        SetDropRate(9613,8922,0); -- Garutear
-        SetDropRate(9613,8923,0); -- Titatear
-        SetDropRate(9613,8924,0); -- Shivatear
-        SetDropRate(9613,8925,50); -- Carbutear
-        SetDropRate(9613,8926,0); -- Fenritear
-    elseif (RND1 == 8) then
-        SetDropRate(9613,8919,0); -- Ifritear
-        SetDropRate(9613,8920,0); -- Leviatear
-        SetDropRate(9613,8921,0); -- Ramutear
-        SetDropRate(9613,8922,0); -- Garutear
-        SetDropRate(9613,8923,0); -- Titatear
-        SetDropRate(9613,8924,0); -- Shivatear
-        SetDropRate(9613,8925,0); -- Carbutear
-        SetDropRate(9613,8926,50); -- Fenritear
+    if (isKiller == true) then
+        local RND1 = math.random(1,8);
+        if (RND1 == 1) then
+            player:addTreasure(8919); -- Ifritear
+        elseif (RND1 == 2) then
+            player:addTreasure(8920); -- Leviatear
+        elseif (RND1 == 3) then
+            player:addTreasure(8921); -- Ramutear
+        elseif (RND1 == 4) then
+            player:addTreasure(8922); -- Garutear
+        elseif (RND1 == 5) then
+            player:addTreasure(8923); -- Titatear
+        elseif (RND1 == 6) then
+            player:addTreasure(8924); -- Shivatear
+        elseif (RND1 == 7) then
+            player:addTreasure(8925); -- Carbutear
+        elseif (RND1 == 8) then
+            player:addTreasure(8926); -- Fenritear
+        end
     end
-
 end;
 -----------------------------------
 -- onMobEngage Action
--- scripts/zones/Outer_Horutoto_Ruins/mobs/Voidwrought.lua
+++ scripts/zones/Outer_Horutoto_Ruins/mobs/Voidwrought.lua
@@ -42,79 +42,25 @@ function onMobSpawn(mob)
     -- var
     -- mob:setLocalVar("depopTime", os.time(t) + 1800);  -- despawn in 30 min
 
-    local RND1 = math.random(1,8);
-    if (RND1 == 1) then
-        SetDropRate(9617,8919,50); -- Ifritear
-        SetDropRate(9617,8920,0); -- Leviatear
-        SetDropRate(9617,8921,0); -- Ramutear
-        SetDropRate(9617,8922,0); -- Garutear
-        SetDropRate(9617,8923,0); -- Titatear
-        SetDropRate(9617,8924,0); -- Shivatear
-        SetDropRate(9617,8925,0); -- Carbutear
-        SetDropRate(9617,8926,0); -- Fenritear
-    elseif (RND1 == 2) then
-        SetDropRate(9617,8919,0); -- Ifritear
-        SetDropRate(9617,8920,50); -- Leviatear
-        SetDropRate(9617,8921,0); -- Ramutear
-        SetDropRate(9617,8922,0); -- Garutear
-        SetDropRate(9617,8923,0); -- Titatear
-        SetDropRate(9617,8924,0); -- Shivatear
-        SetDropRate(9617,8925,0); -- Carbutear
-        SetDropRate(9617,8926,0); -- Fenritear
-    elseif (RND1 == 3) then
-        SetDropRate(9617,8919,0); -- Ifritear
-        SetDropRate(9617,8920,0); -- Leviatear
-        SetDropRate(9617,8921,50); -- Ramutear
-        SetDropRate(9617,8922,0); -- Garutear
-        SetDropRate(9617,8923,0); -- Titatear
-        SetDropRate(9617,8924,0); -- Shivatear
-        SetDropRate(9617,8925,0); -- Carbutear
-        SetDropRate(9617,8926,0); -- Fenritear
-    elseif (RND1 == 4) then
-        SetDropRate(9617,8919,0); -- Ifritear
-        SetDropRate(9617,8920,0); -- Leviatear
-        SetDropRate(9617,8921,0); -- Ramutear
-        SetDropRate(9617,8922,50); -- Garutear
-        SetDropRate(9617,8923,0); -- Titatear
-        SetDropRate(9617,8924,0); -- Shivatear
-        SetDropRate(9617,8925,0); -- Carbutear
-        SetDropRate(9617,8926,0); -- Fenritear
-    elseif (RND1 == 5) then
-        SetDropRate(9617,8919,0); -- Ifritear
-        SetDropRate(9617,8920,0); -- Leviatear
-        SetDropRate(9617,8921,0); -- Ramutear
-        SetDropRate(9617,8922,0); -- Garutear
-        SetDropRate(9617,8923,50); -- Titatear
-        SetDropRate(9617,8924,0); -- Shivatear
-        SetDropRate(9617,8925,0); -- Carbutear
-        SetDropRate(9617,8926,0); -- Fenritear
-    elseif (RND1 == 6) then
-        SetDropRate(9617,8919,0); -- Ifritear
-        SetDropRate(9617,8920,0); -- Leviatear
-        SetDropRate(9617,8921,0); -- Ramutear
-        SetDropRate(9617,8922,0); -- Garutear
-        SetDropRate(9617,8923,0); -- Titatear
-        SetDropRate(9617,8924,50); -- Shivatear
-        SetDropRate(9617,8925,0); -- Carbutear
-        SetDropRate(9617,8926,0); -- Fenritear
-    elseif (RND1 == 7) then
-        SetDropRate(9617,8919,0); -- Ifritear
-        SetDropRate(9617,8920,0); -- Leviatear
-        SetDropRate(9617,8921,0); -- Ramutear
-        SetDropRate(9617,8922,0); -- Garutear
-        SetDropRate(9617,8923,0); -- Titatear
-        SetDropRate(9617,8924,0); -- Shivatear
-        SetDropRate(9617,8925,50); -- Carbutear
-        SetDropRate(9617,8926,0); -- Fenritear
-    elseif (RND1 == 8) then
-        SetDropRate(9617,8919,0); -- Ifritear
-        SetDropRate(9617,8920,0); -- Leviatear
-        SetDropRate(9617,8921,0); -- Ramutear
-        SetDropRate(9617,8922,0); -- Garutear
-        SetDropRate(9617,8923,0); -- Titatear
-        SetDropRate(9617,8924,0); -- Shivatear
-        SetDropRate(9617,8925,0); -- Carbutear
-        SetDropRate(9617,8926,50); -- Fenritear
+    if (isKiller == true) then
+        local RND1 = math.random(1,8);
+        if (RND1 == 1) then
+            player:addTreasure(8919); -- Ifritear
+        elseif (RND1 == 2) then
+            player:addTreasure(8920); -- Leviatear
+        elseif (RND1 == 3) then
+            player:addTreasure(8921); -- Ramutear
+        elseif (RND1 == 4) then
+            player:addTreasure(8922); -- Garutear
+        elseif (RND1 == 5) then
+            player:addTreasure(8923); -- Titatear
+        elseif (RND1 == 6) then
+            player:addTreasure(8924); -- Shivatear
+        elseif (RND1 == 7) then
+            player:addTreasure(8925); -- Carbutear
+        elseif (RND1 == 8) then
+            player:addTreasure(8926); -- Fenritear
+        end
     end
 end;
 
