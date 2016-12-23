2b69b4d46c3b218bed14ea6211f91d620d5fe343
TeoTwawki <arawn.annwn@yandex.com>
2016-12-22 21:39:18 -0500

    Missed one!

--- scripts/zones/Abyssea-Misareaux/mobs/Kutharei.lua
+++ scripts/zones/Abyssea-Misareaux/mobs/Kutharei.lua
@@ -44,22 +44,23 @@ end;
 function onMobDeath(mob, player, isKiller)
 
     if (isKiller == true) then
-        local Chance = math.random(1,100); -- Weapon should be 50% chance
-        local Weapon = math.random(1,5);
+        local itemRate = math.random(1,100); -- Weapon should be 50% chance
+        local selectWeapon = math.random(1,5);
 
-            if (Chance <= 50) then
-                if (Weapon == 1) then
-                    player:addTreasure(21051, mob); -- Shichishito +
-                elseif (Weapon == 2) then
-                    player:addTreasure(20634, mob); -- Leisilonu +1
-                elseif (Weapon == 3) then
-                    player:addTreasure(20961, mob); -- Qatsunoci +1
-                elseif (Weapon == 4) then
-                    player:addTreasure(20915, mob); -- Iizamal +1
-                elseif (Weapon == 5) then
-                    player:addTreasure(21236, mob); -- Bocluamni +1
-                end
+        if (itemRate <= 50) then
+            if (selectWeapon == 1) then
+                player:addTreasure(21051, mob); -- Shichishito +
+            elseif (selectWeapon == 2) then
+                player:addTreasure(20634, mob); -- Leisilonu +1
+            elseif (selectWeapon == 3) then
+                player:addTreasure(20961, mob); -- Qatsunoci +1
+            elseif (selectWeapon == 4) then
+                player:addTreasure(20915, mob); -- Iizamal +1
+            elseif (selectWeapon == 5) then
+                player:addTreasure(21236, mob); -- Bocluamni +1
             end
+        end
+    end
 
     local CHANCE = 15;
     if (math.random(0,99) < CHANCE  and player:hasKeyItem(ATMA_OF_THE_MOUNTED_CHAMPION) == false) then
