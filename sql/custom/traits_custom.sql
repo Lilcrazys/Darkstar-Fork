dbddd52e5ecb23dfce14cef90d1858429134562f
TeoTwawki <arawn.annwn@yandex.com>
2017-01-12 01:16:17 -0500

    Maybe this will help..Custom traits.

---
+++ sql/custom/traits_custom.sql
@@ -0,0 +1,148 @@
+-- ---------------------------------------------------------------------------
+-- This file adds onto or modifies contents of traits.sql
+-- and must be imported/executed AFTER that file
+--
+-- For new entries DarkStar does not have, use "INSERT INTO"
+-- ---------------------------------------------------------------------------
+-- INSERT INTO `traits` VALUES (traitid,name,job,level,rank,modifier,value,required_expansion);
+
+-- PLD custom trains, these piggy back off Undead Killer's ID.
+-- Since Undead killer is granted at lv5 and these later, player will not notice any change to the list.
+INSERT INTO `traits` VALUES (39,'PLD Enmity Bonus',7,30,1,27,5,null);  -- same as enmity (+5) gear         
+INSERT INTO `traits` VALUES (39,'PLD Enmity Bonus',7,60,2,27,12,null); -- same as enmity (+12) gear       
+INSERT INTO `traits` VALUES (39,'PLD Enmity Bonus',7,90,3,27,20,null); -- same as enmity (+20) gear       
+INSERT INTO `traits` VALUES (39,'PLD Enmity Loss Reduction',7,30,1,502,4,null);  -- 4% reduction in hate lost from taking dmg
+INSERT INTO `traits` VALUES (39,'PLD Enmity Loss Reduction',7,60,2,502,8,null);  -- 8% reduction in hate lost from taking dmg
+INSERT INTO `traits` VALUES (39,'PLD Enmity Loss Reduction',7,90,3,502,12,null); -- 12% reduction in hate lost from taking dmg
+INSERT INTO `traits` VALUES (39,'PLD WS power Bonus',7,20,3,4000,4,null); -- Adds 4% to all ws
+INSERT INTO `traits` VALUES (39,'PLD WS power Bonus',7,50,3,4000,5,null); -- Adds 5% to all ws
+
+-- No edits below this line!
+-- ID   Name
+-- 1    Accuracy Bonus
+-- 2    Evasion Bonus
+-- 3    Attack Bonus
+-- 4    Defense Bonus
+-- 5    Magic Atk. Bonus
+-- 6    Magic Def. Bonus
+-- 7    Max HP Boost
+-- 8    Max MP Boost
+-- 9    Auto Regen
+-- 10   Auto Refresh
+-- 11   Rapid Shot
+-- 12   Fast Cast
+-- 13   Conserve MP
+-- 14   Store TP
+-- 15   Double Attack
+-- 16   Triple Attack
+-- 17   Counter
+-- 18   Dual Wield
+-- 19   Treasure Hunter
+-- 20   Gilfinder
+-- 21   Alertness
+-- 22   Stealth
+-- 23   Martial Arts
+-- 24   Clear Mind
+-- 25   Shield Mastery
+-- 26   .
+-- 27   .
+-- 28   .
+-- 29   .
+-- 30   .
+-- 31   .
+-- 32   Beast Killer
+-- 33   Plantoid Killer
+-- 34   Vermin Killer
+-- 35   Lizard Killer
+-- 36   Bird Killer
+-- 37   Amorph Killer
+-- 38   Aquan Killer
+-- 39   Undead Killer
+-- 40   .
+-- 41   Arcana Killer
+-- 42   Demon Killer
+-- 43   Dragon Killer
+-- 44   .
+-- 45   .
+-- 46   .
+-- 47   .
+-- 48   Resist Sleep
+-- 49   Resist Poison
+-- 50   Resist Paralyze
+-- 51   Resist Blind
+-- 52   Resist Silence
+-- 53   Resist Petrify
+-- 54   Resist Virus
+-- 55   Resist Curse
+-- 56   Resist Stun
+-- 57   Resist Bind
+-- 58   Resist Gravity
+-- 59   Resist Slow
+-- 60   Resist Charm
+-- 61   .
+-- 62   .
+-- 63   Resist Amnesia
+-- 64   Treasure Hunter II
+-- 65   Treas. Hunter III
+-- 66   Kick Attacks
+-- 67   Subtle Blow
+-- 68   Assassin
+-- 69   Divine Veil
+-- 70   Zanshin
+-- 71   Savagery
+-- 72   Aggressive Aim
+-- 73   Invigorate
+-- 74   Penance
+-- 75   Aura Steal
+-- 76   Ambush
+-- 77   Iron Will
+-- 78   Guardian
+-- 79   Muted Soul
+-- 80   Desperate Blows
+-- 81   Beast Affinity
+-- 82   Beast Healer
+-- 83   Snapshot
+-- 84   Recycle
+-- 85   Ikishoten
+-- 86   Overwhelm
+-- 87   Ninja Tool Expert.
+-- 88   Empathy
+-- 89   Strafe
+-- 90   Enchainment
+-- 91   Assimilation
+-- 92   Winning Streak
+-- 93   Loaded Deck
+-- 94   Fine-Tuning
+-- 95   Optimization
+-- 96   Closed Position
+-- 97   Stormsurge
+-- 98   Crit. Atk. Bonus
+-- 99   Crit. Def. Bonus
+-- 100  Tactical Parry
+-- 101  Tactical Guard
+-- 102  Shield Def. Bonus
+-- 103  Stout Servant
+-- 104  True Shot
+-- 105  Blood Boon
+-- 106  Skillchain Bonus
+-- 107  Fencer
+-- 108  Conserve TP
+-- 109  Occult Acumen
+-- 110  Mag. Burst Bonus
+-- 111  Divine Benison
+-- 112  Elemental Celerity
+-- 113  Dead Aim
+-- 114  Tranquil Heart
+-- 115  Stalwart Soul
+-- 116  Cardinal Chant
+-- 117  Tenacity
+-- 118  Inquartata
+-- 119  C. Recantation
+-- 120  Primeval Zeal
+-- 121  Inspiration
+-- 122  Sleight of Sword
+-- 123  Daken
+-- 124  Superior
+-- 125  Magic Acc. Bonus
+-- 126  Magic Eva. Bonus
+-- 127  Smite
