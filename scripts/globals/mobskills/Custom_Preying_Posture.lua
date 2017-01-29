f5b6a71e80452d24bfb87259e3239965613587e3
TeoTwawki <arawn.annwn@yandex.com>
2017-01-29 00:16:41 -0500

    Stop Preying Posture from sending invalid message to client..

--- scripts/globals/mobskills/Custom_Preying_Posture.lua
+++ scripts/globals/mobskills/Custom_Preying_Posture.lua
@@ -1,9 +1,9 @@
 ---------------------------------------------
---  Preying Posture
---  Type: Enhancing
---  Utsusemi/Blink absorb: N/A
---  Range: Self
---  Notes: 50% Attack Boost, MATT, ACC
+-- Preying Posture
+-- Type: Enhancing
+-- Utsusemi/Blink absorb: N/A
+-- Range: Self
+-- Notes: 50% Attack Boost, MATT, ACC
 ---------------------------------------------
 require("scripts/globals/settings");
 require("scripts/globals/status");
@@ -15,8 +15,9 @@ function onMobSkillCheck(target,mob,skill)
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-    skill:setMsg(MobBuffMove(mob, EFFECT_ATTACK_BOOST, 50, 0, 180));
-    skill:setMsg(MobBuffMove(mob, EFFECT_MAGIC_ATK_BOOST, 25, 0, 180));
-    skill:setMsg(MobBuffMove(mob, EFFECT_ACCURACY_BOOST, 50, 0, 180));
-    return typeEffect;
+    MobBuffMove(mob, EFFECT_ATTACK_BOOST, 50, 0, 180);
+    MobBuffMove(mob, EFFECT_MAGIC_ATK_BOOST, 25, 0, 180);
+    MobBuffMove(mob, EFFECT_ACCURACY_BOOST, 50, 0, 180);
+
+    return EFFECT_ATTACK_BOOST;
 end;
