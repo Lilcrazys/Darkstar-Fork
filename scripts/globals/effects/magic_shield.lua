224c716f97daaabe55473eba73b0c5e9b9c87f47
TeoTwawki <arawn.annwn@yandex.com>
2017-03-04 16:12:17 -0500

    THOU SHALT NOT SILENTLY REVERT MY FIX, GIT-TARD!

--- scripts/globals/effects/magic_shield.lua
+++ scripts/globals/effects/magic_shield.lua
@@ -62,7 +62,7 @@ function onEffectLose(target,effect)
     elseif (effect:getPower() < 2) then
         target:delMod(MOD_UDMGMAGIC, -101);
     elseif (effect:getPower() == 100) then
-        target:delMod(MOD_UDMGMAGIC, -64); -- damned blue spell
+        target:delMod(MOD_UDMGMAGIC, -25); -- damned blue spell
     else
         target:delMod(MOD_UDMGMAGIC, -101); -- temp
         --[[ broken
