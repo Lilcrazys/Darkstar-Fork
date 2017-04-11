6e77faa514093dd2c6778eb6c84f09270f19da88
TeoTwawki <arawn.annwn@yandex.com>
2017-04-11 13:05:35 -0400

    Correct Ash_Dragon.lua (this ceased being a trial mob awhile back)

--- scripts/zones/Ifrits_Cauldron/mobs/Ash_Dragon.lua
+++ scripts/zones/Ifrits_Cauldron/mobs/Ash_Dragon.lua
@@ -1,31 +1,15 @@
 -----------------------------------
 -- Area: Ifrit's Cauldron
---  NM:  Ash Dragon
+--  MOB: Ash Dragon
 -----------------------------------
 
 require("scripts/globals/titles");
-require("scripts/globals/status");
-require("scripts/globals/custom_trials");
-
------------------------------------
--- onMobInitialize Action
------------------------------------
-
-function onMobInitialize(mob)
-    -- addMod
-    mob:addMod(MOD_STR,7);
-    mob:addMod(MOD_DEX,5);
-    mob:addMod(MOD_ACC,200);
-    mob:addMod(MOD_DOUBLE_ATTACK,15);
-end;
 
 -----------------------------------
 -- onMobSpawn Action
 -----------------------------------
 
 function onMobSpawn(mob)
-    -- setMod
-    mob:setMod(MOD_REGAIN,33);
 end;
 
 -----------------------------------
@@ -34,18 +18,6 @@ end;
 
 function onMobDeath(mob, player, isKiller)
     player:addTitle(DRAGON_ASHER);
-
-    ------------------------------------
-    -- Begin Custom Legion Code
-    ------------------------------------
-
-    -- Custom Trial Check
-    cTrialProgress(player, 3, "relic");
-
-    ------------------------------------
-    -- End Custom Legion Code
-    ------------------------------------
-
 end;
 
 -----------------------------------
@@ -53,9 +25,12 @@ end;
 -----------------------------------
 
 function onMobDespawn(mob)
-
+    --[[
+    -- Set ASh Dragon's spawnpoint and respawn time (3-5 Days)
+    UpdateNMSpawnPoint(mob:getID());
+    mob:setRespawnTime(math.random(259200,432000));
+    ]]
     -- Set ASh Dragon's spawnpoint and respawn time (21-24 hours)
     UpdateNMSpawnPoint(mob:getID());
     mob:setRespawnTime(math.random(75600,86400));
-
 end;
