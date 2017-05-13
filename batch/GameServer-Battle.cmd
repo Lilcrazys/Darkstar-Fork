f8e7ebb8b78b88cf54785d89b50688071b017b7a
TeoTwawki <arawn.annwn@yandex.com>
2017-05-13 00:00:20 -0400

    Wtf? How'd that get there.

--- batch/GameServer-Battle.cmd
+++ batch/GameServer-Battle.cmd
@@ -1,7 +1,5 @@
 @echo off
 cd ..
-del DSGame-Battle.exe
-copy DSGame-server.exe /b DSGame-Battle.exe /b
 :onCrash
 echo [%date% %time%] Restarting Battle Map Server...
 DSGame-Battle.exe --ip 24.192.160.140 --port 54530 --log .\log\Battle_Map.log
