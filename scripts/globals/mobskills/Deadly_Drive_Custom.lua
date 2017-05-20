2b001938385db665b919687f2f963e683c1e4af6
Dave <R3P0ffxi@gmail.com>
2017-05-20 18:16:21 -0400

    mob_skills_custom.sql adjusted 769-856
    Deleted unnecessary duplicate scripts, renamed duplicate custom nm scripts, turned off mobskills with no scripts

--- scripts/globals/mobskills/Deadly_Drive_Custom.lua
+++ scripts/globals/mobskills/Deadly_Drive_Custom.lua
@@ -1,3 +1,6 @@
+---------------------------------------------------
+-- Deadly Drive (Custom Version for NM)
+---------------------------------------------------
 require("scripts/globals/settings");
 require("scripts/globals/status");
 require("scripts/globals/monstertpmoves");
@@ -10,7 +13,7 @@ end;
 function onMobWeaponSkill(target, mob, skill)
 	local numhits = 1;
 	local accmod = 1;
-	local dmgmod = 2.5;
+	local dmgmod = 3;
 	local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_DMG_VARIES,1,2,3);
 	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_SLASH,info.hitslanded);
 	target:delHP(dmg);
