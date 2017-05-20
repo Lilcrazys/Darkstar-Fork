6bd3f3bf47e21669b8b95fffade67e893fb76833
Dave <R3P0ffxi@gmail.com>
2017-05-19 20:25:29 -0400

    mob_skills_custom.sql adjusted 557-656
    Deleted unnecessary duplicate scripts, renamed duplicate custom nm scripts

--- scripts/globals/mobskills/Chaos_Blade_Custom.lua
+++ scripts/globals/mobskills/Chaos_Blade_Custom.lua
@@ -1,26 +1,21 @@
 ---------------------------------------------
---  Chaos Blade
+-- Chaos Blade (Custom Version for NM)
 --
---  Description: Deals Dark damage to enemies within a fan-shaped area. Additional effect: Curse
---  Type: Magical
---  Utsusemi/Blink absorb: Ignores Shadows
---  Range: Melee
---  Notes:
+-- Deals Dark Magic damage to enemies within a fan-shaped area.
+-- Ignores Shadows. Additional effect: Curse
 ---------------------------------------------
 require("scripts/globals/settings");
 require("scripts/globals/status");
 require("scripts/globals/monstertpmoves");
-
 ---------------------------------------------
+
 function onMobSkillCheck(target,mob,skill)
     return 0;
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-
     local numhits = 1;
-
-    local dmgmod = 2;
+    local dmgmod = 4;
     local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg() * 3,ELE_DARK,dmgmod,TP_MAB_BONUS,1);
     local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_DARK,MOBPARAM_IGNORE_SHADOWS);
     target:delHP(dmg);
