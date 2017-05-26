fad72ba1dd1f2dbedb62dddfe5ab6a4ad098382a
Dave <R3P0FFXI@gmail.com>
2017-05-26 11:19:24 -0400

    mob_skills_custom.sql 1802-2098
    Deleted unnecessary duplicate scripts, renamed duplicate custom nm scripts, turned off mobskills with no scripts

--- scripts/globals/mobskills/Chimera_Ripper_Custom.lua
+++ scripts/globals/mobskills/Chimera_Ripper_Custom.lua
@@ -1,11 +1,9 @@
 ---------------------------------------------------
--- Chimera Ripper
+-- Chimera Ripper (Custom Version for NM)
 ---------------------------------------------------
-
 require("scripts/globals/settings");
 require("scripts/globals/status");
 require("scripts/globals/monstertpmoves");
-
 ---------------------------------------------------
 
 function onMobSkillCheck(target,mob,skill)
@@ -13,10 +11,9 @@ function onMobSkillCheck(target,mob,skill)
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-
-    local numhits = 1;
-    local accmod = 1;
-    local dmgmod = 2.8;
+    local numhits = 3;
+    local accmod = 3;
+    local dmgmod = 3.2;
     local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_NO_EFFECT);
     local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_SLASH,info.hitslanded);
     target:delHP(dmg);
