7b90b5fc3756c7d65ce384602afb817280d694d8
TeoTwawki <arawn.annwn@yandex.com>
2017-05-31 15:41:14 -0700

    update mapserver batch IPs to match zone_settings IP

--- batch/GameServer-DynaLimbus.cmd
+++ batch/GameServer-DynaLimbus.cmd
@@ -2,7 +2,7 @@
 cd ..
 :onCrash
 echo [%date% %time%] Restarting Dynamis/Limbus Map Server...
-DSGame-DynaLimbus.exe --ip 24.192.160.140 --port 54430 --log .\log\DynaLimbus_Map.log
+DSGame-DynaLimbus.exe --ip 69.14.82.66 --port 54430 --log .\log\DynaLimbus_Map.log
 echo Server was stopped or crashed!
 echo %date% %time%> .\log\Last_DynaLimbus_Map_Stop.log
 @echo ...
--- batch/GameServer-GM.cmd
+++ batch/GameServer-GM.cmd
@@ -2,7 +2,7 @@
 cd ..
 :onCrash
 echo [%date% %time%] Restarting GM Map Server...
-DSGame-GM.exe --ip 24.192.160.140 --port 54130 --log .\log\GM_Map.log
+DSGame-GM.exe --ip 69.14.82.66 --port 54130 --log .\log\GM_Map.log
 echo Server was stopped or crashed!
 echo %date% %time%> .\log\Last_GM_Map_Stop.log
 @echo ...
