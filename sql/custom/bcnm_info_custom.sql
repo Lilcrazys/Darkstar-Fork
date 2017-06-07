ab450c9a99124a67d1c8acd656b62af6d1f1bbbf
TeoTwawki <arawn.annwn@yandex.com>
2017-06-06 20:40:00 -0400

    Adjust casing on custom files - everything in mobskills and BCNM is full lowercased in DSP now.

--- sql/custom/bcnm_info_custom.sql
+++ sql/custom/bcnm_info_custom.sql
@@ -16,6 +16,6 @@ INSERT INTO `bcnm_info` VALUES (548,207,'htb_trial_by_fire',NULL,NULL,1800,0,6,0
 INSERT INTO `bcnm_info` VALUES (581,209,'htb_trial_by_earth',NULL,NULL,1800,0,6,0,5);     -- HTB Avatars: Titan
 INSERT INTO `bcnm_info` VALUES (612,211,'htb_trial_by_water',NULL,NULL,1800,0,6,0,5);     -- HTB Avatars: Leviathan
 
--- REPLACE INTO `bcnm_info` VALUES (1184,78,'The_Rider_Cometh',null,null,900,0,6,0,5);
+-- REPLACE INTO `bcnm_info` VALUES (1184,78,'the_rider_cometh',null,null,900,0,6,0,5);
 
-INSERT INTO `bcnm_info` VALUES (1308,255,'The_Wyrm_God',null,null,1800,0,18,0,7);
+INSERT INTO `bcnm_info` VALUES (1308,255,'the_wyrm_god',null,null,1800,0,18,0,7);
