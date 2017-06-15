5088da36968d02d1d1dbed1d235492409dd81b3a
TeoTwawki <arawn.annwn@yandex.com>
2017-06-14 17:32:03 -0700

    Fix my derp in auto updater.
    Somehow forgot how to use cmd variables har har.

--- batch/Auto-SQL-Updater.cmd
+++ batch/Auto-SQL-Updater.cmd
@@ -10,47 +10,48 @@ PAUSE
 set mysqlExec="C:\Program Files\MariaDB 10.1\bin\mysql.exe"
 ECHO ---------------------------------
 ECHO Importing misc non character data tables...
-FOR %%X IN (augments.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
-FOR %%X IN (status_effects.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
-FOR %%X IN (synth_recipes.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
-FOR %%X IN (zone_settings.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
-FOR %%X IN (treasure_spawn_points.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
-FOR %%X IN (transport.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (augments.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (status_effects.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (synth_recipes.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (zone_settings.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (treasure_spawn_points.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (transport.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
 ECHO ---------------------------------
 ECHO Importing ability/trait/ws/merit tables...
-FOR %%X IN (abilities.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
-FOR %%X IN (abilities_charges.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
-FOR %%X IN (traits.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
-FOR %%X IN (weapon_skills.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
-FOR %%X IN (merits.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (abilities.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (abilities_charges.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (traits.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (weapon_skills.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (merits.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
 ECHO ---------------------------------
 ECHO Importing spell tables...
-FOR %%X IN (spell_list.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
-FOR %%X IN (blue_spell_list.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
-FOR %%X IN (blue_spell_mods.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
-FOR %%X IN (blue_traits.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (spell_list.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (blue_spell_list.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (blue_spell_mods.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (blue_traits.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
 ECHO ---------------------------------
 ECHO Importing bcnm and instance tables...
-FOR %%X IN (bcnm*.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
-FOR %%X IN (instance*.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (bcnm*.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (instance*.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
 ECHO ---------------------------------
 ECHO Importing guild tables...
-FOR %%X IN (guild*.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (guild*.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
 ECHO ------------------------------------------------------------------
 ECHO Importing item tables...
-FOR %%X IN (item*.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (item*.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
 ECHO ---------------------------------
 ECHO importing mob and npc tables...
-FOR %%X IN (mob*.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
-FOR %%X IN (npc*.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (mob*.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (npc*.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
 ECHO ---------------------------------
 ECHO Importing custom tables...
 cd custom
-FOR %%X IN (*_custom.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (*_custom.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
 cd ..
 ECHO ---------------------------------
 ECHO Resetting triggers.
-FOR %%X IN (triggers.sql) DO ECHO Importing %%X & mysqlExec legiondb -h localhost -u root -pIr0nd00r < %%X
+FOR %%X IN (triggers.sql) DO ECHO Importing %%X & %mysqlExec% legiondb -h localhost -u root -pIr0nd00r < %%X
+set %mysqlExec%=""
 ECHO Finished!
 PAUSE
 PAUSE
