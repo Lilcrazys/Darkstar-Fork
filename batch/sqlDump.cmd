74f0675dd4f26697211b552a62b09ca41a5559d2
TeoTwawki <arawn.annwn@yandex.com>
2017-07-28 15:28:51 -0400

    Update sqlDump batch script to use a temp directory to reduce latency involved due to networked drive location. Also add blacklist to backup >.>
    ..And why the F did I have "call" in there.. That's not a batch thats an exe.. Past me go home, yer drunk.

--- batch/sqlDump.cmd
+++ batch/sqlDump.cmd
@@ -6,8 +6,8 @@ set dumpExe="C:\Program Files\MariaDB 10.1\bin\mysqldump.exe"
 set mysqlhost=localhost
 set mysqluser=root
 set mysqlpass=Ir0nd00r
-IF NOT EXIST E:\database-backup\logs\ md E:\database-backup\logs\
-set LogPath=E:\database-backup\logs
+IF NOT EXIST C:\LegionDark\GameServer\log\sqldump\ md C:\LegionDark\GameServer\log\sqldump\
+set LogPath=C:\LegionDark\GameServer\log\sqldump
 for /f "tokens=1" %%i in ('date /t') do set DATE_DOW=%%i
 for /f "tokens=2" %%i in ('date /t') do set DATE_DAY=%%i
 for /f %%i in ('echo %date_day:/=-%') do set DATE_DAY=%%i
@@ -16,43 +16,55 @@ for /f %%i in ('echo %date_time::=-%') do set DATE_TIME=%%i
 for /f %%i in ('time /t') do set START_TIME=%%i
 for /f %%i in ('echo %start_time::=:%') do set START_TIME=%%i
 CLS
-ECHO Backup Started Date: %DATE_DAY% Time: %START_TIME%
+REM Make missing temp directories for sql dump as needed..
+IF NOT EXIST C:\temp\ md C:\temp\
+IF NOT EXIST C:\temp\database-backup\ md C:\temp\database-backup\
+IF NOT EXIST C:\temp\database-backup\legiondb\ md C:\temp\database-backup\legiondb\
+IF NOT EXIST C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\ md C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\
+REM Make missing 7z directories as needed..
+IF NOT EXIST C:\temp\database-backup\legiondb\7z\ md C:\temp\database-backup\legiondb\7z\
+IF NOT EXIST C:\temp\database-backup\legiondb\7z\%DATE_DAY%_%DATE_TIME%\ md C:\temp\database-backup\legiondb\7z\%DATE_DAY%_%DATE_TIME%\
+REM Make missing network drive directories as needed..
+IF NOT EXIST E:\database-backup\ md E:\database-backup\
 IF NOT EXIST E:\database-backup\legiondb\ md E:\database-backup\legiondb\
 IF NOT EXIST E:\database-backup\legiondb\7z\ md E:\database-backup\legiondb\7z\
-IF NOT EXIST E:\database-backup\legiondb\temp\ md E:\database-backup\legiondb\temp\
 IF NOT EXIST E:\database-backup\legiondb\7z\%DATE_DAY%_%DATE_TIME%\ md E:\database-backup\legiondb\7z\%DATE_DAY%_%DATE_TIME%\
-IF NOT EXIST E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\ md E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\
+ECHO Backup Started Date: %DATE_DAY% Time: %START_TIME%
 ECHO Dumping game server DB tables's..
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb accounts > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\accounts.sql"
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb account_ip_record > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\account_ip_record.sql"
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_effects > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\char_effects.sql"
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_equip > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\char_equip.sql"
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_exp > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\char_exp.sql"
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_inventory > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\char_inventory.sql"
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_jobs > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\char_jobs.sql"
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_look > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\char_look.sql"
-REM Stupid ass DSP misnamed the file below
-REM %dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_merit > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\char_merit.sql"
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_merit > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\char_merits.sql"
-REM Stupid ass DSP misnamed the file above
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_pet_name > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\char_pet_name.sql"
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_points > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\char_points.sql"
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_profile > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\char_profile.sql"
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_skills > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\char_skills.sql"
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_stats > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\char_stats.sql"
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_storage > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\char_storage.sql"
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_spells > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\char_spells.sql"
-REM (doesn't exist anymore/yet?) %dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_titles > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\char_titles.sql"
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_vars > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\char_vars.sql"
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb chars > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\chars.sql"
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb delivery_box > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\delivery_box.sql"
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb linkshells > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\linkshells.sql"
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb server_variables > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\server_variables.sql"
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb zone_settings > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\zone_settings.sql"
-%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb auction_house > "E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\auction_house.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb accounts > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\accounts.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb account_ip_record > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\account_ip_record.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_blacklist > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\char_blacklist.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_effects > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\char_effects.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_equip > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\char_equip.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_exp > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\char_exp.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_inventory > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\char_inventory.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_jobs > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\char_jobs.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_look > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\char_look.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_merit > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\char_merit.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_pet_name > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\char_pet_name.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_points > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\char_points.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_profile > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\char_profile.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_skills > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\char_skills.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_stats > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\char_stats.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_storage > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\char_storage.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_spells > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\char_spells.sql"
+REM (doesn't exist anymore/yet?) %dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_titles > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\char_titles.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb char_vars > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\char_vars.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb chars > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\chars.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb delivery_box > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\delivery_box.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb linkshells > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\linkshells.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb server_variables > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\server_variables.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb zone_settings > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\zone_settings.sql"
+%dumpExe% --lock-tables=false --skip-triggers --skip-extended-insert --order-by-primary --hex-blob --no-create-info --compact --skip-comments -h %mysqlhost% -u %mysqluser% -p%mysqlpass% legiondb auction_house > "C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\auction_house.sql"
 ECHO ..Done dumping tables. Now compressing SQL Files..
-IF NOT EXIST E:\database-backup\legiondb\7z\%DATE_DAY%_%DATE_TIME%\ md E:\database-backup\legiondb\7z\%DATE_DAY%_%DATE_TIME%\
-CALL "C:\Program Files\7-Zip\7z.exe" a -t7z -aoa -mx9 E:\database-backup\legiondb\7z\%DATE_DAY%_%DATE_TIME%\DB.7z E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\*.*
-ECHO ..DONE. Now deleting Temp SQL Files..
-@rmdir E:\database-backup\legiondb\temp\%DATE_DAY%_%DATE_TIME%\ /S /q
-ECHO All done! %DATE_DAY% StartTime: %START_TIME% - EndTime: %END_TIME%
+"C:\Program Files\7-Zip\7z.exe" a -t7z -aoa -mx9 C:\temp\database-backup\legiondb\7z\%DATE_DAY%_%DATE_TIME%\DB.7z C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\*.*
+copy /V /Y C:\temp\database-backup\legiondb\7z\%DATE_DAY%_%DATE_TIME%\DB.7z /B E:\database-backup\legiondb\7z\%DATE_DAY%_%DATE_TIME%\DB.7z /B
+ECHO ..DONE. Now nuking vars and deleting temp files..
+set dumpExe=
+set mysqlhost=
+set mysqluser=
+set mysqlpass=
+set LogPath=
+@rmdir C:\temp\database-backup\legiondb\%DATE_DAY%_%DATE_TIME%\ /S /q
+@rmdir C:\temp\database-backup\legiondb\7z\%DATE_DAY%_%DATE_TIME%\ /S /q
+ECHO All finished! %DATE_DAY% StartTime: %START_TIME% - EndTime: time /t
