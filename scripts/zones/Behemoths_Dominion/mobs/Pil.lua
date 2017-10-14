d3ba196e5eab419125128544e9fd847bc9243cdc
TeoTwawki <arawn.annwn@yandex.com>
2017-10-14 18:15:19 -0400

    "Enhanced" /Behemoths_Dominion/mobs/Pil.lua

--- scripts/zones/Behemoths_Dominion/mobs/Pil.lua
+++ scripts/zones/Behemoths_Dominion/mobs/Pil.lua
@@ -41,6 +41,18 @@ function onMobSpawn(mob)
     -- Vars
     -- mob:setLocalVar("depopTime", os.time(t) + 1800);  -- despawn in 30 min
 end;
+
+
+-----------------------------------
+-- onMobRoam Action
+-----------------------------------
+
+function onMobRoam(mob)
+    if (mob:getLocalVar("RAGED") == 1 and mob:getLocalVar("RAGE_TIME") < os.time()) then
+        mob:setLocalVar("RAGED", 0);
+    end
+end;
+
 -----------------------------------
 -- onMobEngage Action
 -----------------------------------
@@ -49,10 +61,44 @@ function onMobEngaged(mob, target)
 end;
 
 -----------------------------------
+-- onMobDisengage Action
+-----------------------------------
+
+function onMobDisengage(mob)
+    if (mob:getLocalVar("RAGED") == 1) then
+        mob:setLocalVar("RAGE_TIME", os,time()+30);
+    end
+end;
+
+-----------------------------------
 -- onMobFight Action
 -----------------------------------
 
-function onMobFight(mob, target)
+function onMobFight(mob, target).
+    -- 45 min passed and its not already raged
+    if (BattleTime - os.time() > 2700 and mob:getLocalVar("RAGED") == 0) then
+        mob:setLocalVar("RAGED", 1);
+        -- Now lets make claim holder's eat SH and die.
+        mob:addMod(MOD_ATTP, 999);
+        mob:addMod(MOD_DEFP, 999);
+        mob:addMod(MOD_ACC, 9999);
+        mob:addMod(MOD_EVA, 9999);
+        mob:addMod(MOD_MOD_STR, 999);
+        mob:addMod(MOD_MOD_DEX, 999);
+        mob:addMod(MOD_MOD_VIT, 999);
+        mob:addMod(MOD_MOD_AGI, 999);
+        mob:addMod(MOD_MOD_INT, 999);
+        mob:addMod(MOD_MOD_MND, 999);
+        mob:addMod(MOD_MOD_CHR, 999);
+        mob:addMod(MOD_CRITHITRATE, 50);
+        mob:addMod(MOD_HASTE_MAGIC, 50);
+        mob:addMod(MOD_MOVE, 50);
+        mob:addMod(MOD_DMGRANGE, -50);
+        mob:addMod(MOD_UDMGPHYS, -50);
+        mob:addMod(MOD_UDMGBREATH, -50);
+        mob:addMod(MOD_UDMGMAGIC, -50);
+        mob:addMod(MOD_UDMGRANGE, -50);
+    end
 end;
 
 -----------------------------------
@@ -60,46 +106,23 @@ end;
 -----------------------------------
 
 function onSpellPrecast(mob, spell)
-    if (spell:getID() == 219) then -- comet
+    local spellTable =
+    {
+        -- [spellID] = animationID, -- Spell Name
+        [502] = 242, -- Kaustra
+        [219] = 280, -- Comet
+        [215] = 820, -- Flood 2
+        [213] = 824, -- Burst 2
+        [211] = 819, -- Quake 2
+        [209] = 822, -- Tornado 2
+        [207] = 823, -- Freeze2
+        [205] = 821  -- Flare2
+    }
+    if (spellTable[spell:getID] ~= nil) then
         spell:setAoE(SPELLAOE_RADIAL);
         spell:setFlag(SPELLFLAG_HIT_ALL);
         spell:setRadius(30);
-        spell:setAnimation(280);
-    elseif (spell:getID() == 502) then -- Kaustra
-        spell:setAoE(SPELLAOE_RADIAL);
-        spell:setFlag(SPELLFLAG_HIT_ALL);
-        spell:setRadius(30);
-        spell:setAnimation(242);
-    elseif (spell:getID() == 205) then -- flare2
-        spell:setAoE(SPELLAOE_RADIAL);
-        spell:setFlag(SPELLFLAG_HIT_ALL);
-        spell:setRadius(30);
-        spell:setAnimation(821);
-    elseif (spell:getID() == 207) then -- freeze2
-        spell:setAoE(SPELLAOE_RADIAL);
-        spell:setFlag(SPELLFLAG_HIT_ALL);
-        spell:setRadius(30);
-        spell:setAnimation(823);
-    elseif (spell:getID() == 209) then -- tornado 2
-        spell:setAoE(SPELLAOE_RADIAL);
-        spell:setFlag(SPELLFLAG_HIT_ALL);
-        spell:setRadius(30);
-        spell:setAnimation(822);
-    elseif (spell:getID() == 211) then -- quake 2
-        spell:setAoE(SPELLAOE_RADIAL);
-        spell:setFlag(SPELLFLAG_HIT_ALL);
-        spell:setRadius(30);
-        spell:setAnimation(819);
-    elseif (spell:getID() == 213) then -- burst 2
-        spell:setAoE(SPELLAOE_RADIAL);
-        spell:setFlag(SPELLFLAG_HIT_ALL);
-        spell:setRadius(30);
-        spell:setAnimation(824);
-    elseif (spell:getID() == 215) then -- flood 2
-        spell:setAoE(SPELLAOE_RADIAL);
-        spell:setFlag(SPELLFLAG_HIT_ALL);
-        spell:setRadius(30);
-        spell:setAnimation(820);
+        spell:setAnimation(spellTable[spell:getID]);
     end
 end;
 
@@ -108,26 +131,19 @@ end;
 -----------------------------------
 
 function onMobDeath(mob, player, isKiller)
-
     if (isKiller == true) then
-        local RND1 = math.random(1,8);
-        if (RND1 == 1) then
-            player:addTreasure(8919, mob); -- Ifritear
-        elseif (RND1 == 2) then
-            player:addTreasure(8920, mob); -- Leviatear
-        elseif (RND1 == 3) then
-            player:addTreasure(8921, mob); -- Ramutear
-        elseif (RND1 == 4) then
-            player:addTreasure(8922, mob); -- Garutear
-        elseif (RND1 == 5) then
-            player:addTreasure(8923, mob); -- Titatear
-        elseif (RND1 == 6) then
-            player:addTreasure(8924, mob); -- Shivatear
-        elseif (RND1 == 7) then
-            player:addTreasure(8925, mob); -- Carbutear
-        elseif (RND1 == 8) then
-            player:addTreasure(8926, mob); -- Fenritear
-        end
+        local tearsTable =
+        {
+            [1] = 8919, -- Ifritear
+            [2] = 8920, -- Leviatear
+            [3] = 8921, -- Ramutear
+            [4] = 8922, -- Garutear
+            [5] = 8923, -- Titatear
+            [6] = 8924, -- Shivatear
+            [7] = 8925, -- Carbutear
+            [8] = 8926  -- Fenritear
+        }
+        player:addTreasure(tearsTable[math.random(1,8)], mob); -- 100% drop of one of eight elemental tears
     end
 
     if (player:hasKeyItem(WHITE_STRATUM_ABYSSITE_III)) then -- Pil Kill
