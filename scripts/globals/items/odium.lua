51d0170d1cec5c842d0d315f8050436a7a880f32
TeoTwawki <arawn.annwn@yandex.com>
2017-10-23 19:14:05 -0400

    Less odius odium script..

--- scripts/globals/items/odium.lua
+++ scripts/globals/items/odium.lua
@@ -9,9 +9,6 @@ require("scripts/globals/msg");
 -----------------------------------
 
 function onAdditionalEffect(player,target,damage)
-    local MESSAGE = msgBasic.ADD_EFFECT_STATUS;
-    local procRate = 3;
-
     -- AddEffect Death does not Proc on Notorious Monsters
     if (target:isMob()) then
         if (target:isMobType(MOBTYPE_NOTORIOUS)) then
@@ -20,12 +17,13 @@ function onAdditionalEffect(player,target,damage)
     end
 
     -- Now continue with normal resistance and random number checks..
-    if (procRate < math.random(1,100) or applyResistanceAddEffect(player,target,ELE_DARK,0) <= 0.5) then
+    local procRate = 3;
+    if (procRate >= math.random(1,100) or applyResistanceAddEffect(player,target,ELE_DARK,0) <= 0.5) then
         -- If random number beats procRate OR if effect is resisted, return all zero
         return 0,0,0;
     else
         -- It's clobberin time!
         target:setHP(0)
-        return SUBEFFECT_DEATH, MESSAGE, EFFECT_KO;
+        return SUBEFFECT_DEATH, msgBasic.ADD_EFFECT_STATUS, EFFECT_KO;
     end
 end;
