eb4b1c6dc2ad78a8d2aba773b520159e88c7b51f
TeoTwawki <arawn.annwn@yandex.com>
2017-11-30 20:03:41 -0500

    Fix (I hope) auroral_drape.lua resist problems from DSP function change

--- scripts/globals/spells/bluemagic/auroral_drape.lua
+++ scripts/globals/spells/bluemagic/auroral_drape.lua
@@ -13,27 +13,31 @@ end;
 
 function onSpellCast(caster,target,spell)
     local effect = EFFECT_NONE;
-    local dINT = caster:getStat(MOD_INT) - target:getStat(MOD_INT);
-    -- The 2 effects roll their resistance separately..
-    local BLIND_RES = applyResistanceEffect(caster,spell,target,dINT,BLUE_SKILL,0,EFFECT_BLINDNESS);
-    local SILENCE_RES = applyResistanceEffect(caster,spell,target,dINT,BLUE_SKILL,0,EFFECT_SILENCE);
+    local params = {};
+    params.attribute = MOD_INT;
+    params.effect = EFFECT_BLINDNESS;
+    local BLIND_RES = applyResistance(caster, target, spell, params);
+    params.attribute = MOD_MND;
+    params.effect = EFFECT_SILENCE;
+    local SILENCE_RES = applyResistance(caster, target, spell, params);
+    params.skillType = BLUE_SKILL;
 
     if (target:hasStatusEffect(EFFECT_BLINDNESS) and target:hasStatusEffect(EFFECT_SILENCE)) then
-        spell:setMsg(75); -- No effect.
+        spell:setMsg(msgBasic.MAGIC_NO_EFFECT); -- No effect.
         return EFFECT_NONE;
     else
-        local MSG = 85; -- Will be used if both effects are resited.
+        local MSG = msgBasic.MAGIC_RESIST; -- Will be used if both effects are resisted.
 
         if (BLIND_RES > 0.125) then
             target:addStatusEffect(EFFECT_BLINDNESS,60,0,60);
             effect = EFFECT_BLINDNESS;
-            MSG = 237; -- Landed it.
+            MSG = msgBasic.MAGIC_ENFEEB; -- Landed it.
         end
 
         if (SILENCE_RES > 0.125) then
             target:addStatusEffect(EFFECT_SILENCE,20,0,60);
             effect = EFFECT_SILENCE;
-            MSG = 237; -- Landed it.
+            MSG = msgBasic.MAGIC_ENFEEB; -- Landed it.
         end
 
         spell:setMsg(MSG);
