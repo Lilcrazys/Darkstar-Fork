98883b699f62ce54733cbd754ea33fc5824e871c
TeoTwawki <arawn.annwn@yandex.com>
2017-12-02 16:55:35 -0500

    Temp script for gates of hades..its still not correct yet need work done in darkstar first

--- scripts/globals/spells/bluemagic/gates_of_hades.lua
+++ scripts/globals/spells/bluemagic/gates_of_hades.lua
@@ -1,17 +1,18 @@
 -----------------------------------------
---  Gates of Hades
+-- Gates of Hades
+-- Deals severe Fire damage to enemies within an area of effect. Additional effect: Burn
+-- Can only be cast with job ability Unbridled Learning.
+-- Burn effect is 22 HP/tick and -47 INT.
+-- https://www.bg-wiki.com/bg/Gates_of_Hades
 -----------------------------------------
-require("scripts/globals/magic");
-require("scripts/globals/status");
 require("scripts/globals/bluemagic");
+require("scripts/globals/status");
+require("scripts/globals/magic");
 require("scripts/globals/msg");
-
------------------------------------------
--- OnSpellCast
 -----------------------------------------
 
 function onMagicCastingCheck(caster,target,spell)
-	if (caster:hasStatusEffect(EFFECT_UNBRIDLED_LEARNING) == true) then
+    if (caster:hasStatusEffect(EFFECT_UNBRIDLED_LEARNING) == true) then
       return 0;
    else
       return chatType.STATUS_PREVENTS;
@@ -20,7 +21,9 @@ end;
 
 function onSpellCast(caster,target,spell)
     local params = {};
-
+    params.attribute = MOD_INT;
+    params.skillType = BLUE_SKILL;
+    params.effect = EFFECT_BURN;
     params.multiplier = 5.0;
     params.tMultiplier = 1.0;
     params.duppercap = 100;
@@ -32,17 +35,18 @@ function onSpellCast(caster,target,spell)
     params.mnd_wsc = 0.0;
     params.chr_wsc = 0.0;
 
-    local damage = BlueMagicalSpell(caster, target, spell, params, STR_BASED);
+    local resist = applyResistanceEffect(caster, target, spell, params)
+    local damage = BlueMagicalSpell(caster, target, spell, params, 0); -- Don't use a stat here
     damage = BlueFinalAdjustments(caster, target, spell, damage, params);
 
-	local resist = applyResistance(caster,spell,target,caster:getStat(MOD_INT) - target:getStat(MOD_INT),BLUE_SKILL,1.0);
-
-	if (damage > 0 and resist < 0.125) then
-		local typeEffect = EFFECT_BURN;
-		target:delStatusEffect(typeEffect);
-		target:addStatusEffect(typeEffect,21,1,getBlueEffectDuration(caster,resist,typeEffect));
-	end
+    if (damage > 0 and resist < 0.125) then
+        -- Bg doesn't say effect varies.
+        -- Used variables here to make adjustment easy.
+        local DOT    = 22;
+        local duration = 90; -- getBlueEffectDuration doesn't handle burn.
+        local debuff = -47;
+        target:addStatusEffect(params.effect, DOT, 3, duration, 0, debuff);
+    end
 
     return damage;
 end;
-
