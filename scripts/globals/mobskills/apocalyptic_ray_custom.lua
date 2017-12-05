7b00a8d04b74e1b4135d14e61040eccbb7ecf8ca
TeoTwawki <arawn.annwn@yandex.com>
2017-12-05 13:50:42 -0500

    rewrite apocalyptic_ray_custom.lua Power is no longer duration, also note: do not do resist checks on duration, if resist outright cancel effect

--- scripts/globals/mobskills/apocalyptic_ray_custom.lua
+++ scripts/globals/mobskills/apocalyptic_ray_custom.lua
@@ -2,11 +2,9 @@
 -- Apocalyptic Ray
 --
 ---------------------------------------------
-
+require("scripts/globals/monstertpmoves");
 require("scripts/globals/settings");
 require("scripts/globals/status");
-require("scripts/globals/monstertpmoves");
-
 ---------------------------------------------
 
 function onMobSkillCheck(target,mob,skill)
@@ -14,13 +12,12 @@ function onMobSkillCheck(target,mob,skill)
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-    local typeEffect = EFFECT_DOOM;
-
-    skill:setMsg(MobGazeMove(mob, target, typeEffect, 10, 3, 5));
-
     local dmgmod = 3;
     local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*3.5,ELE_DARK,dmgmod,TP_MAB_BONUS);
     local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_DARK,MOBPARAM_IGNORE_SHADOWS);
+
+    skill:setMsg(MobGazeMove(mob, target, EFFECT_DOOM, 10, 3, 30));
+
     target:delHP(dmg);
     return dmg;
 end;
