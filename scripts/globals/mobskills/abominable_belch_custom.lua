615586612cfbacffc6c42dd882c6f7d34da76d40
TeoTwawki <arawn.annwn@yandex.com>
2017-12-11 01:54:45 -0500

    Made Orobon Belch moves as retail as can be without getting on retail to check message behavior.

--- scripts/globals/mobskills/abominable_belch_custom.lua
+++ scripts/globals/mobskills/abominable_belch_custom.lua
@@ -1,29 +1,37 @@
 ---------------------------------------------------
--- Stellar Burst
--- A starburst damages targets in an area of effect. Additional effect: Silence
+-- Abominable Belch
+-- AoE Paralyze, Silence, and Plague.
+-- Used in place of Vile Belch starting at <50% HP.
 ---------------------------------------------------
-
+require("scripts/globals/monstertpmoves");
 require("scripts/globals/settings");
 require("scripts/globals/status");
-require("scripts/globals/monstertpmoves");
-
 ---------------------------------------------------
 
 function onMobSkillCheck(target,mob,skill)
-	return 0;
+    if (mob:getHPP() <= 50) then
+        return 0;
+    else
+        return 1;
+    end
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-	local numhits = 1;
-	local accmod = 1;
-	local dmgmod = 1;
-	local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg(),ELE_NONE,dmgmod,TP_NO_EFFECT);
-	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_NONE,MOBPARAM_3_SHADOW);
-    MobStatusEffectMove(mob, target, EFFECT_SILENCE, 1, 0, 30);
-	MobStatusEffectMove(mob, target, EFFECT_PLAGUE, 1, 0, 30);
-	MobStatusEffectMove(mob, target, EFFECT_PARALYSIS, 1, 0, 30);
-	MobStatusEffectMove(mob, target, EFFECT_CHARM_I, 1, 0, 30);	
-	target:delHP(dmg);
-    mob:resetEnmity(target);
-	return dmg;
+    local effect = EFFECT_NONE;
+    local msg = msgBasic.MISS;
+    if (MobStatusEffectMove(mob, target, EFFECT_SILENCE, 1, 0, 30) == msgBasic.ENFEEB_IS) then
+        effect = EFFECT_SILENCE;
+    end
+    if (MobStatusEffectMove(mob, target, EFFECT_PARALYSIS, 1, 0, 30) == msgBasic.ENFEEB_IS) then
+        effect = EFFECT_PARALYSIS;
+    end
+    if (MobStatusEffectMove(mob, target, EFFECT_PLAGUE, 1, 0, 30) == msgBasic.ENFEEB_IS) then
+        effect = EFFECT_PLAGUE;
+    end
+    if (effect ~= EFFECT_NONE) then
+        msg = msgBasic.ENFEEB_IS;
+    end
+    skill:setMsg(msg);
+
+    return effect;
 end;
--- scripts/globals/mobskills/vile_belch_custom.lua
+++ scripts/globals/mobskills/vile_belch_custom.lua
@@ -1,30 +1,34 @@
 ---------------------------------------------------
-
+-- Vile Belch
+-- AoE Silence and Plague.
+-- Replaced by Abominable Belch when under 50% HP
 ---------------------------------------------------
+require("scripts/globals/monstertpmoves");
 require("scripts/globals/settings");
 require("scripts/globals/status");
-require("scripts/globals/monstertpmoves");
 ---------------------------------------------------
 
 function onMobSkillCheck(target,mob,skill)
-	local NM = mob:getID();
-	local HP = mob:getHPP();
-	
-	if (NM == 17662476 or NM == 17662481 or NM == 17662486) and (HP > 50) then
-		return 0;
-	end
-	return 1;
+    if (mob:getHPP() < 50) then
+        return 1;
+    else
+        return 0;
+    end
 end;
 
 function onMobWeaponSkill(target, mob, skill)
-	local numhits = 2;
-	local accmod = 2;
-	local dmgmod = 6;
-	local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg(),ELE_NONE,dmgmod,TP_NO_EFFECT);
-	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_NONE,MOBPARAM_3_SHADOW);
-    MobStatusEffectMove(mob, target, EFFECT_SILENCE, 1, 0, 30);
-	MobStatusEffectMove(mob, target, EFFECT_PLAGUE, 1, 0, 30);
-	target:delHP(dmg);
-    mob:resetEnmity(target);
-	return dmg;
+    local effect = EFFECT_NONE;
+    local msg = msgBasic.MISS;
+    if (MobStatusEffectMove(mob, target, EFFECT_SILENCE, 1, 0, 30) == msgBasic.ENFEEB_IS) then
+        effect = EFFECT_SILENCE;
+    end
+    if (MobStatusEffectMove(mob, target, EFFECT_PLAGUE, 1, 0, 30) == msgBasic.ENFEEB_IS) then
+        effect = EFFECT_PLAGUE;
+    end
+    if (effect ~= EFFECT_NONE) then
+        msg = msgBasic.ENFEEB_IS;
+    end
+    skill:setMsg(msg);
+
+    return effect;
 end;
