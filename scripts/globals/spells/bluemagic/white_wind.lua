7e2194cf169b7b8a19b94a9c2d7e996508866809
TeoTwawki <arawn.annwn@yandex.com>
2018-01-17 13:50:34 -0500

    Fix copypaste/typo. mob =/= caster..

--- scripts/globals/spells/bluemagic/white_wind.lua
+++ scripts/globals/spells/bluemagic/white_wind.lua
@@ -17,7 +17,7 @@ function onMagicCastingCheck(caster,target,spell)
 end;
 
 function onSpellCast(caster,target,spell)
-    local cure = math.floor(mob:getHP()/7)*2;
+    local cure = math.floor(caster:getHP()/7)*2;
     cure = getCureFinal(caster,spell,cure,1,true);
 
     spell:setMsg(msgBasic.MAGIC_RECOVERS_HP);
